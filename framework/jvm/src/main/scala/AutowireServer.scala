c93e8ed188be59195073e55ec1f619b68c1e685f
johannes karoff <johannes@karoff.net>
2016-12-16 20:13:42 +0100

    rename and some todos

--- framework/jvm/src/main/scala/AutowireServer.scala
+++ framework/jvm/src/main/scala/AutowireServer.scala
@@ -33,25 +33,26 @@ trait WebsocketServer {
   implicit val system = ActorSystem()
   implicit val materializer = ActorMaterializer()
 
-  val greeterWebSocketService =
+  val messageHandler =
     Flow[Message]
-      .mapAsync(4) {
-        case bm: BinaryMessage if bm.isStrict =>
+      .mapAsync(4) { // TODO why 4?
+        case bm: BinaryMessage if bm.isStrict => //TODO: extract serializer out of here
           val buffer = bm.getStrictData.asByteBuffer
           val call = Unpickle[api.Call].fromBytes(buffer)
           // println(call)
           router(Request(call.path, call.args)).map { result =>
-            val encoded = Pickle.intoBytes(Response(call.seqId, result): WebsocketMessage)
+            val encoded = Pickle.intoBytes(Response(call.seqId, result): ApiMessage)
             BinaryMessage(ByteString(encoded))
           }
       }
 
+  //TODO: serve index html
   val requestHandler: HttpRequest => HttpResponse = {
     //TODO: simpler upgrade router implementation
     //http://doc.akka.io/docs/akka-http/current/scala/http/websocket-support.html
     case req @ HttpRequest(GET, Uri.Path("/"), _, _, _) =>
       req.header[UpgradeToWebSocket] match {
-        case Some(upgrade) => upgrade.handleMessages(greeterWebSocketService)
+        case Some(upgrade) => upgrade.handleMessages(messageHandler)
         case None => HttpResponse(400, entity = "Not a valid websocket request!")
       }
     case r: HttpRequest =>
