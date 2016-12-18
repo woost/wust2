008f54eab371de1599ad1f8494619c7112f4117a
johannes karoff <johannes@karoff.net>
2016-12-18 21:13:40 +0100

    type dispatch actor

--- framework/jvm/src/main/scala/AutowireServer.scala
+++ framework/jvm/src/main/scala/AutowireServer.scala
@@ -28,60 +28,68 @@ object AutowireServer extends autowire.Server[ByteBuffer, Pickler, Pickler] {
   def write[Result: Pickler](r: Result) = Pickle.intoBytes(r)
 }
 
+trait Registrar[CHANNEL] {
+  def subscribe(channel: CHANNEL, sender: ActorRef): Unit
+}
+
+trait Dispatcher[CHANNEL,EVENT] extends Registrar[CHANNEL] {
+  def notify(channel: CHANNEL, event: EVENT): Unit
+}
+
+class DispatcherImpl[CHANNEL,EVENT] extends Dispatcher[CHANNEL,EVENT] with TypedActor.Receiver {
+  import scala.collection.mutable
+  private val connectedClients = mutable.Map.empty[CHANNEL, mutable.Set[ActorRef]].withDefaultValue(mutable.Set.empty)
+
+  def subscribe(channel: CHANNEL, sender: ActorRef) {
+    connectedClients(channel) += sender
+    TypedActor.context.watch(sender) // emits terminated when sender disconnects
+  }
+
+  def notify(channel: CHANNEL, event: EVENT) {
+    val notification = Notification(channel, event)
+    connectedClients(channel).foreach(_ ! notification)
+  }
+
+  def onReceive(message: Any, sender: ActorRef): Unit = message match {
+    case Terminated(actor) => connectedClients.values.foreach(_ -= actor)
+    case _ =>
+  }
+}
+
+class ConnectedClient[CHANNEL](registrar: Registrar[CHANNEL], router: AutowireServer.Router) extends Actor {
+  private def connected(outgoing: ActorRef): Receive = {
+    case CallRequest(seqId, path, args) =>
+      val response = router(Request(path, args)).map(Response(seqId, _))
+      response.foreach(outgoing ! _)
+    case Subscribe(channel: CHANNEL) => registrar.subscribe(channel, outgoing)
+  }
+
+  def receive = {
+    case ConnectedClient.Connected(outgoing) => context.become(connected(outgoing))
+  }
+}
+object ConnectedClient {
+  case class Connected(actor: ActorRef)
+}
+
 trait WebsocketServer[CHANNEL,EVENT] {
-  implicit val channelPickler: Pickler[CHANNEL]
-  implicit val eventPickler: Pickler[EVENT]
-  val router: AutowireServer.Router
+  implicit def channelPickler: Pickler[CHANNEL]
+  implicit def eventPickler: Pickler[EVENT]
+  implicit def clientMessagePickler = ClientMessage.pickler[CHANNEL]
+  implicit def serverMessagePickler = ServerMessage.pickler[CHANNEL, EVENT]
+
+  def route: Route
+  def router: AutowireServer.Router
 
   val wire = AutowireServer
 
   implicit val system = ActorSystem()
   implicit val materializer = ActorMaterializer()
 
-  private class Dispatcher extends Actor {
-    //TODO map of connectedClients
-    import scala.collection.mutable
-    val connectedClients = mutable.Map.empty[CHANNEL, mutable.Set[ActorRef]].withDefaultValue(mutable.Set.empty)
+  private val dispatcher: Dispatcher[CHANNEL,EVENT] = TypedActor(system).typedActorOf(TypedProps[DispatcherImpl[CHANNEL,EVENT]])
 
-    def receive = {
-      case Subscribe(channel: CHANNEL) =>
-        connectedClients(channel) += sender()
-        context.watch(sender()) // emits terminated when sender disconnects
-      case Terminated(sender) =>
-        connectedClients.values.foreach(_ -= sender)
-      case n@Notification(channel: CHANNEL, _) =>
-        connectedClients(channel).foreach(_ ! n)
-    }
-  }
-
-  private val dispatchActor = system.actorOf(Props(new Dispatcher))
-
-  private case class Connected(actor: ActorRef)
-
-  private class ConnectedClient extends Actor {
-    private def connected(outgoing: ActorRef): Receive = {
-      {
-        case CallRequest(seqId, path, args) =>
-          val response = router(Request(path, args)).map { result =>
-            Response(seqId, result)
-          }
-          response.foreach(outgoing ! _) // TODO better something like mapasync in flow?
-
-        case n: Subscribe[_] => dispatchActor ! n
-        case n: Notification[_,_] => outgoing ! n
-      }
-    }
-
-    def receive = {
-      case Connected(outgoing) => context.become(connected(outgoing))
-    }
-  }
-
-  private def newConnectedClient: Flow[Message, Message, NotUsed] = {
-    implicit def clientMessagePickler = ClientMessage.pickler[CHANNEL]
-    implicit def serverMessagePickler = ServerMessage.pickler[CHANNEL, EVENT]
-
-    val connectedClientActor = system.actorOf(Props(new ConnectedClient))
+  def newConnectedClient: Flow[Message, Message, NotUsed] = {
+    val connectedClientActor = system.actorOf(Props(new ConnectedClient(dispatcher, router)))
 
     val incomingMessages: Sink[Message, NotUsed] =
       Flow[Message].map {
@@ -93,10 +101,9 @@ trait WebsocketServer[CHANNEL,EVENT] {
     val outgoingMessages: Source[Message, NotUsed] =
       Source.actorRef[ServerMessage](10, OverflowStrategy.fail) //TODO why 10?
         .mapMaterializedValue { outActor =>
-          connectedClientActor ! Connected(outActor)
+          connectedClientActor ! ConnectedClient.Connected(outActor)
           NotUsed
-        }.map { (outMsg: ServerMessage) =>
-          println(outMsg)
+        }.map { outMsg =>
           val bytes = Pickle.intoBytes(outMsg)
           BinaryMessage(ByteString(bytes))
         }
@@ -109,10 +116,8 @@ trait WebsocketServer[CHANNEL,EVENT] {
   }
 
   def emit(channel: CHANNEL, event: EVENT) {
-    dispatchActor ! Notification(channel, event) // TODO: typed actors?
+    dispatcher.notify(channel, event)
   }
-
-  val route: Route
 
   def run(interface: String, port: Int): Future[ServerBinding] = {
     Http().bindAndHandle(commonRoute ~ route, interface = interface, port = port)
