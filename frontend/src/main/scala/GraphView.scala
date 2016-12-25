1415a15dd722b49462cadbb2e5f0ee1ca4aca1b0
Felix Dietze <felix@beerleader.net>
2016-12-25 19:38:16 +0100

    move CustomComponent to own file

--- frontend/src/main/scala/GraphView.scala
+++ frontend/src/main/scala/GraphView.scala
@@ -119,29 +119,3 @@ object GraphView extends CustomComponent[Graph]("GraphView") {
 
   val backendFactory = new Backend(_)
 }
-
-abstract class CustomComponent[_Props](componentName: String = "CustomComponent") {
-  type Props = _Props
-  type Scope = BackendScope[Props, Unit]
-
-  abstract class CustomBackend($: Scope) {
-    def render(p: Props) = <.div(^.ref := "component")
-    lazy val component = Ref[raw.HTMLElement]("component")($).get
-
-    def init(p: Props) = Callback.empty
-    def update(p: Props, oldProps: Option[Props] = None) = Callback.empty
-    def cleanup() = Callback.empty
-  }
-  val backendFactory: Scope => CustomBackend
-
-  protected val component = ReactComponentB[Props](componentName)
-    .backend(backendFactory(_))
-    .render(c => c.backend.render(c.props))
-    .componentDidMount(c => c.backend.init(c.props) >> c.backend.update(c.props, None))
-    .componentWillReceiveProps(c => c.$.backend.update(c.nextProps, Some(c.currentProps)))
-    .shouldComponentUpdate(_ => false) // let our custom code handle the update instead
-    .componentWillUnmount(c => c.backend.cleanup())
-    .build
-
-  def apply(p: Props) = component(p)
-}
