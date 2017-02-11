f550d7cf389027eacf050ae9481b0f32c8e8d93a
johannes karoff <johannes@karoff.net>
2017-02-11 10:52:20 +0100

    toggle visibility of graph and list when switching tabs

--- frontend/src/main/scala/MainView.scala
+++ frontend/src/main/scala/MainView.scala
@@ -21,18 +21,18 @@ object MainView {
   private val tabMode: Var[Tab] = Var(Tab.Graph)
 
   def component(state: GlobalState.type) =
-    <div fontFamily="sans-serif">
+    <div style="font-family:sans-serif">
       <button onclick={ (_: Event) => Client.auth.register("hans", "***").call(); () }>register</button>
       <button onclick={ (_: Event) => Client.login(api.PasswordAuth("hans", "***")); () }>login</button>
       <button onclick={ (_: Event) => Client.logout(); () }>logout</button>
-      <button onclick={ (_: Event) => tabMode.update(_ => Tab.Graph); GraphView.init(state.graph); () }>graph</button>
-      <button onclick={ (_: Event) => tabMode.update(_ => Tab.List); () }>list</button>
-      {
-        tabMode.map {
-          case Tab.Graph => GraphView.component
-          case Tab.List => ListView.component(state.graph)
-        }
-      }
+      <button onclick={ (_: Event) => tabMode := Tab.Graph; () }>graph</button>
+      <button onclick={ (_: Event) => tabMode := Tab.List; () }>list</button>
+      <div style={ tabMode.map(m => if (m == Tab.Graph) "visibility:visible" else "visibility:hidden") }>
+        { GraphView.component }
+      </div>
+      <div style={ tabMode.map(m => if (m == Tab.List) "visibility:visible" else "visibility:hidden") }>
+        { ListView.component(state.graph) }
+      </div>
       <div style="position:fixed; width:100%; bottom:0; left:0; padding:5px; background:#FFF; borderTop:1px; solid #DDD">
         { AddPostForm.component(state.graph, state.focusedPost) }
       </div>
