730a768d03436913bedc6c083db764ac90faec85
johannes karoff <johannes@karoff.net>
2017-02-17 13:34:19 +0100

    fix foreachNext in SourceVar

--- frontend/src/main/scala/SourceVar.scala
+++ frontend/src/main/scala/SourceVar.scala
@@ -7,7 +7,7 @@ class SourceVar[S, A](source: Var[S], mapping: Rx[S] => Rx[A]) extends Rx[A] {
   def update(f: S => S) = source.update(f)
 
   override def value = target.value
-  override def foreachNext(s: A => Unit) = target.foreach(s)
+  override def foreachNext(s: A => Unit) = target.foreachNext(s)
   override def foreach(s: A => Unit) = target.foreach(s)
   override def map[B](f: A => B): SourceVar[S, B] = new SourceVar(source, (_: Rx[S]) => target.map(f))
   override def flatMap[B](s: A => Rx[B]): SourceVar[S, B] = new SourceVar(source, (_: Rx[S]) => target.flatMap(s))
