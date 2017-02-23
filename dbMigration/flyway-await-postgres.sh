17d8fbb608191f13e89562e16146bed806fda47e
johannes karoff <johannes@karoff.net>
2017-02-23 19:28:22 +0100

    remove unneeded todo

--- dbMigration/flyway-await-postgres.sh
+++ dbMigration/flyway-await-postgres.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 echo -n "waiting for postgres to be up..."
-#TODO without nc: (echo >/dev/tcp/postgres/5432) > /dev/null 2>&1 && echo connected
 while [ -z "`nc -z postgres 5432 && echo connected`" ]; do
     sleep 1
 done
