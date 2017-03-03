4c0ecad3c3afe887694a2b7cb4dc585ae8a12538
johannes karoff <johannes@karoff.net>
2017-03-03 12:34:49 +0100

    scalatags in views and listview

--- frontend/src/main/scala/views/ListView.scala
+++ frontend/src/main/scala/views/ListView.scala
@@ -13,35 +13,29 @@ import frontend._
 import util.algorithm.{Tree, redundantSpanningTree}
 
 object ListView {
-  def postColor(id: AtomId): InteractionMode => d3v4.Color = {
-    case FocusMode(`id`) => d3v4.d3.lab("#00ff00")
-    case EditMode(`id`) => d3v4.d3.lab("#0000ff")
+  def postColor(id: AtomId, mode: Rx[InteractionMode])(implicit ctx: Ctx.Owner): Rx[d3v4.Color] = mode.map {
+    //TODO scala.reflect.internal.FatalError: unexpected UnApply frontend.FocusMode.unapply(<unapply-selector>) <unapply> (`id`)
+    // case FocusMode(`id`) => d3v4.d3.lab("#00ff00")
+    // case EditMode(`id`) => d3v4.d3.lab("#0000ff")
     case _ => Color.postDefaultColor
   }
 
-  def editPostStyle(id: AtomId): InteractionMode => String = {
-    case _ => ""
-  }
-
-  def postItem(state: GlobalState, post: Post)(implicit ctx: Ctx.Owner) = {
+  def postItem(state: GlobalState, post: Post)(implicit ctx: Ctx.Owner): Modifier = {
     import state._
-    div(
-      onclick := { () => focusedPostId.update(_.setOrToggle(post.id)) },
-      mode.map { mode =>
-        post.toString
-        // Views.post(post, color = postColor(post.id)(mode), afterTitle = Some(
-        //   <div>
-        //     <span onclick={ () => editedPostId.update(_.setOrToggle(post.id)) }>[edit]</span>
-        //     {
-        //       collapsedPostIds.map { collapsed =>
-        //         <span onclick={ () => collapsedPostIds.update(_.toggle(post.id)) }>
-        //           { if (collapsed(post.id)) "+" else "-" }
-        //         </span>
-        //       }
-        //     }
-        //     )
-        // ))
-      }
+    Views.post(
+      post,
+      color := postColor(post.id, mode).map(_.toString),
+      onclick := { () => focusedPostId.update(_.setOrToggle(post.id)) }
+    )(
+      div(
+        span(onclick := { () => editedPostId.update(_.setOrToggle(post.id)) }, "[edit]"),
+        collapsedPostIds.rx.map { collapsed =>
+          span(
+            onclick := { () => collapsedPostIds.update(_.toggle(post.id)) },
+            if (collapsed(post.id)) "+" else "-"
+          ).render
+        }
+      )
     )
   }
 
@@ -52,14 +46,13 @@ object ListView {
   )
 
   def component(state: GlobalState)(implicit ctx: Ctx.Owner) = {
-    val a: Rx[Element] = state.graph.map { graph =>
+    div(state.graph.rx.map { graph =>
       div( //TODO: avoid this nesting by passing Rx[Seq[Element]] to the outer div?
         (graph.posts.keys.map { postId =>
           val tree = redundantSpanningTree(postId, graph.children)
           postTreeItem(tree, id => postItem(state, graph.posts(id)))
         }).toList
       ).render
-    }
-    div(a)
+    })
   }
 }
--- frontend/src/main/scala/views/Views.scala
+++ frontend/src/main/scala/views/Views.scala
@@ -1,38 +1,43 @@
-// package frontend.views
+package frontend.views
 
-// import scalajs.concurrent.JSExecutionContext.Implicits.queue
+import scalajs.concurrent.JSExecutionContext.Implicits.queue
 
-// import org.scalajs.d3v4
-// import org.scalajs.dom._
-// import frontend.Client
-// import frontend.Color._
-// import graph._
-// import autowire._
-// import boopickle.Default._
-// import scala.xml.Node
-// import mhtml.emptyHTML
+import org.scalajs.d3v4
+import org.scalajs.dom
+import frontend.Client
+import frontend.Color._
+import graph._
+import autowire._
+import boopickle.Default._
 
-// object Views {
-//   def post(post: Post, color: d3v4.Color = postDefaultColor, afterTitle: Option[Node] = None) =
-//     //TODO: share style with graphview
-//     <div style={ s"padding: 3px 5px; margin: 3px; border-radius: 3px; max-width: 10em; border: 1px solid #444; background-color: ${color.toString}" }>
-//       { post.title }
-//       { afterTitle.getOrElse(emptyHTML) }
-//     </div>
+import rx._
+import scalatags.generic.Modifier
+import scalatags.rx.all._
+import scalatags.JsDom.all._
 
-//   // times symbol
-//   def parents(containsIds: Seq[AtomId], graph: Graph) =
-//     <div style="display: flex">
-//       {
-//         containsIds.map { containsId =>
-//           val contains = graph.containments(containsId)
-//           val parent = graph.posts(contains.parentId)
-//           post(parent, baseColor(parent.id), Some(
-//             <span onclick={ (_: Event) => Client.api.deleteContainment(contains.id).call(); () } style="padding: 0 0 0 3px; cursor: pointer">
-//               ×
-//             </span>
-//           ))
-//         }
-//       }
-//     </div>
-// }
+object Views {
+  def post(post: Post, attrs: Modifier[dom.raw.Element]*) = {
+    //TODO: share style with graphview
+    val defaults = Seq(
+      style := s"padding: 3px 5px; margin: 3px; border-radius: 3px; max-width: 10em; border: 1px solid #444"
+    )
+    div((defaults ++ attrs): _*)(p(post.title))
+  }
+
+  // times symbol
+  def parents(containsIds: Seq[AtomId], graph: Graph) =
+    div(
+      style := "display: flex",
+      containsIds.map { containsId =>
+        val contains = graph.containments(containsId)
+        val parent = graph.posts(contains.parentId)
+        post(parent, color := baseColor(parent.id).toString)(
+          span(
+            onclick := { () => Client.api.deleteContainment(contains.id).call(); () },
+            style := "padding: 0 0 0 3px; cursor: pointer",
+            "×"
+          )
+        )
+      }
+    )
+}
