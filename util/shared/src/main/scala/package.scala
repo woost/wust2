e978b58109d652c980efdc4571740c11010b57f9
johannes karoff <johannes@karoff.net>
2017-03-06 18:08:12 +0100

    identity pipe does not need generic

--- util/shared/src/main/scala/package.scala
+++ util/shared/src/main/scala/package.scala
@@ -2,6 +2,6 @@
 package object util {
   implicit class Pipe[T](val v: T) extends AnyVal {
     def |>[U] (f: T => U): U = f(v)
-    def ||>[U](f: T => U): T = {f(v); v}
+    def ||>(f: T => Any): T = {f(v); v}
   }
 }
