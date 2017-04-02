5a5834913a57e3b9a7400e7812810632bc9510ae
johannes karoff <johannes@karoff.net>
2017-04-02 23:05:35 +0200

    use location.hash in url router

--- frontend/src/main/scala/UrlRouter.scala
+++ frontend/src/main/scala/UrlRouter.scala
@@ -11,35 +11,20 @@ trait Routeable[T] {
   def toRoute(routeable: T): String
 }
 
-case class UrlHash(url: String, hash: Option[String]) {
-  def toUrl = url + hash.map("#" + _).getOrElse("")
-}
-object UrlHash {
-  def apply(url: String): UrlHash = {
-    val parts: Array[String] = url.split("#")
-    val baseUrl = parts.init.fold("")(_ + _)
-    val hash = parts.tail.lastOption
-    UrlHash(baseUrl, hash)
-  }
-}
-
 object UrlRouter {
   def route[T: Routeable](page: Var[T])(implicit ctx: Ctx.Owner) {
     val routeable = implicitly[Routeable[T]]
 
     page.foreach { page =>
       val newHash = routeable.toRoute(page)
-      val current = UrlHash(window.location.href)
-
-      if (current.hash != Some(newHash))
-        window.location.href = current.copy(hash = Some(newHash)).toUrl
+      window.location.hash = newHash
     }
 
     window.onhashchange = { ev: HashChangeEvent =>
-      val current = UrlHash(ev.newURL)
-      val newPage = current.hash.flatMap(routeable.fromRoute.lift).getOrElse {
+      val current = Option(window.location.hash).filterNot(_.isEmpty).map(_.drop(1))
+      val newPage = current.flatMap(routeable.fromRoute.lift).getOrElse {
         val hash = routeable.toRoute(routeable.default)
-        window.location.href = current.copy(hash = Some(hash)).toUrl
+        window.location.hash = hash
         routeable.default
       }
 
