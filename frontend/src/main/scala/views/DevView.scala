90d46b136f3819f3fe71eacff17df8c45bb9ad21
Felix Dietze <felix@beerleader.net>
2017-04-07 17:27:47 +0200

    extract DevView

---
+++ frontend/src/main/scala/views/DevView.scala
@@ -0,0 +1,40 @@
+package wust.frontend.views
+
+import scalajs.concurrent.JSExecutionContext.Implicits.queue
+import org.scalajs.dom._
+
+import rx._
+import scalatags.rx.all._
+import scalatags.JsDom.all._
+import autowire._
+import boopickle.Default._
+
+import wust.frontend.{GlobalState, Client, DevOnly, ViewPage}
+import wust.graph._
+import graphview.GraphView
+
+object DevView {
+  def apply(state: GlobalState)(implicit ctx: Ctx.Owner) = {
+    Rx {
+      import scala.util.Random.{nextInt => rInt, nextString => rStr}
+      div(position.fixed, right := 0, top := 0, display.flex, flexDirection.column,
+        {
+          def addRandomPost() { Client.api.addPost(rStr(1 + rInt(20))).call() }
+          div(
+            button("create random post", onclick := { () => addRandomPost() }),
+            button("10", onclick := { () => for (_ <- 0 until 10) addRandomPost() }),
+            button("100", onclick := { () => for (_ <- 0 until 100) addRandomPost() })
+          )
+        },
+        {
+          val posts = scala.util.Random.shuffle(state.displayGraph().graph.postsById.keys.toSeq)
+          def deletePost(id: PostId) { Client.api.deletePost(id).call() }
+          div(
+            button("delete random post", onclick := { () => posts.take(1) foreach deletePost }),
+            button("10", onclick := { () => posts.take(10) foreach deletePost }),
+            button("100", onclick := { () => posts.take(100) foreach deletePost })
+          )
+        }).render
+    }
+  }
+}
--- frontend/src/main/scala/views/MainView.scala
+++ frontend/src/main/scala/views/MainView.scala
@@ -34,29 +34,7 @@ object MainView {
         padding := "5px", background := "#f7f7f7", borderTop := "1px solid #DDD")(
           AddPostForm(state)
         ),
-      DevOnly {
-        Rx {
-          import scala.util.Random.{nextInt => rInt, nextString => rStr}
-          div(position.fixed, right := 0, top := 0, display.flex, flexDirection.column,
-            {
-              def addRandomPost() { Client.api.addPost(rStr(1 + rInt(20))).call() }
-              div(
-                button("create random post", onclick := { () => addRandomPost() }),
-                button("10", onclick := { () => for (_ <- 0 until 10) addRandomPost() }),
-                button("100", onclick := { () => for (_ <- 0 until 100) addRandomPost() })
-              )
-            },
-            {
-              val posts = scala.util.Random.shuffle(state.displayGraph().graph.postsById.keys.toSeq)
-              def deletePost(id: PostId) { Client.api.deletePost(id).call() }
-              div(
-                button("delete random post", onclick := { () => posts.take(1) foreach deletePost }),
-                button("10", onclick := { () => posts.take(10) foreach deletePost }),
-                button("100", onclick := { () => posts.take(100) foreach deletePost })
-              )
-            }).render
-        }
-      }
+      DevOnly { DevView(state) }
     )
   }
 }
