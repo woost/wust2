b492ff36709ea17ec853e2ddcaf085f2f19d102d
Felix Dietze <felix@beerleader.net>
2017-04-07 17:50:40 +0200

    login as different users in DevView

--- frontend/src/main/scala/views/DevView.scala
+++ frontend/src/main/scala/views/DevView.scala
@@ -17,7 +17,16 @@ object DevView {
   def apply(state: GlobalState)(implicit ctx: Ctx.Owner) = {
     Rx {
       import scala.util.Random.{nextInt => rInt, nextString => rStr}
-      div(position.fixed, right := 0, top := 0, display.flex, flexDirection.column,
+      div(
+        position.fixed, right := 0, top := 0, display.flex, flexDirection.column,
+        {
+          val users = List("a", "b", "c", "d")
+          div(
+            "login: ",
+            //button("delete random post", onclick := { () => posts.take(1) foreach deletePost }),
+            users.map(u => button(u, onclick := { () => Client.auth.register(u, u).filter(_ == false).foreach { _ => Client.auth.login(u, u) } }))
+          )
+        },
         {
           def addRandomPost() { Client.api.addPost(rStr(1 + rInt(20))).call() }
           div(
@@ -34,7 +43,8 @@ object DevView {
             button("10", onclick := { () => posts.take(10) foreach deletePost }),
             button("100", onclick := { () => posts.take(100) foreach deletePost })
           )
-        }).render
+        }
+      ).render
     }
   }
 }
