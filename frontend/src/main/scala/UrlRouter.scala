71e9f2b0e0889357f4ad01f3020d235f42420e48
johannes karoff <johannes@karoff.net>
2017-04-24 22:14:32 +0200

    use history.pushState and initialize url hash variable

--- frontend/src/main/scala/UrlRouter.scala
+++ frontend/src/main/scala/UrlRouter.scala
@@ -5,16 +5,20 @@ import org.scalajs.dom.window
 import rx._
 
 object UrlRouter {
+  private def locationHash = Option(window.location.hash).map(_.drop(1)).filterNot(_.isEmpty)
+
   val variable: RxVar[Option[String], Option[String]] = {
-    val hash = RxVar[Option[String]](None)
+    val hash = RxVar[Option[String]](locationHash)
     hash.foreach { hash =>
-      val current = hash.getOrElse("")
-      if (window.location.hash != current)
-        window.location.hash = current
+      if (locationHash != hash) {
+        val current = hash.getOrElse("")
+        // instead of setting window.hash_=, pushState does not emit a hashchange event
+        window.history.pushState(null, null, s"#$current")
+      }
     }
 
-    window.onhashchange = { _: HashChangeEvent =>
-      val current = Option(window.location.hash).filterNot(_.isEmpty).map(_.drop(1))
+    window.onhashchange = { ev: HashChangeEvent =>
+      val current = locationHash
       if (hash.now != current)
         hash() = current
     }
