2b6275792b969ebc9a6bde506912879bf189f545
johannes karoff <johannes@karoff.net>
2017-05-08 22:55:52 +0200

    fix sql tests

--- dbMigration/tests/user.sql
+++ dbMigration/tests/user.sql
@@ -1,15 +1,11 @@
 BEGIN;
-SELECT plan(4);
+SELECT plan(3);
 
 SELECT col_is_pk('usergroup', 'id');
 SELECT col_not_null('usergroup', 'id');
 
-SELECT isnt_empty(
-    'select * from usergroup where id = 1'
-);
-
 SELECT is_empty(
-    'select * from usergroup where id <> 1'
+    'select * from usergroup'
 );
 
 SELECT * FROM finish();
