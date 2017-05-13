ed68948765889f9854c9385dc16cde42b41b10ef
johannes karoff <johannes@karoff.net>
2017-05-13 12:49:15 +0200

    print config on startup

--- backend/src/main/scala/Main.scala
+++ backend/src/main/scala/Main.scala
@@ -3,6 +3,8 @@ package wust.backend
 import scala.concurrent.ExecutionContext.Implicits.global
 
 object Main extends App {
+  scribe.info(s"Starting wust with Config: ${config.Config}")
+
   Server.run(8080) foreach { binding =>
     scribe.info(s"Server online at ${binding.localAddress}")
   }
