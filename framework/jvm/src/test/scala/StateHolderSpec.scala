c3d808cd6327423ce576a27bc5fc79986be21e63
johannes karoff <johannes@karoff.net>
2017-05-17 21:44:56 +0200

    chain stateholder test

--- framework/jvm/src/test/scala/StateHolderSpec.scala
+++ framework/jvm/src/test/scala/StateHolderSpec.scala
@@ -6,8 +6,8 @@ import scala.concurrent.Future
 import scala.collection.mutable
 
 class StateHolderSpec extends AsyncFreeSpec with MustMatchers {
-  private def newStateHolder[T](state: String) = {
-    new StateHolder[String, Int](Future.successful(state))
+  private def newStateHolder[T](state: T) = {
+    new StateHolder[T, Int](Future.successful(state))
   }
 
   "result to requestResponse" in {
@@ -103,6 +103,43 @@ class StateHolderSpec extends AsyncFreeSpec with MustMatchers {
     holder.events.map(_ must contain theSameElementsAs Set(666))
     res.map { res =>
       res mustEqual 2
+    }
+  }
+
+  "chain stateholder state/events" in {
+    val initialState = ""
+    val holder = newStateHolder(initialState)
+    import holder._
+
+    val nextState = "new"
+    val res: Future[String] = { (state: String) =>
+      val newState = Future.successful(nextState)
+      val response = Future.successful(respondWithEvents("response2", 777))
+      replaceState(newState, response)
+    }
+
+    holder.state.map(_ mustEqual nextState)
+    holder.events.map(_ must contain theSameElementsAs Set(777))
+    res.map { res =>
+      res mustEqual "response2"
+    }
+
+    val betweenState = holder.state
+
+    {
+    val holder = newStateHolder(betweenState)
+    val nextState = "new2"
+    val res: Future[String] = { (state: String) =>
+      val newState = Future.successful(nextState)
+      val response = Future.successful(respondWithEvents("response", 666))
+      replaceState(newState, response)
+    }
+
+    holder.state.map(_ mustEqual nextState)
+    holder.events.map(_ must contain theSameElementsAs Set(666))
+    res.map { res =>
+      res mustEqual "response"
+    }
     }
   }
 
