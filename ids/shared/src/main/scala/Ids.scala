1a829b3f7d2f502534e7c04a7e7f8be78f0cd91f
johannes karoff <johannes@karoff.net>
2017-05-27 13:31:34 +0200

    make tagged id types non-generic

--- ids/shared/src/main/scala/Ids.scala
+++ ids/shared/src/main/scala/Ids.scala
@@ -11,13 +11,13 @@ package object ids {
   // unboxed types with scalaz: http://eed3si9n.com/learning-scalaz/Tagged+type.html
   sealed trait PostIdType
   type PostId = IdType @@ PostIdType
-  implicit def PostId[T](id: T): T @@ PostIdType = Tag[T, PostIdType](id)
+  implicit def PostId(id: IdType): PostId = Tag[IdType, PostIdType](id)
 
   sealed trait GroupIdType
   type GroupId = IdType @@ GroupIdType
-  implicit def GroupId[T](id: T): T @@ GroupIdType = Tag[T, GroupIdType](id)
+  implicit def GroupId(id: IdType): GroupId = Tag[IdType, GroupIdType](id)
 
   sealed trait UserIdType
   type UserId = IdType @@ UserIdType
-  implicit def UserId[T](id: T): T @@ UserIdType = Tag[T, UserIdType](id)
+  implicit def UserId(id: IdType): UserId = Tag[IdType, UserIdType](id)
 }
