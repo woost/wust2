67b7025fdd470a9c94395c9d2f2f587b23228179
johannes karoff <johannes@karoff.net>
2017-06-10 14:23:05 +0200

    format scribe logger in backend

--- backend/src/main/scala/Main.scala
+++ backend/src/main/scala/Main.scala
@@ -1,8 +1,21 @@
 package wust.backend
 
 import config.Config
+import scribe._
+import scribe.formatter.FormatterBuilder
+import scribe.writer.ConsoleWriter
 
 object Main extends App {
+  val formatter = FormatterBuilder()
+    .date()
+    .string(" ")
+    .levelPaddedRight
+    .string(": ")
+    .message.newLine
+
+  Logger.root.clearHandlers()
+  Logger.root.addHandler(LogHandler(Level.Info, formatter, ConsoleWriter))
+
   scribe.info(s"Starting wust with Config: $Config")
   Server.run(8080)
 }
