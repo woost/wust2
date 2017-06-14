b1ce02bfcc3ce0fa3b9c014fd7235d6b0fa81cbb
Felix Dietze <felix@beerleader.net>
2017-06-14 17:15:27 +0200

    invite links with default lock to group

--- frontend/src/main/scala/views/MainView.scala
+++ frontend/src/main/scala/views/MainView.scala
@@ -151,7 +151,7 @@ object MainView {
         case Some(groupId) =>
           Client.api.getGroupInviteToken(groupId).call().foreach {
             //TODO: we should not construct absolute paths here
-            case Some(token) => inviteLink() = Some(s"${location.href.split("#").head}#${ViewConfig.toHash(state.viewConfig())}&invite=$token")
+            case Some(token) => inviteLink() = Some(s"${location.href.split("#").head}#${ViewConfig.toHash(state.viewConfig())}&invite=$token&locktogroup=true")
             case None        =>
           }
         case None => inviteLink() = None
