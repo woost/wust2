bf2218af2e15783947266934a1dacbb4cb63a395
johannes karoff <johannes@karoff.net>
2017-11-05 18:41:33 +0100

    wip

--- frontend/src/main/scala/GraphPersistence.scala
+++ frontend/src/main/scala/GraphPersistence.scala
@@ -48,7 +48,7 @@ class GraphPersistence(state: GlobalState) {
     enrichedChanges.merge(pureChanges).filter(_.nonEmpty)
   }
 
-  storage.graphChanges <-- localChanges
+  // storage.graphChanges <-- localChanges
 
   private val bufferedChanges = localChanges.bufferUnless(state.syncMode.map(_ == SyncMode.Live))
 
