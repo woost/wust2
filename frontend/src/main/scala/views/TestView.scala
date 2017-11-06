d6d73b0026e713db5162d8ae1999b7f11b180468
Felix Dietze <felix@beerleader.net>
2017-11-06 11:43:03 +0100

    port TestView

--- frontend/src/main/scala/views/TestView.scala
+++ frontend/src/main/scala/views/TestView.scala
@@ -6,44 +6,46 @@ import wust.graph._
 import org.scalajs.dom.{window, document, console}
 import scala.scalajs.js
 
-import outwatch.dom._
-import wust.util.outwatchHelpers._
 
 import cats.instances.list._
 import cats.syntax.traverse._
+
 import rxscalajs.Observable
+import outwatch.dom._
+import outwatch.Sink
+import wust.util.outwatchHelpers._
 
-// object TestView {
-//   def postItem(post: Post, graphSelectionHandler: Handler[GraphSelection]) = {
-//     div(
-//       stl("minHeight") := "12px",
-//       stl("border") := "solid 1px",
-//       stl("cursor") := "pointer",
-//       click(GraphSelection.Union(Set(post.id))) --> graphSelectionHandler,
-//       post.title
-//     )
-//   }
+object TestView {
+  def apply(state: GlobalState) = {
+    import state._
+    val graph = displayGraphWithoutParents.map(_.graph)
+    graph(g => println(s"TestView: got graph update"))
+    component(graph, graphSelection)
+  }
 
-//   def sortedPostItems(state: GlobalState, graphSelectionHandler: Handler[GraphSelection])(implicit ctx: Ctx.Owner): Observable[Seq[VNode]] = state.displayGraphWithoutParents.toObservable.map { dg =>
-//     val graph = dg.graph
-//     val sortedPosts = HierarchicalTopologicalSort(graph.postIds, successors = graph.successors, children = graph.children)
+  def component(graph:Observable[Graph], graphSelection:Sink[GraphSelection]) = {
+    div(
+      stl("padding") := "20px",
+      children <-- sortedPostItems(graph, graphSelection)
+    ).render
+  }
 
-//     sortedPosts.map { postId =>
-//       val post = graph.postsById(postId)
-//       postItem(post, graphSelectionHandler)
-//     }
-//   }
+  def sortedPostItems(graph:Observable[Graph], graphSelection:Sink[GraphSelection]): Observable[Seq[VNode]] = graph.map { graph =>
+    val sortedPosts = HierarchicalTopologicalSort(graph.postIds, successors = graph.successors, children = graph.children)
 
-//   def apply(state: GlobalState)(implicit ctx: Ctx.Owner) = {
-//     val ints = createHandler[Int](): Handler[Int]
-//     val incs = ints.isomorphic[Int](_ + 1, _ - 1)
-//     ints(i => println(s"ints: $i"))
-//     incs(i => println(s"incs: $i"))
-//     div(
-//       input(placeholder := "ints", value := 2, value <-- ints, inputString(_.toInt) --> ints),
-//       input(placeholder := "incs", value := 3, value <-- incs, inputString(_.toInt) --> incs),
-//       stl("padding") := "20px",
-//       children <-- sortedPostItems(state, state.graphSelection)
-//     ).render
-//   }
-// }
+    sortedPosts.map { postId =>
+      val post = graph.postsById(postId)
+      postItem(post, graphSelection)
+    }
+  }
+
+  def postItem(post: Post, graphSelection: Sink[GraphSelection]) = {
+    div(
+      post.title,
+      stl("minHeight") := "12px",
+      stl("border") := "solid 1px",
+      stl("cursor") := "pointer",
+      click(GraphSelection.Union(Set(post.id))) --> graphSelection
+    )
+  }
+}
