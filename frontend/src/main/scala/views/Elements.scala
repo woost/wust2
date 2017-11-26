d6baa01e4b13638d1f130a3a5a47d76dfc8255b4
johannes karoff <johannes@karoff.net>
2017-11-26 16:18:39 +0100

    wip

--- frontend/src/main/scala/views/Elements.scala
+++ frontend/src/main/scala/views/Elements.scala
@@ -1,14 +1,15 @@
 package wust.frontend.views
 
-import org.scalajs.dom
+import org.scalajs.dom.Event
 import org.scalajs.dom.ext.KeyCode
+import org.scalajs.dom.raw.Element
 import outwatch.Sink
 import outwatch.dom._
 import wust.util.outwatchHelpers._
 
 //TODO: merge with util.Tags
 object Elements {
-  def scrollToBottom(elem: dom.Element):Unit = {
+  def scrollToBottom(elem: Element):Unit = {
     //TODO: scrollHeight is not yet available in jsdom tests: https://github.com/tmpvar/jsdom/issues/1013
     try {
       elem.scrollTop = elem.scrollHeight
@@ -20,46 +21,39 @@ object Elements {
     // - textarea: enter emits keyCode for Enter
     // - input: Enter triggers submit
 
-    // val userInput = Handler.create[String]().unsafeRunSync()
-    val setInputValue = Handler.create[String]().unsafeRunSync()
-    // val clearHandler = setInputValue.map(_ => "a")
-    // val insertFieldValue = userInput.merge(clearHandler)
+    val userInput = createStringHandler().unsafeRunSync()
+    val setInputValue = createStringHandler().unsafeRunSync()
+    val clearHandler = setInputValue.map(_ => "")//scala.util.Random.nextInt.toString)
+    val insertFieldValue = userInput.merge(clearHandler)
 
-    // val submitHandler = Handler.create[dom.Event]().unsafeRunSync()
-    // val enterKeyHandler = Handler.create[dom.KeyboardEvent]().unsafeRunSync()
-    // val actionHandler = submitHandler
-    //   .merge(enterKeyHandler)
-    //   // .replaceWithLatestFrom(insertFieldValue)
-    //   .withLatestFrom(insertFieldValue).map(_._2)
-    //   .filter(_.nonEmpty)
+    val submitHandler = createHandler[Event]().unsafeRunSync()
+    val enterKeyHandler = createKeyboardHandler().unsafeRunSync()
+    val actionHandler = submitHandler
+      .merge(enterKeyHandler)
+      .replaceWithLatestFrom(insertFieldValue)
+      .filter(_.nonEmpty)
 
-    // (actionSink <-- actionHandler).unsafeRunSync()
-    // (setInputValue <-- actionHandler.delay(1000).map(_ => "")).unsafeRunSync() //TODO: only trigger clearHandler
-    // (setInputValue <-- Observable.interval(1000).map(_.toString)).unsafeRunSync()
-    // enterKeyHandler( event => event.preventDefault() )
-    // submitHandler( event => event.preventDefault() )
-        // insertFieldValue { text => println(s"Insertfield: '${text}'") }
-        // enterKeyHandler { _ => println(s"EnterKeyHandler") }
-        // submitHandler { _ => println(s"SumbitHandler") }
-        // actionHandler { text => println(s"ActionHandler: ${text}") }
+    (actionSink <-- actionHandler).unsafeRunSync()
+    (setInputValue <-- actionHandler.delay(1000)).unsafeRunSync() //TODO: only trigger clearHandler
+    enterKeyHandler( event => event.preventDefault() )
+    submitHandler( event => event.preventDefault() )
+    //     insertFieldValue { text => println(s"Insertfield: '${text}'") }
+    //     enterKeyHandler { _ => println(s"EnterKeyHandler") }
+    //     submitHandler { _ => println(s"SumbitHandler") }
+    //     actionHandler { text => println(s"ActionHandler: ${text}") }
 
-    setInputValue.debug("setInputValue")
     form(
-      // input(tpe := "checkbox", checked <-- rxscalajs.Observable.interval(5000).map(_ % 2 == 0)),//clearHandler.map(_ => false)),
-      button(tpe := "button", "clear", onClick("A") --> setInputValue),
-      textArea(
-        onKeyDown.map(_.toString) --> setInputValue
-      ),
-      textArea(
-        tpe := "text", //TODO: dom-types attribute enums
+      textarea(
         placeholder := "Create new post. Press Enter to submit.",
+        // data.bla <-- Observable.interval(2000).map(_.toString),
+        // div(child <-- clearHandler),
         width := "100%",
-        // onInputString --> userInput, //TODO: outwatch: this is not triggered when setting the value with `value <-- observable`
-        value <-- setInputValue,
-        onKeyDown.filter(e => e.keyCode == KeyCode.Enter && !e.shiftKey).map(_.toString) --> setInputValue
+        inputString --> userInput, //TODO: outwatch: this is not triggered when setting the value with `value <-- observable`
+        value <-- clearHandler,
+        keydown.filter(e => e.keyCode == KeyCode.Enter && !e.shiftKey) --> enterKeyHandler
       ),
       input(tpe := "submit", value := "insert"),
-      // onSubmit --> submitHandler
+      onSubmit --> submitHandler
     )
   }
 
