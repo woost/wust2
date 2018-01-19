02e308e8848269b00c08cf533e1c25a7fd6338bc
Julius Elias <GRBurst@protonmail.com>
2018-01-19 21:56:05 +0100

    Add some task heuristics
    - Newest: may be usefull to directly restructure new messages
    - GaussTime: Choose 1 post and the second based on a gauss distribution
        over the creation time of all posts

--- frontend/src/main/scala/views/RestructuringHeuristics.scala
+++ frontend/src/main/scala/views/RestructuringHeuristics.scala
@@ -1,6 +1,8 @@
 package wust.frontend.views
-import wust.frontend.views.{RestructuringTask} 
-import wust.graph.{Post}
+
+import wust.graph.Post
+
+import scala.util.Random
 
 object Heuristic {
   type PostHeuristic = (Set[Post], Int) => Set[Post]
@@ -16,26 +18,43 @@ case object ChooseTaskHeuristic {
 }
 
 case object ChoosePostHeuristic {
-  def random(posts: Set[Post], num: Int = 1): Set[Post] = {
+
+  private[this] def wrapHeuristic(f: List[Post] => List[Post], posts: Set[Post], num: Int): Set[Post] = {
     assert(num <= posts.size, "Cannot pick more elements than there are")
-    assert(posts.nonEmpty, "Post must not be empty")
+    if(posts.isEmpty) return Set.empty[Post]
 
-    val choice = scala.util.Random.shuffle(posts.toList).take(num).toSet
-
-    assert(choice.nonEmpty, "At least one element must be chosen")
-
-    choice
+    val choice = f(posts.toList)
+    choice.take(num).toSet
   }
-//  def newest(posts: List[Post], num: Int = 1): List[Post] = {
-//    val post = posts.head
-//    if(num > 1) List(post) ++ random(posts, num - 1)
-//    else List(post)
-//  }
-//  def gaussTime(posts: List[Post], num: Int = 1): List[Post] = {
-//    val post = posts(scala.util.Random.nextInt(posts.size))
-//    if(num > 1) List(post) ++ random(posts, num - 1)
-//    else List(post)
-//  }
+  def random(posts: Set[Post], num: Int = 1): Set[Post] = {
+    def _random(p: List[Post]) = scala.util.Random.shuffle(p)
+    wrapHeuristic(_random, posts, num)
+  }
+
+  def newest(posts: Set[Post], num: Int = 1): Set[Post] = {
+    def _newest(p: List[Post]) = p.sortWith((p1, p2) => p1.created.isBefore(p2.created))
+    wrapHeuristic(_newest, posts, num)
+  }
+
+  def gaussTime(posts: Set[Post], num: Int = 2): Set[Post] = {
+    def _gaussTime(posts: List[Post]) = {
+      def clamp(value: Int, minValue: Int, maxValue: Int) = math.min(math.max(value, minValue), maxValue)
+      val base = scala.util.Random.shuffle(posts).head
+
+      val sortByTime: List[Post] = posts.sortWith((p1, p2) => p1.created.isBefore(p2.created))
+
+      val baseIndex = sortByTime.indexOf(base)
+      val pseudoStdDevPosts = 34.0 * sortByTime.length / 100
+
+      def gauss = math.round(scala.util.Random.nextGaussian * pseudoStdDevPosts).toInt
+      val gaussPosts = for(_ <- 0 to num) yield sortByTime(clamp(baseIndex + gauss, 0, sortByTime.length - 1))
+
+      gaussPosts.toList
+    }
+    wrapHeuristic(_gaussTime, posts, num)
+
+
+  }
 
   def defaultHeuristic: Heuristic.PostHeuristic = random
 }
