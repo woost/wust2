522188fa2b63dad9dfb602cfc62d0327a34cb776
Julius Elias <GRBurst@protonmail.com>
2018-02-01 16:37:56 +0100

    Run zsh in remotedev to cleanup remote processes

--- remotedev.sh
+++ remotedev.sh
@@ -7,7 +7,7 @@ DEVPORT=$(shuf -i 40000-41000 -n 1)
 BACKEND=$(shuf -i 50000-51000 -n 1)
 REMOTETMP=$(mktemp)
 
-rsync -aP --delete $LOCALDIR/ fff:$REMOTETMP/ --exclude-from=$LOCALDIR/.ignore || exit 1
+rsync -aP --delete $LOCALDIR/ $REMOTEHOST:$REMOTETMP/ --exclude-from=$LOCALDIR/.ignore || exit 1
 
 lsyncd =(cat <<EOF
 settings {
@@ -28,7 +28,7 @@ EOF
 LSYNCDPID=$!
 echo $LSYNCDPID
 
-ssh -tL 12345:localhost:${DEVPORT} ${REMOTEHOST} "mkdir -p $REMOTETMP; cd $REMOTETMP; nix-shell --run \"WUST_BACKEND_PORT=$BACKEND WUST_DEVSERVER_PORT=$DEVPORT WUST_DEVSERVER_COMPRESS=true ./start sbt\""
+ssh -tL 12345:localhost:${DEVPORT} ${REMOTEHOST} "mkdir -p $REMOTETMP; cd $REMOTETMP; nix-shell --run \"zsh -c \\\"WUST_BACKEND_PORT=$BACKEND WUST_DEVSERVER_PORT=$DEVPORT WUST_DEVSERVER_COMPRESS=true ./start sbt\\\"\""
 
 ssh ${REMOTEHOST} "rm -rf $REMOTETMP"
 
