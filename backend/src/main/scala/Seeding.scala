049d42a9b5ffe828ad13043989597355917a3e71
Julius Elias <GRBurst@protonmail.com>
2018-02-02 18:01:16 +0100

    Add hierarchical tags on import (gitter + github)

--- backend/src/main/scala/Seeding.scala
+++ backend/src/main/scala/Seeding.scala
@@ -9,6 +9,13 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scalaj.http.HttpResponse
 
+object Constants {
+  //TODO
+  val githubId: PostId = "wust-github"
+  val issueTagId: PostId = "github-issue"
+  val commentTagId: PostId = "github-comment"
+  val gitterId: PostId = "wust-gitter"
+}
 object GitHubImporter {
   import github4s.Github
   import github4s.Github._
@@ -16,7 +23,7 @@ object GitHubImporter {
   import github4s.free.domain.{Comment, Issue, User => GHUser}
   import github4s.jvm.Implicits._
 
-  private val gitAccessToken = sys.env.get("GITHUB4S_ACCESS_TOKEN")
+  private val gitAccessToken = sys.env.get("WUST_GITHUB_TOKEN")
 
   def urlExtractor(url: String): (String, String, Option[Int]) = {
 
@@ -46,24 +53,24 @@ object GitHubImporter {
     def getSingleIssue(number: Int): Future[List[Issue]] =
       Github(gitAccessToken).issues.getIssue(owner, repo, number)
         .execFuture[HttpResponse[String]]()
-        .map( response => response match {
+        .map {
           case Right(GHResult(result, _, _)) => List(result)
           case _ => {
             println("Error getting Issue")
             List.empty[Issue]
           }
-        })
+        }
 
     def getIssueList: Future[List[Issue]] =
       Github(gitAccessToken).issues.listIssues(owner, repo)
         .execFuture[HttpResponse[String]]()
-        .map ( response => response match {
+        .map {
           case Right(GHResult(result, _, _)) => result
           case _ => {
             println("Error getting List of Issues")
             List.empty[Issue]
           }
-        })
+        }
 
     val issueList = issueNumber match {
       case Some(number) => getSingleIssue(number)
@@ -98,22 +105,41 @@ object GitHubImporter {
         }) //TODO: create this user
         val tempUserId = user.id
 
+        // Ensure posts
+        val _github = Post(Constants.githubId, "wust-github", tempUserId)
+        val _issue = Post(Constants.issueTagId, "wust-github-issue", tempUserId)
+        val _comment = Post(Constants.commentTagId, "wust-github-comment", tempUserId)
+        val _github_issue = Connection(_issue.id, Label.parent, _github.id)
+        val _github_comment = Connection(_comment.id, Label.parent, _github.id)
 
-        val title = Post(PostId.fresh, s"#${issue.number} ${issue.title}", tempUserId, parseTime(issue.created_at), parseTime(issue.updated_at))
-        val desc = Post(PostId.fresh, issue.body, tempUserId, parseTime(issue.created_at), parseTime(issue.updated_at))
-        val cont = Connection(desc.id, Label.parent, title.id)
-        val conn = Connection(desc.id, "describes", title.id)
+        // Issue posts and connections
+        val issueTitle = Post(PostId.fresh, s"#${issue.number} ${issue.title}", tempUserId, parseTime(issue.created_at), parseTime(issue.updated_at))
+        val titleGitTag = Connection(issueTitle.id, Label.parent, _github.id)
+        val titleIssueTag = Connection(issueTitle.id, Label.parent, _issue.id)
 
-        val comments: List[(Post, Connection)] = commentsList.map(comment => {
+        val desc = if(issue.body.nonEmpty) {
+          val issueDesc = Post(PostId.fresh, issue.body, tempUserId, parseTime(issue.created_at), parseTime(issue.updated_at))
+          val conn = Connection(issueDesc.id, "describes", issueTitle.id)
+          val cont = Connection(issueDesc.id, Label.parent, issueTitle.id)
+          (Set(issueDesc), Set(conn, cont))
+        } else {
+          (Set.empty[Post], Set.empty[Connection])
+        }
+
+        val issuePosts = Set[Post](_github, _issue, _comment, issueTitle) ++ desc._1
+        val issueConn = Set[Connection](_github_issue, _github_comment, titleGitTag, titleIssueTag) ++ desc._2
+
+        // Comments
+        val comments: List[(Post, Set[Connection])] = commentsList.map(comment => {
           val cpost = Post(PostId.fresh, comment.body, tempUserId, parseTime(comment.created_at), parseTime(comment.updated_at))
-          val cconn = Connection(cpost.id, Label.parent, title.id)
+          val cconn = Set(Connection(cpost.id, Label.parent, issueTitle.id), Connection(issueTitle.id, Label.parent, _comment.id))
           (cpost, cconn)
         })
 
         val (commentPosts, commentConnections) = comments.unzip
 
-        val posts = Set[Post](title, desc) ++ commentPosts.toSet
-        val connections = Set[Connection](conn, cont) ++ commentConnections.toSet
+        val posts = issuePosts ++ commentPosts.toSet
+        val connections = issueConn ++ commentConnections.toSet.flatten
         (posts, connections)
 
       }).toSet)
@@ -135,7 +161,7 @@ object GitterImporter {
   private val gitterAccessToken = sys.env.getOrElse("WUST_GITTER_TOKEN", "")
 
   def getRoomMessages(url: String, user: User): Future[(Set[Post], Set[Connection])] = {
-    val _url = url.stripLineEnd.stripMargin.trim.
+    val _uri = url.stripLineEnd.stripMargin.trim.
       stripPrefix("https://").
       stripPrefix("http://").
       stripPrefix("gitter.im/").
@@ -144,14 +170,17 @@ object GitterImporter {
     val tempUserId = user.id
     val client: SyncGitterApiClient = new SyncGitterApiClient.Builder().withAccountToken(gitterAccessToken).build()
 
-    val discussion = Post(PostId.fresh, _url, tempUserId)
+    // Ensure gitter post
+    val _gitter = Post(Constants.gitterId, "wust-gitter", tempUserId)
+
+    val discussion = Post(PostId.fresh, _uri, tempUserId)
+    val discussionTag = Connection(discussion.id, Label.parent, _gitter.id)
     val postsAndConnection = for {
-      roomId <- Future { client.getRoomIdByUri(_url).id }
+      roomId <- Future { client.getRoomIdByUri(_uri).id }
       roomMessages <- Future { client.getRoomMessages(roomId).asScala.toList }
     } yield {
       roomMessages.map { message =>
         //TODO what about this userid?
-        // val post = Post(PostId.fresh, message.text, message.fromUser.id)
         val post = Post(PostId.fresh, message.text, tempUserId)
         val conn = Connection(post.id, Label.parent, discussion.id)
         (Set(post), Set(conn))
@@ -160,7 +189,7 @@ object GitterImporter {
 
     postsAndConnection.map(zipped => {
       val (posts, conns) = zipped.unzip
-      (posts.flatten + discussion, conns.flatten)
+      (posts.flatten + _gitter + discussion, conns.flatten + discussionTag)
     })
   }
 }
