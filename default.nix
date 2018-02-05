69a46b62370f95b48508d77e17e56698741dc095
Julius Elias <GRBurst@protonmail.com>
2018-02-05 23:06:54 +0100

    Add ngrok to default.nix to forward github webhook

--- default.nix
+++ default.nix
@@ -9,6 +9,7 @@ in
       scala
       sbt
       docker docker_compose
+      ngrok # github app -> webhooks to localhost
       nodejs-8_x yarn
       phantomjs
       gnumake gcc # required for some weird npm things
