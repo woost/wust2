99596ef5b2b0bbf2c247b4c1ef2c5eeacc2809c6
Julius Elias <GRBurst@protonmail.com>
2018-02-08 20:22:15 +0100

    Parse github token to option

--- githubApp/src/main/scala/Config.scala
+++ githubApp/src/main/scala/Config.scala
@@ -4,7 +4,7 @@ import derive.derive
 
 @derive((host, user) => toString)
 case class WustConfig(host: String, port: String, user: String, password: String)
-case class GithubConfig(host: String, port: Int, path: String, accessToken: String)
+case class GithubConfig(host: String, port: Int, path: String, accessToken: Option[String])
 case class Config(wust: WustConfig, github: GithubConfig)
 
 object Config {
