5e4ef2fdb3f90debfedb6d22fd15b5bcc0b648bb
Felix Dietze <felix.dietze@rwth-aachen.de>
2018-02-09 16:48:04 +0100

    Simplify nixos android usage

--- default.nix
+++ default.nix
@@ -20,11 +20,13 @@ in
     '';
 
     shellHook=''
-    export ANDROID_HOME=~/Android/Sdk
-    cp -a /nix/store/6k288pvgs447878qnpiad87mwqidk6bs-android-sdk-25.2.5/bin/* ~/Android/Sdk/build-tools/27.0.3
     echo --- Welcome to woost! ---
     echo "Make sure you have the docker service running and added your user to the group 'docker'."
     echo Now run ./start sbt dev
     echo Then point your browser to http://localhost:12345
+
+    mkdir -p ~/.android/sbt/sdk/{platform-tools,build-tools/27.0.3}/
+    cp -a ${pkgs.androidsdk}/bin/* ~/.android/sbt/sdk/platform-tools/
+    cp -a ${pkgs.androidsdk}/bin/* ~/.android/sbt/sdk/build-tools/27.0.3/
     '';
   }
