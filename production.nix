88123778233bfe7a5c0df545b373e8244622a985
Julius Elias <GRBurst@protonmail.com>
2018-02-14 23:47:43 +0100

    Add production.nix

---
+++ production.nix
@@ -0,0 +1,27 @@
+{ }:
+
+let
+  pkgs = import <nixpkgs> { };
+in
+  pkgs.stdenv.mkDerivation {
+    name = "Woost";
+    buildInputs = with pkgs; [
+      scala
+      sbt
+      docker docker_compose
+      ngrok # github app -> webhooks to localhost
+      nodejs-8_x yarn
+      phantomjs
+      gnumake gcc # required for some weird npm things
+    ];
+
+    installPhase= ''
+    '';
+
+    shellHook=''
+    echo --- Welcome to woost! ---
+    echo "Make sure you have the docker service running and added your user to the group 'docker'."
+    echo Now run ./start sbt dev
+    echo Then point your browser to http://localhost:12345
+    '';
+  }
