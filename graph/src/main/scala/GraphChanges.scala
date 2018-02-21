cb735cff43bfe4ce5f583e93b5de44ddfa731177
Felix Dietze <felix.dietze@rwth-aachen.de>
2018-02-21 23:25:01 +0100

    Useful Factories for GraphChanges

--- graph/src/main/scala/GraphChanges.scala
+++ graph/src/main/scala/GraphChanges.scala
@@ -68,4 +68,22 @@ object GraphChanges {
     delConnections:  Iterable[Connection]  = Set.empty,
     delOwnerships:   Iterable[Ownership]   = Set.empty
   ) = GraphChanges(addPosts.toSet, addConnections.toSet, addOwnerships.toSet, updatePosts.toSet, delPosts.toSet, delConnections.toSet, delOwnerships.toSet)
+
+  def addPost(content:String, author:UserId) = GraphChanges(addPosts = Set(Post(content, author)))
+
+  def connect(source:PostId, label:Label, target:PostId) = GraphChanges(addConnections = Set(Connection(source, label, target)))
+
+  def moveInto(graph:Graph, subject:PostId, target:PostId) = {
+    val newContainments = Set(Connection(subject, Label.parent, target))
+    val removeContainments:Set[Connection] = if (graph.ancestors(target).toSet contains subject) { // creating cycle
+      Set.empty // remove nothing, because in cycle
+    } else { // no cycle
+      (graph.parents(subject) map (Connection(subject, Label.parent, _))) - newContainments.head
+    }
+    GraphChanges(addConnections = newContainments, delConnections = removeContainments)
+  }
+
+  def tagWith(graph:Graph, subject:PostId, tag:PostId) = {
+    GraphChanges.connect(subject, Label.parent, tag)
+  }
 }
