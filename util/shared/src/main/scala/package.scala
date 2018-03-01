4548d91cdb126c1e5cc2626360c99d136b0b0a00
Felix Dietze <felix.dietze@rwth-aachen.de>
2018-03-01 12:19:23 +0100

    Future.transform(Try => Try) polyfill for 2.11

--- util/shared/src/main/scala/package.scala
+++ util/shared/src/main/scala/package.scala
@@ -1,8 +1,9 @@
 package wust
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{ExecutionContext, Future, Promise}
 import scala.util.control.NonFatal
 import scala.util.{Failure, Success}
+import scala.util.Try
 
 package object util {
   implicit class Pipe[T](val v: T) extends AnyVal {
@@ -10,15 +11,15 @@ package object util {
     def sideEffect(f: T => Any): T = { f(v); v }
   }
 
-  implicit class RichFuture[A](val fut: Future[A]) extends AnyVal {
-    def recoverValueWithoutLog(a: A)(implicit ec: ExecutionContext) = fut.recover { case NonFatal(_) => a }
-    def recoverValue(a: A)(implicit ec: ExecutionContext, name: sourcecode.FullName, line: sourcecode.Line) = fut.recover {
+  implicit class RichFuture[T](val fut: Future[T]) extends AnyVal {
+    def recoverValueWithoutLog(a: T)(implicit ec: ExecutionContext) = fut.recover { case NonFatal(_) => a }
+    def recoverValue(a: T)(implicit ec: ExecutionContext, name: sourcecode.FullName, line: sourcecode.Line) = fut.recover {
       case NonFatal(e) =>
         scribe.error(s"${name.value}:${line}")
         scribe.error(e)
         a
     }
-    def log(customMessage: String = null)(implicit ec: ExecutionContext, name: sourcecode.FullName, line: sourcecode.Line): Future[A] = {
+    def log(customMessage: String = null)(implicit ec: ExecutionContext, name: sourcecode.FullName, line: sourcecode.Line): Future[T] = {
       val message = Option(customMessage).fold("")(" - " + _)
       fut.onComplete {
         case Success(res) =>
@@ -29,6 +30,14 @@ package object util {
           scribe.error(e)
       }
       fut
+    }
+
+    // Polyfill for 2.11
+    // from https://github.com/scala/scala/blob/79e5101738d5b4675ab4194d6c18d443a496ef7c/src/library/scala/concurrent/impl/Promise.scala#L27
+    def transform[S](f: Try[T] => Try[S])(implicit executor: ExecutionContext): Future[S] = {
+      val p = Promise[S]()
+      fut.onComplete { result => p.complete(try f(result) catch { case NonFatal(t) => Failure(t) }) }
+      p.future
     }
   }
 }
