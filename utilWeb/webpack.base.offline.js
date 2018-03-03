905c9762d3dc1ffbd3f4810c98bce9df4e84386e
johannes karoff <johannes@karoff.net>
2018-03-03 20:23:59 +0100

    offline support with service worker

--- utilWeb/webpack.base.offline.js
+++ utilWeb/webpack.base.offline.js
@@ -3,6 +3,10 @@ const OfflinePlugin = require("offline-plugin");
 module.exports = {
     plugins: [
         new OfflinePlugin({
+            ServiceWorker: {
+                minify: true
+            },
+            AppCache: false
         })
     ]
 }
