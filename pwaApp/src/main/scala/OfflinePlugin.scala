0dbe05d73121e07572ce7d7f69425268a02901d4
johannes karoff <johannes@karoff.net>
2018-03-04 22:48:03 +0100

    service worker events in app

--- pwaApp/src/main/scala/OfflinePlugin.scala
+++ pwaApp/src/main/scala/OfflinePlugin.scala
@@ -6,5 +6,14 @@ import scala.scalajs.js.annotation.JSImport
 @js.native
 @JSImport("offline-plugin/runtime", JSImport.Default)
 object OfflinePlugin extends js.Object {
-  def install(): Unit = js.native
+  def install(config: OfflinePluginConfig = ???): Unit = js.native
+  def applyUpdate(): Unit = js.native
+  def update(): Unit = js.native
+}
+
+trait OfflinePluginConfig extends js.Object {
+  def onUpdating(): Unit
+  def onUpdateReady(): Unit
+  def onUpdated(): Unit
+  def onUpdateFailed(): Unit
 }
