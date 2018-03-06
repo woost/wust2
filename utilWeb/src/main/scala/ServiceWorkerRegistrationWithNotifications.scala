6f266fb7a85ea7f89ef08ebf9e72f8c8f1db4b44
johannes karoff <johannes@karoff.net>
2018-03-07 00:01:54 +0100

    notifications in pwa

---
+++ utilWeb/src/main/scala/ServiceWorkerRegistrationWithNotifications.scala
@@ -0,0 +1,16 @@
+package wust.utilWeb
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSImport
+import org.scalajs.dom.experimental.{ Notification, NotificationOptions }
+import org.scalajs.dom.experimental.serviceworkers.ServiceWorkerRegistration
+import org.scalajs.dom
+
+//TODO: pr scala-js-dom
+trait ServiceWorkerRegistrationWithNotifications extends ServiceWorkerRegistration {
+  def getNotifications(options: GetNotificationOptions = ???): js.Promise[js.Array[Notification]] = js.native
+  //TODO: NotificationEvent
+  def showNotification(title: String, options: NotificationOptions = ???): js.Promise[dom.Event] = js.native
+}
+
+class GetNotificationOptions(val tag: String) extends js.Object
