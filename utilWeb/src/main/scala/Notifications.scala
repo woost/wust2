83aac67c5b545fcb0bd9fb67aa4dd56ba40f8185
johannes karoff <johannes@karoff.net>
2018-03-07 20:31:25 +0100

    improve notifications

--- utilWeb/src/main/scala/Notifications.scala
+++ utilWeb/src/main/scala/Notifications.scala
@@ -25,22 +25,44 @@ class Notifications {
     promise.future
   }
 
-  def serviceWorkerNotify(title: String, body: Option[String] = None)(implicit ec: ExecutionContext): Unit = if (isGranted) {
-    window.navigator.serviceWorker.getRegistration().toFuture.onComplete {
-      case Success(_registration) if _registration != js.undefined =>
-        val registration = _registration.asInstanceOf[ServiceWorkerRegistrationWithNotifications]
-        registration.showNotification(title, NotificationOptions(body = body.orUndefined)).toFuture.foreach { event =>
-          scribe.info(s"Send out notification via service worker: $event")
+  //TODO: modernizr
+  def notify(title: String, body: Option[String] = None, tag: Option[String] = None)(implicit ec: ExecutionContext): Unit =
+    if (!isGranted) {
+      scribe.info(s"Notifications are not granted, cannot send notification: $title")
+    } else {
+      scribe.info(s"Go notify: $title")
+      val options = NotificationOptions(
+        body = body.orUndefined,
+        tag = tag.orUndefined,
+        renotify = tag.isDefined,
+        icon = "icon.ico"
+      )
+
+      if (window.navigator.serviceWorker != js.undefined) {
+        serviceWorkerNotify(title, options).foreach { success =>
+          if (success) scribe.info(s"Sent notification via ServiceWorker: $title")
+          else {
+            scribe.info(s"Cannot send notification via ServiceWorker, falling back to browser notify: $title")
+            browserNotify(title, options)
+          }
         }
-      case _ =>
-        scribe.warn("Cannot send notification via service worker, falling back to browser notification")
-        browserNotify(title, body)
+      } else {
+        scribe.info(s"ServiceWorker ist not supported by browser, falling back to browser notify: $title")
+        browserNotify(title, options)
+      }
     }
+
+  private def serviceWorkerNotify(title: String, options: NotificationOptions)(implicit ec: ExecutionContext): Future[Boolean] = {
+    window.navigator.serviceWorker.getRegistration().toFuture.flatMap {
+      case _registration if _registration != js.undefined =>
+        val registration = _registration.asInstanceOf[ServiceWorkerRegistrationWithNotifications]
+        registration.showNotification(title, options).toFuture.map { _ => true }.recover { case _ => false }
+      case _ => Future.successful(false)
+    }.recover { case _ => false }
   }
 
-  def browserNotify(title: String, body: Option[String] = None, onclick: Notification => Any = _ => ()): Unit = if (isGranted) {
-    val n = new Notification(title, NotificationOptions(body = body.orUndefined))
-    scribe.info(s"Send out notification via browser: $n")
+  private def browserNotify(title: String, options: NotificationOptions): Unit = {
+    val n = new Notification(title, options)
     // def fire() {
     //   Analytics.sendEvent("notification", "fired", "pwa")
     //   val n = new Notification(title, NotificationOptions(body = body.orUndefined))
