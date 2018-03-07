4b5c930ddaa6fe7706a837d4f22758ac26ae0fef
Felix Dietze <felix.dietze@rwth-aachen.de>
2018-03-07 23:51:58 +0100

    Default background color: gray

--- utilWeb/src/main/scala/views/PageStyle.scala
+++ utilWeb/src/main/scala/views/PageStyle.scala
@@ -1,15 +1,18 @@
 package wust.utilWeb.views
 
-import colorado.{Color, HCL, RGB}
+import cats.data.NonEmptyList
+import colorado.{Color, HCL, LAB, RGB}
+import wust.graph.{Page, Post}
 import wust.utilWeb.Color._
-import wust.graph.{Graph, Page, Post}
+
+import scala.collection.breakOut
 
 object PageStyle {
   object Color {
     //TODO: ensure that these are calculated at compile time
-    val baseBg = RGB("#F3EFCC").hcl
-    val baseBgDark = RGB("#4D394B").hcl
-    val border = RGB("#95CCDF").hcl
+    private[PageStyle] val baseBg = RGB("#F3EFCC").hcl
+    private[PageStyle] val baseBgDark = RGB("#4D394B").hcl
+    private[PageStyle] val border = RGB("#95CCDF").hcl
   }
 
 
@@ -17,19 +20,19 @@ object PageStyle {
 
     val title = parents.map(_.content).mkString(", ")
 
-    val mixedDirectParentColors = mixColors(page.parentIds.map(baseColor))
-    val baseHue = mixedDirectParentColors.hcl.h
+    val mixedDirectParentColors = NonEmptyList.fromList(page.parentIds.map(baseColor)(breakOut):List[Color]).map(mixColors)
+    val baseHue = mixedDirectParentColors.map(_.hcl.h)
 
-    def withBaseHue(base:HCL) = HCL(baseHue, base.c, base.l)
+    def withBaseHueDefaultGray(base:HCL) = baseHue.fold(LAB(base.l, 0, 0):Color)(hue => HCL(hue, base.c, base.l))
 
-    val accentLineColor = withBaseHue(Color.border)
-    val darkBgColor = withBaseHue(Color.baseBgDark)
-    val bgColor = withBaseHue(Color.baseBg)
+    val accentLineColor = withBaseHueDefaultGray(Color.border)
+    val darkBgColor = withBaseHueDefaultGray(Color.baseBgDark)
+    val bgColor = withBaseHueDefaultGray(Color.baseBg)
 
     new PageStyle(title, baseHue, bgColor, accentLineColor, darkBgColor)
   }
 }
 
-case class PageStyle(title:String, baseHue:Double, bgColor:Color, accentLineColor:Color, darkBgColor:Color)
+case class PageStyle(title:String, baseHue:Option[Double], bgColor:Color, accentLineColor:Color, darkBgColor:Color)
 
 
