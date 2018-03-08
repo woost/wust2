4f0a28ca63405afc998eefda49342ea139d0a3a2
Felix Dietze <felix.dietze@rwth-aachen.de>
2018-03-08 16:00:17 +0100

    Update scala.rx

--- project/Deps.scala
+++ project/Deps.scala
@@ -40,7 +40,7 @@ object Deps {
   // webApp
   val d3v4 = dep("com.github.fdietze" %% "scala-js-d3v4" % "08fc8de")
   val vectory = dep("com.github.fdietze" % "vectory" % "d0e70f4")
-  val scalarx = dep("com.github.fdietze.duality" %%% "scalarx" % "1c6709b")
+  val scalarx = dep("com.github.fdietze.duality" %%% "scalarx" % "8d99eea")
   val outwatch = dep("io.github.outwatch" % "outwatch" % "6dd72a31cb4")
 
   // utility
