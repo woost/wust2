c050bcc8293cb19fe5fe2eb2f8552733c00baa00
johannes karoff <johannes@karoff.net>
2018-03-08 20:53:15 +0100

    fix webpack prod

--- utilWeb/webpack/webpack.base.common.js
+++ utilWeb/webpack/webpack.base.common.js
@@ -1,6 +1,7 @@
 const CopyPlugin = require("copy-webpack-plugin");
 const glob = require("glob");
 const Path = require("path");
+const fs = require("fs");
 
 // directories
 const dirs = {}
@@ -16,6 +17,13 @@ const appName = Object.keys(webpack.entry)[0];
 webpack.plugins = webpack.plugins || [];
 webpack.module = webpack.module || {};
 webpack.module.rules = webpack.module.rules || [];
+
+// TODO bug with custom output in https://github.com/scalacenter/scalajs-bundler/issues/192
+// expects the originally configured output file to exist, just create it.
+const dummyOutputFile = Path.join(webpack.output.path, webpack.output.filename.replace('[name]', appName));
+if (!fs.existsSync(dummyOutputFile)) {
+    fs.closeSync(fs.openSync(dummyOutputFile, 'w'));
+}
 
 // gather css resources
 const cssFiles = glob.sync(Path.join(dirs.assets, "*.css")).concat(glob.sync(Path.join(dirs.sharedAssets, "*.css")));
