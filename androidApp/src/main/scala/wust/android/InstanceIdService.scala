cc50e1fc190561db2df1967c35d0e0479e351da3
Felix Dietze <felix.dietze@rwth-aachen.de>
2018-03-10 19:49:32 +0100

    Prepare Android firebase messaging

---
+++ androidApp/src/main/scala/wust/android/InstanceIdService.scala
@@ -0,0 +1,8 @@
+package wust.android
+
+import com.google.firebase.iid.FirebaseInstanceIdService
+
+class InstanceIdService extends FirebaseInstanceIdService {
+  // https://medium.com/@nileshsingh/how-to-add-push-notification-capability-to-your-android-app-a3cac745e56e
+
+}
