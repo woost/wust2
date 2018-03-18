d29381e97e5bb9d7b32977659b046ebfbcc83878
Felix Dietze <felix.dietze@rwth-aachen.de>
2018-03-18 18:46:21 +0100

    Show rawgraph in devview

--- util/shared/src/main/scala/package.scala
+++ util/shared/src/main/scala/package.scala
@@ -38,4 +38,8 @@ package object util {
       p.future
     }
   }
+
+  implicit class RichBoolean(val b: Boolean) extends AnyVal {
+    final def option[A](a: => A): Option[A] = if (b) Some(a) else None
+  }
 }
