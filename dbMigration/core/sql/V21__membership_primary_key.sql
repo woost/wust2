30ac268298603b25bad347fca9fd4e8735524dc9
Felix Dietze <felix.dietze@rwth-aachen.de>
2018-03-24 18:41:10 +0100

    Fix membership primary key migration

--- dbMigration/core/sql/V21__membership_primary_key.sql
+++ dbMigration/core/sql/V21__membership_primary_key.sql
@@ -1,2 +1,5 @@
+-- deleting duplicates before adding unique index:
+delete from membership a using membership b where a.ctid < b.ctid and a.userid = b.userid and a.postid = b.postid;
+
 alter table membership add PRIMARY KEY (userid,postid);
 
