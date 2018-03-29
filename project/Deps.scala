46b0222f3c13ceba5b025637d20696eac4839e86
johannes karoff <johannes@karoff.net>
2018-03-29 21:24:59 +0200

    update covenant

--- project/Deps.scala
+++ project/Deps.scala
@@ -56,7 +56,7 @@ object Deps {
 
   // rpc
   val covenant = new {
-    private val version = "c7bfb44"
+    private val version = "da2ce5a"
     val core = dep("com.github.cornerman.covenant" %%% "covenant-core" % version)
     val ws = dep("com.github.cornerman.covenant" %%% "covenant-ws" % version)
     val http = dep("com.github.cornerman.covenant" %%% "covenant-http" % version)
