696e48708d2d60be2fc23a39a949661184fad1d9
johannes karoff <johannes@karoff.net>
2018-04-12 16:24:12 +0200

    use filename instead of position in scribe loggin

--- utilBackend/src/main/scala/Logging.scala
+++ utilBackend/src/main/scala/Logging.scala
@@ -3,12 +3,14 @@ package wust.utilBackend
 import scribe._
 import scribe.format._
 import scribe.writer._
+import java.io.File
 
 object Logging {
   val shortThreadName = threadName.map(_.replaceFirst("server-akka.actor.default-dispatcher-", ""))
-  val shortLevel = levelPaddedRight.map(_.trim)
-  val simpleFormatter = formatter"$positionAbbreviated - $message$newLine"
-  val detailFormatter = formatter"$date $shortLevel [$shortThreadName] $positionAbbreviated - $message$newLine"
+  val shortLevel = level.map(_.trim)
+  val fileBaseName = FormatBlock.FileName.map(fileName => fileName.split('/').last)
+  val simpleFormatter = formatter"$fileBaseName:${FormatBlock.LineNumber} - $message$newLine"
+  val detailFormatter = formatter"$date $shortLevel [$shortThreadName] $fileBaseName - $message$newLine"
 
   def setup(): Unit = {
     Logger.update(Logger.rootName) {
