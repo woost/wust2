d91565f97c5e1eaba16f16063f099f88e0cb1b9e
Felix Dietze <felix.dietze@rwth-aachen.de>
2018-04-19 16:15:59 +0200

    Fix View.fromString

--- utilWeb/src/main/scala/TiledView.scala
+++ utilWeb/src/main/scala/TiledView.scala
@@ -6,8 +6,8 @@ import wust.utilWeb._
 import rx._
 
 class TiledView(val views: List[View]) extends View {
-  override val key = views.map(_.key).mkString(TiledView.separator)
-  override val displayName = views.map(_.displayName).mkString(TiledView.separator)
+  override val key = views.map(_.key).mkString(TiledView.separator.toString)
+  override val displayName = views.map(_.displayName).mkString(TiledView.separator.toString)
 
   override final def apply(state: GlobalState)(implicit ctx: Ctx.Owner) = div(
     id := "viewgrid",
@@ -15,5 +15,5 @@ class TiledView(val views: List[View]) extends View {
   )
 }
 object TiledView {
-  val separator = "|"
+  val separator:Char = '|' // needs to be a char, because else strings will be interpreted as regexes
 }
