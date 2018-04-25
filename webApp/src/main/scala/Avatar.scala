05e8782dbd8ccd963c9488d3e83cbf6056d24e1d
Felix Dietze <felix.dietze@rwth-aachen.de>
2018-04-25 16:22:33 +0200

    refactor avatar

--- webApp/src/main/scala/Avatar.scala
+++ webApp/src/main/scala/Avatar.scala
@@ -13,99 +13,113 @@ import wust.ids._
 import colorado.HCL
 
 object Avatar {
+  //TODO: less-angry rainbow? https://bl.ocks.org/mbostock/310c99e53880faec2434
 
-  def post(postId:PostId) = apply(postId, 7, true)
-  def user(postId:UserId) = apply(postId, 5, false)
-  def apply(seed: Any, n: Int, xMirror:Boolean): VNode = {
+  def post(postId:PostId) = twoMirror(postId, 10)
+  def user(postId:UserId) = verticalMirror(postId, 5)
+
+  private val accentColorDeltas = Array[Double](-1, 0, 0, 0, 0, 1)
+  @inline private def accentColorLookup(r:scala.util.Random) = accentColorDeltas(r.nextInt(accentColorDeltas.size))
+  @inline private def addPixel(pixels:mutable.ArrayBuffer[VNode], x:Int, y:Int, color:String):Unit = {
+    import outwatch.dom.dsl.svg
+    pixels += svg.rect(
+      dsl.svg.x := x.toString,
+      dsl.svg.y := y.toString,
+      svg.width := "1",
+      svg.height := "1",
+      svg.fill := color
+    )
+  }
+
+  def verticalMirror(seed: Any, n: Int): VNode = {
     import outwatch.dom.dsl.svg.{svg, rect, fill, width, height, viewBox}
     val rnd = new scala.util.Random(new scala.util.Random(seed.hashCode).nextLong()) // else nextDouble is too predictable
 
     val hue = genericBaseHue(seed)
     // every avatar has exactly two possible accent-colors, which are base -+ 63deg
-    def accentDelta = rnd.nextGaussian().toInt.min(1).max(-1) // (1:-1  1:1  4:0), 1 corresponds to 63 deg hue
+    def accentDelta = accentColorLookup(rnd)
     val area = n*n
-
-    val fillx = ceil(n / 2.0).toInt
-    val filly = if(xMirror) fillx else n
-    def index(x:Int,y:Int) = y*n + x
-    val color = new Array[String](area)
-    var x = 0
-    var y = 0
-    while(y < filly) {
-      x = 0
-      while(x < fillx) {
-        if(rnd.nextBoolean()) {
-          //TODO: less-angry rainbow? https://bl.ocks.org/mbostock/310c99e53880faec2434
-          color(index(x,y)) = HCL(hue + accentDelta, 60, 65).toHex
-        }
-        x+=1
-      }
-      y+=1
-    }
-
-    if(xMirror) {
-      y = filly
-      while(y < n) {
-        x = 0
-        while(x < fillx) {
-          color(index(x,y)) = color(index(x, n-y-1)) // mirror on x-axis
-          x+=1
-        }
-        y+=1
-      }
-    }
-
-    y = 0
-    while(y < n) {
-      x = fillx
-      while(x < n) {
-        color(index(x,y)) = color(index(n-x-1, y)) // mirror on y-axis
-        x+=1
-      }
-      y+=1
-    }
+    val half = (n / 2) + (n % 2)
 
     val pixels = new mutable.ArrayBuffer[VNode](initialSize = area)
-    y = 0
+
+    // mirror on y axis
+    var x = 0
+    var y = 0
     while(y < n) {
       x = 0
-      while(x < n) {
-        val c = color(index(x,y))
-        if(c != null) {
-         pixels += rect(
-             dsl.svg.x := x.toString,
-             dsl.svg.y := y.toString,
-             width := "1",
-             height := "1",
-             fill := c
-           )
+      while(x < half) {
+        if(rnd.nextBoolean()) {
+          val color = HCL(hue + accentDelta, 60, 65).toHex
+          addPixel(pixels, x,y, color)
+          addPixel(pixels, n-x-1,y, color)
         }
         x+=1
       }
       y+=1
     }
-
-    // val left = List.tabulate(fillx, filly)((_, _)).flatten
-    //   .filter(_ => rnd.nextBoolean()).map { case (x, y) =>
-    //   val hue = (hue + rnd.nextGaussian().toInt)%360 // this was an accident! The hue should be 0..2PI
-    //   val color = colorado.HCL(hue, 60, 65)
-
-    //   (x,y, color.toHex)
-    // }
-    // val full = (left ++ left.map{case (x,y,c) => ((n-1)-x, y, c)}).distinct
 
     svg(
       viewBox := s"0 0 $n $n",
       pixels
-      // full.map{case (px,py, c) =>
-      //   rect(
-      //     dom.dsl.svg.x := s"${px}",
-      //     dom.dsl.svg.y := s"${py}",
-      //     width := "1",
-      //     height := "1",
-      //     fill := c
-      //   )
-      // }
+    )
+  }
+
+  def twoMirror(seed: Any, n: Int): VNode = {
+    import outwatch.dom.dsl.svg.{svg, rect, fill, width, height, viewBox}
+    val rnd = new scala.util.Random(new scala.util.Random(seed.hashCode).nextLong()) // else nextDouble is too predictable
+
+    val hue = genericBaseHue(seed)
+    // every avatar has exactly two possible accent-colors, which are base -+ 63deg
+    def accentDelta = accentColorLookup(rnd)
+    val area = n*n
+    val half = (n / 2) + (n % 2)
+
+    val pixels = new mutable.ArrayBuffer[VNode](initialSize = area)
+
+    if(rnd.nextBoolean()) {
+      // mirror on x and y axis
+      var x = 0
+      var y = 0
+      while(y < half) {
+        x = 0
+        while(x < half) {
+          if(rnd.nextBoolean()) {
+            val color = HCL(hue + accentDelta, 60, 65).toHex
+            addPixel(pixels, x,y, color)
+            addPixel(pixels, x,n-y-1, color)
+            addPixel(pixels, n-x-1,y, color)
+            addPixel(pixels, n-x-1,n-y-1, color)
+          }
+          x+=1
+        }
+        y+=1
+      }
+    } else {
+      // mirror on diagonals
+      var x = 0
+      var y = 0
+      var startx = 0
+      while(y < half) {
+        x = startx
+        while(x < n - startx) {
+          if(rnd.nextBoolean()) {
+            val c = HCL(hue + accentDelta, 60, 65).toHex
+            addPixel(pixels, x,y, c)
+            addPixel(pixels, y,x, c)
+            addPixel(pixels, n-y-1,n-x-1, c)
+            addPixel(pixels, n-x-1,n-y-1, c)
+          }
+          x+=1
+        }
+        startx += 1
+        y+=1
+      }
+    }
+
+    svg(
+      viewBox := s"0 0 $n $n",
+      pixels
     )
   }
 }
--- webApp/src/main/scala/views/AvatarView.scala
+++ webApp/src/main/scala/views/AvatarView.scala
@@ -24,15 +24,28 @@ object AvatarView extends View {
       overflow.auto,
       div(
         padding := "10px",
-        display := "grid",
-        style("grid-gap") := "10px",
-        style("grid-template-columns") := s"repeat(auto-fill, minmax(${size}px, 1fr))",
-        List.tabulate(n)(i => Avatar(i, 5, false)(width := s"${size}px", height := s"${size}px")  ),
-        List.tabulate(n)(i => Avatar(i, 7, true)(width := s"${size}px", height := s"${size}px")  ),
+        // display := "grid",
+        // style("grid-gap") := "10px",
+        // style("grid-template-columns") := s"repeat(auto-fill, minmax(${size}px, 1fr))",
+        (5 to 5).map{ res =>
+          div(width := s"${n*(size + 10)}px", marginBottom := "10px", List.tabulate(n)(i => Avatar.verticalMirror(i, res)(width := s"${size}px", height := s"${size}px", display.inlineBlock, marginRight := "10px")  )),
+        },
+        (10 to 10).map{ res =>
+          div(width := s"${n*(size + 10)}px", marginBottom := "10px", List.tabulate(n)(i => Avatar.twoMirror(i, res)(width := s"${size}px", height := s"${size}px", display.inlineBlock, marginRight := "10px")  )),
+        },
+        // List.tabulate(n)(i => Avatar(i, 10, false)(width := s"${size}px", height := s"${size}px")  ),
+        // div(),
+        // List.tabulate(n)(i => Avatar(i, 11, false)(width := s"${size}px", height := s"${size}px")  ),
+        // div(),
+        // List.tabulate(n)(i => Avatar(i, 12, false)(width := s"${size}px", height := s"${size}px")  ),
+        // div(),
+        // List.tabulate(n)(i => Avatar(i, 13, true)(width := s"${size}px", height := s"${size}px")  ),
+        // // div(),
+        // List.tabulate(n)(i => Avatar(i, 25, true)(width := s"${size}px", height := s"${size}px")  ),
         //{
         //  //~290us
         //  val sw = new wust.util.time.StopWatch()
-        //  sw.benchmark(10000)(avatarGrid2(scala.util.Random.nextInt(), size, 9, true))
+        //  sw.benchmark(100000)(Avatar.twoMirror(scala.util.Random.nextInt(), 10))
         //  sw.readMicros.toInt //TODO: Outwatch: accept Long
         //}
       )
