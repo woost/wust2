861f4674f57b3e6efe66fd6c531c28c794dcef4b
johannes karoff <johannes@karoff.net>
2018-05-11 17:43:23 +0200

    exit db-migration after retries if cannot connect to postgres

--- dbMigration/flyway-await-postgres.sh
+++ dbMigration/flyway-await-postgres.sh
@@ -2,10 +2,17 @@
 
 host="$1"
 echo -n "waiting for postgres to be up..."
+counter=0
 while ! nc -z "$host" 5432; do
+    counter=$((counter+1))
+    if [[ $counter -gt 30 ]]; then
+        echo "cannot connect to postgres on host '$host:5432'!"
+        exit 1
+    fi
+
     sleep 1
 done
 
-echo "done"
+echo "connected to postgres!"
 
 flyway -url="jdbc:postgresql://$host/$POSTGRES_DB" -schemas=public -user=$POSTGRES_USER -password=$POSTGRES_PASSWORD migrate
