384d65497844ba52e9b0069aec29072880e2c658
johannes karoff <johannes@karoff.net>
2018-05-14 13:54:00 +0200

    add json response marshaller to json api for correct content type

--- core/src/main/scala/Server.scala
+++ core/src/main/scala/Server.scala
@@ -3,8 +3,12 @@ package wust.backend
 import java.nio.ByteBuffer
 
 import akka.actor.ActorSystem
+import akka.http.scaladsl.marshalling.{Marshaller, ToResponseMarshaller}
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpResponse, StatusCodes}
 import akka.stream.{ActorMaterializer, OverflowStrategy}
-import wust.api._, serialize.Boopickle._, serialize.Circe._
+import wust.api._
+import serialize.Boopickle._
+import serialize.Circe._
 import wust.ids._
 import wust.backend.Dsl._
 import wust.backend.auth._
@@ -20,12 +24,14 @@ import mycelium.server._
 import chameleon.ext.boopickle._
 import boopickle.Default._
 import chameleon.ext.circe._
-import io.circe._, io.circe.syntax._, io.circe.generic.auto._
-import wust.util.{ Pipe, RichFuture }
+import io.circe._
+import io.circe.syntax._
+import io.circe.generic.auto._
+import wust.util.{Pipe, RichFuture}
 import cats.implicits._
 import monix.execution.Scheduler
 
-import scala.util.{ Success, Failure }
+import scala.util.{Failure, Success}
 
 object Server {
   import akka.http.scaladsl.server.RouteResult._
@@ -79,6 +85,9 @@ object Server {
       AkkaWsRoute.fromApiRouter(binaryRouter, serverConfig, apiConfig)
     } ~ pathPrefix("api") {
       cors(corsSettings) {
+        implicit val jsonMarshaller: ToResponseMarshaller[String] = Marshaller.withFixedContentType(ContentTypes.`application/json`) { s =>
+          HttpResponse(status = StatusCodes.OK, entity = HttpEntity(ContentTypes.`application/json`, s))
+        }
         AkkaHttpRoute.fromApiRouter(jsonRouter, apiConfig)
       }
     } ~ (path("health") & get) {
