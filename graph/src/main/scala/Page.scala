8a18a4f1312ae88ddea4d8f89d990a0cc8309012
johannes karoff <johannes@karoff.net>
2018-05-19 19:32:09 +0200

    click on header goes to channels page of user

--- graph/src/main/scala/Page.scala
+++ graph/src/main/scala/Page.scala
@@ -5,7 +5,8 @@ import wust.ids._
 case class Page(parentIds: Seq[PostId], childrenIds: Seq[PostId] = Nil)
 
 object Page {
-  val empty = new Page(Seq.empty)
+  val empty = Page(Seq.empty)
+  def ofUser(user: User) = Page(Seq(user.channelPostId))
   def apply(parentId:PostId):Page = Page(Seq(parentId))
 }
 
