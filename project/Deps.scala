566e5d3bfc56feb69eb104e335183286b74bd369
johannes karoff <johannes@karoff.net>
2018-05-22 13:46:23 +0200

    update outwatch to fixed modifier stream version

--- project/Deps.scala
+++ project/Deps.scala
@@ -45,7 +45,7 @@ object Deps {
   val vectory = dep("com.github.fdietze" % "vectory" % "d0e70f4")
   val scalarx = dep("com.lihaoyi" %%% "scalarx" % "0.4.0")
   // val scalarx = dep("com.github.fdietze.duality" %%% "scalarx" % "94c6d80") // jitpack cannot handle the . in repo name scala.rx
-  val outwatch = dep("io.github.cornerman" % "outwatch" % "f97e275")
+  val outwatch = dep("io.github.cornerman" % "outwatch" % "0cea657")
 
   // utility
   val scribe = dep("com.outr" %%% "scribe" % "2.3.5-SNAPSHOT")
