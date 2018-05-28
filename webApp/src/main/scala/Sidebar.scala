582ca24dd36f611128338f67ca04883a25fb3d79
Felix Dietze <felix.dietze@rwth-aachen.de>
2018-05-28 11:47:33 +0200

    Notification icons

--- webApp/src/main/scala/Sidebar.scala
+++ webApp/src/main/scala/Sidebar.scala
@@ -6,6 +6,7 @@ import outwatch.dom._
 import outwatch.dom.dsl._
 import rx._
 import fontAwesome.freeSolid._
+import fontAwesome.freeRegular
 import wust.webApp.outwatchHelpers._
 import wust.graph._
 import wust.ids._
@@ -28,13 +29,17 @@ object Sidebar {
       Notifications.permissionStateObservable.map { state =>
         if (state == PermissionState.granted) Option.empty[VNode]
         else if (state == PermissionState.denied) Some(
-          span("Notifications blocked", title := "To enable Notifications for this site, reconfigure your browser to not block Notifications from this domain.")
+          span(
+            freeRegular.faBellSlash,
+            color := "tomato",
+            title := "Notifications blocked by browser. Reconfigure your browser allow Notifications for this site."
+          )
         ) else Some(
-          button("Enable Notifications",
-            padding := "5px 3px",
-            width := "100%",
+          div(
+            freeRegular.faBellSlash,
+            cursor.pointer,
             onClick --> sideEffect { Notifications.requestPermissions() },
-            buttonStyle
+            title := "Enable Notifications"
           )
         )
       }
