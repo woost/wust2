b963f10f422bdc49b021f31eb128a9070fd2bc5b
Felix Dietze <felix.dietze@rwth-aachen.de>
2018-05-31 17:23:49 +0200

    sw: activate skipWaiting and clientsClaim

--- webApp/webpack/webpack.base.offline.js
+++ webApp/webpack/webpack.base.offline.js
@@ -1,8 +1,11 @@
 const {InjectManifest} = require('workbox-webpack-plugin');
 
+// https://developers.google.com/web/tools/workbox/modules/workbox-webpack-plugin#injectmanifest_plugin_1
 module.exports = {
     plugins: [
         new InjectManifest({
+            skipWaiting: true,
+            clientsClaim: true,
             swSrc: '../../../../../webApp/src/js/sw.js',
             swDest: 'sw.js',
             importWorkboxFrom: 'local', // will copy all of the Workbox runtime libraries into a versioned directory alongside your generated service worker, and configure the service worker to use those local copies. 
