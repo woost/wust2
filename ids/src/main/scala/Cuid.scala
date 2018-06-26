4e7c17464bd56cf96f7184bc17a95e272a09a240
johannes karoff <johannes@karoff.net>
2018-06-27 01:09:59 +0200

    simplify cuid

--- ids/src/main/scala/Cuid.scala
+++ ids/src/main/scala/Cuid.scala
@@ -3,11 +3,7 @@ package wust.ids
 import java.util.UUID
 
 case class Cuid(left: Long, right: Long) {
-  def toUuid: UUID = {
-    val hexString = f"${left}%016x${right}%016x"
-    val uuidString = uuidStringWithHyphens(hexString)
-    UUID.fromString(uuidString)
-  }
+  def toUuid: UUID = new UUID(left, right)
 
   def toCuidString: String = {
     val base = 36
@@ -15,38 +11,9 @@ case class Cuid(left: Long, right: Long) {
     val rightCuid = java.lang.Long.toUnsignedString(right, base)
     "c" + leftCuid + rightCuid
   }
-
-  private def uuidStringWithHyphens(uuid: String) = {
-    require(uuid.length == 32, "UUID string needs to have length of 32")
-
-    val sb = new StringBuilder
-    sb.append(uuid.substring(0, 8))
-    sb.append("-")
-    sb.append(uuid.substring(8, 12))
-    sb.append("-")
-    sb.append(uuid.substring(12, 16))
-    sb.append("-")
-    sb.append(uuid.substring(16, 20))
-    sb.append("-")
-    sb.append(uuid.substring(20, 32))
-    sb.toString
-  }
-
 }
 object Cuid {
-  def fromUuid(uuid: UUID): Cuid = {
-    val uuidString = uuid.toString.filterNot(_ == '-')
-    require(uuidString.length == 32, "UUID string needs to have length of 32")
-
-    val base = 16
-    val leftUuid = uuidString.substring(0, 16)
-    val rightUuid = uuidString.substring(16, 32)
-    println("LEFT " + leftUuid)
-    println("RIGHT " + rightUuid)
-    val leftLong = java.lang.Long.parseUnsignedLong(leftUuid, base)
-    val rightLong = java.lang.Long.parseUnsignedLong(rightUuid, base)
-    Cuid(leftLong, rightLong)
-  }
+  def fromUuid(uuid: UUID): Cuid = Cuid(uuid.getMostSignificantBits, uuid.getLeastSignificantBits)
 
   def fromCuidString(cuid: String): Cuid = {
     require(cuid.startsWith("c"), "Cuid string needs to start with letter c")
@@ -55,8 +22,8 @@ object Cuid {
     val base = 36
     val leftCuid = cuid.substring(1, 13)
     val rightCuid = cuid.substring(13, 25)
-    val leftLong = java.lang.Long.parseLong(leftCuid, base)
-    val rightLong = java.lang.Long.parseLong(rightCuid, base)
+    val leftLong = java.lang.Long.parseUnsignedLong(leftCuid, base)
+    val rightLong = java.lang.Long.parseUnsignedLong(rightCuid, base)
     Cuid(leftLong, rightLong)
   }
 }
