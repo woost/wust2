82af015f07e546a6d8e7ece14960ccd218cc1843
johannes karoff <johannes@karoff.net>
2018-07-16 22:11:19 +0200

    update to bugfix for vdommodifier stream

--- project/Deps.scala
+++ project/Deps.scala
@@ -46,7 +46,7 @@ object Deps {
   val vectory = dep("com.github.fdietze" % "vectory" % "d0e70f4")
   val scalarx = dep("com.lihaoyi" %%% "scalarx" % "0.4.0")
   // val scalarx = dep("com.github.fdietze.duality" %%% "scalarx" % "94c6d80") // jitpack cannot handle the . in repo name scala.rx
-  val outwatch = dep("io.github.cornerman" % "outwatch" % "c4f0094")
+  val outwatch = dep("com.github.outwatch" % "outwatch" % "130a9a4")
 
   // utility
   val scribe = dep("com.outr" %%% "scribe" % "2.5.0")
