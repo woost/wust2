1aaca9be6ab8242835718b0a333b969b117b0cc6
Felix Dietze <felix.dietze@rwth-aachen.de>
2018-07-20 17:36:35 +0200

    disable patch logging

--- webApp/src/main/scala/Main.scala
+++ webApp/src/main/scala/Main.scala
@@ -14,11 +14,11 @@ object Main {
 
   def main(args: Array[String]): Unit = {
 
-    DevOnly {
-      helpers.OutwatchTracing.patch.zipWithIndex.foreach { case ((old, cur), index) =>
-        console.log(s"Snabbdom patch ($index)!", old, cur)
-      }
-    }
+//    DevOnly {
+//      helpers.OutwatchTracing.patch.zipWithIndex.foreach { case ((old, cur), index) =>
+//        console.log(s"Snabbdom patch ($index)!", old, cur)
+//      }
+//    }
 
     Logging.setup()
 
