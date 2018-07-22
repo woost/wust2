bffc7709c204097ef8337c7df5d10de04c23e4be
johannes karoff <johannes@karoff.net>
2018-07-22 22:51:34 +0200

    toggle tags list for small screens

--- webApp/src/main/scala/views/TagsList.scala
+++ webApp/src/main/scala/views/TagsList.scala
@@ -1,5 +1,6 @@
 package wust.webApp.views
 
+import fontAwesome._
 import outwatch.dom._
 import outwatch.dom.dsl._
 import rx._
@@ -11,26 +12,47 @@ import wust.webApp.views.Elements._
 object TagsList  {
   def apply(state: GlobalState)(implicit ctx: Ctx.Owner): VNode = {
 
+    val sidebarVisible = Var[Boolean](false)
+
+    val sidebarWidth = "150px"
+    val tagSidebar = VDomModifier(
+      width := sidebarWidth,
+      Styles.flex,
+      flexDirection.columnReverse,
+      Rx {
+        val graph = state.graphContent()
+
+        val sortedContainments = graph.containments.toSeq.sortBy { containment =>
+          val authorships = graph.authorshipsByNodeId(containment.sourceId)
+          authorships.sortBy(_.data.timestamp: Long).last.data.timestamp: Long
+        }
+
+        sortedContainments.map(_.targetId).reverse.distinct.map { id =>
+          nodeTag(state, graph.nodesById(id)).apply(margin := "2px")
+        }
+      }
+    )
+
+
+    val sidebarToggleControl = div(
+      freeSolid.faHashtag,
+      onClick.map(_ => !sidebarVisible.now) --> sidebarVisible,
+      cursor.pointer,
+      position := "absolute", //TODO: better?
+      bottom := "50px",
+      paddingRight := "5px"
+    )
+
     div(
       state.screenSize.map {
-        case ScreenSize.Desktop => VDomModifier(
-          width := "150px",
-          Styles.flex,
-          flexDirection.columnReverse,
-          Rx {
-            val graph = state.graphContent()
-
-            val sortedContainments = graph.containments.toSeq.sortBy { containment =>
-              val authorships = graph.authorshipsByNodeId(containment.sourceId)
-              authorships.sortBy(_.data.timestamp: Long).last.data.timestamp: Long
-            }
-
-            sortedContainments.map(_.targetId).reverse.distinct.map { id =>
-              nodeTag(state, graph.nodesById(id)).apply(margin := "2px")
-            }
-          }
-        )
-        case ScreenSize.Mobile => VDomModifier.empty
+        case ScreenSize.Desktop => tagSidebar
+        case ScreenSize.Mobile => VDomModifier(Rx {
+          if (sidebarVisible()) VDomModifier(
+            tagSidebar,
+            sidebarToggleControl(right := sidebarWidth)
+          )
+          else sidebarToggleControl(right := "0px")
+        })
       }
     )
   }
