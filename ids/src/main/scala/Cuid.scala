617aabdfe10225d3e82c9349d589f44e0c5ca8df
Felix Dietze <felix.dietze@rwth-aachen.de>
2018-07-25 14:42:58 +0200

    Fix Cuid requirements

--- ids/src/main/scala/Cuid.scala
+++ ids/src/main/scala/Cuid.scala
@@ -11,11 +11,11 @@ case class Cuid(left: Long, right: Long) {
   // the maximum number of each long for being convertable to a cuid (base 36 with 12 digits): java.lang.Long.parseLong("z" * 12, 36)
   require(
     left >= 0 && left <= maxLong,
-    s"left part of Cuid needs to be positive and less than $maxLong, value is: $left"
+    s"left part of Cuid needs to be positive and less or equal than $maxLong, value is: $left."
   )
   require(
     right >= 0 && right <= maxLong,
-    s"right part of Cuid needs to be positive and less than $maxLong, value is: $right"
+    s"right part of Cuid needs to be positive and less or equal than $maxLong, value is: $right."
   )
 
   def toUuid: UUID = new UUID(left, right)
@@ -45,8 +45,8 @@ object Cuid {
     Cuid(uuid.getMostSignificantBits, uuid.getLeastSignificantBits)
 
   def fromCuidString(cuid: String): Cuid = {
-    require(cuid.startsWith("c"), "Cuid string needs to start with letter c")
-    require(cuid.length == 25, "Cuid string needs to have length of 25")
+    require(cuid.startsWith("c"), "Cuid string needs to start with letter c.")
+    require(cuid.length == 25, "Cuid string needs to have length of 25.")
 
     val base = 36
     val leftCuid = cuid.substring(1, 13)
@@ -57,14 +57,16 @@ object Cuid {
   }
 
   def fromByteArray(arr: Array[Byte]): Cuid = {
-    require(arr.length == 16, "Array[Byte] must have exactly 16 elements")
+    require(arr.length <= 16, "Array[Byte] must have maximum 16 elements.")
     val bb = java.nio.ByteBuffer.allocate(16)
+    bb.position(16-arr.length)
     bb.put(arr)
     bb.flip()
     Cuid(bb.getLong, bb.getLong)
   }
 
   def fromBase58(str: String): Cuid = {
+    require(str.length <= 22, "Base58 Cuid must be max 22 characters long.")
     fromByteArray(Base58.toByteArray(Base58.fromString(str).get).get)
   }
 
