9c26a46f41edc41423c52b5338d266ca942ac00c
Felix Dietze <felix.dietze@rwth-aachen.de>
2018-07-27 11:05:32 +0200

    update db scheme

--- dbMigration/slack/sql/V1__init.sql
+++ dbMigration/slack/sql/V1__init.sql
@@ -1,14 +1,23 @@
 -- Many to one mappings are possible and necessary to imitate linking possibilities of woost in slack
 
 CREATE TABLE user_mapping (
-    slack_id uuid PRIMARY KEY,
-    wust_id uuid NULL,
-    unique(slack_id, wust_id)
+    slack_id text NOT NULL,
+    wust_id uuid NOT NULL,
+    slack_token text NOT NULL,
+    wust_token text NOT NULL,
+    PRIMARY KEY(slack_id, wust_id)
 );
 
-CREATE TABLE node_mapping (
-    slack_id uuid PRIMARY KEY,
-    wust_id uuid NULL,
-    unique(slack_id, wust_id)
+ -- channels
+CREATE TABLE conversation_mapping (
+    slack_conversation_id text NOT NULL,
+    wust_id uuid NOT NULL,
+    PRIMARY KEY(slack_conversation_id, wust_id)
 );
 
+CREATE TABLE message_mapping (
+    slack_channel_id text NOT NULL,
+    slack_message_ts text NOT NULL,
+    wust_id uuid NOT NULL,
+    PRIMARY KEY(slack_channel_id, slack_message_ts, wust_id)
+);
