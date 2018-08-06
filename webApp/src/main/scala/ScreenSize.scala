fa6871d79eb395506b825e7f28ea67e5fc41c256
Felix Dietze <felix.dietze@rwth-aachen.de>
2018-08-06 11:44:43 +0200

    Refactor ScreenSize

--- webApp/src/main/scala/ScreenSize.scala
+++ webApp/src/main/scala/ScreenSize.scala
@@ -2,16 +2,24 @@ package wust.webApp
 
 import org.scalajs.dom
 
-sealed trait ScreenSize
+sealed trait ScreenSize {
+  def minWidth:Int
+}
 object ScreenSize {
-  case object Large extends ScreenSize
-  case object Middle extends ScreenSize
-  case object Small extends ScreenSize
+  case object Large extends ScreenSize {def minWidth = 961}
+  case object Middle extends ScreenSize {def minWidth = 641}
+  case object Small extends ScreenSize {def minWidth = 0}
+
+  def fromPixelSize(width: Int): ScreenSize = width match {
+    case _ if width >= Large.minWidth => Large
+    case _ if width >= Middle.minWidth => Middle
+    case _ => Small
+  }
 
   def calculate(): ScreenSize =
-    if (dom.window.matchMedia("only screen and (min-width : 961px)").matches)
+    if (dom.window.matchMedia(s"only screen and (min-width : ${Large.minWidth}px)").matches)
       ScreenSize.Large
-    else if (dom.window.matchMedia("only screen and (min-width : 641px)").matches)
+    else if (dom.window.matchMedia(s"only screen and (min-width : ${Middle.minWidth}px)").matches)
       ScreenSize.Middle
     else
       ScreenSize.Small
