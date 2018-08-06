cba17baa85e5387fc5f25d45681de1d192e54458
Felix Dietze <felix.dietze@rwth-aachen.de>
2018-08-06 11:32:42 +0200

    consistent tag color across views

--- sdk/shared/src/main/scala/NodeColor.scala
+++ sdk/shared/src/main/scala/NodeColor.scala
@@ -15,9 +15,9 @@ object BaseColors {
   val pageBgLight = RGB("#e2f8f2").hcl
   val pageBorder = RGB("#95CCDF").hcl
 
-  val tag = RGB("#dfa597").hcl
-  val eulerBg = RGB("#89B8FF").hcl
-  val kanbanColumnBg = RGB("#dfa597").hcl
+  val tag = RGB("#E39B86").hcl
+  val eulerBg = RGB("#E39B86").hcl
+  val kanbanColumnBg = RGB("#E39B86").hcl
 }
 
 object NodeColor {
