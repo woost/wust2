054d61aabef59397c8da6247232ccfbb3d50a80b
Felix Dietze <felix.dietze@rwth-aachen.de>
2018-08-09 13:18:50 +0200

    hide orphans in collapsed sidebar

--- webApp/src/main/scala/Sidebar.scala
+++ webApp/src/main/scala/Sidebar.scala
@@ -23,16 +23,18 @@ object Sidebar {
     div(
       cls := "sidebar",
       backgroundColor <-- state.pageStyle.map(_.sidebarBgColor),
-//      flexBasis <-- sidebarOpen.map { case true => "175px"; case false => "30px" },
+      //      flexBasis <-- sidebarOpen.map { case true => "175px"; case false => "30px" },
       sidebarOpen.map {
-        case true => VDomModifier( // sidebar open
+        case true  => VDomModifier( // sidebar open
           channels(state)(ctx),
           newChannelButton(state)(ctx)(
             cls := "newChannelButton-large " + buttonStyles,
           ),
-          Rx{ (state.screenSize() == ScreenSize.Small).ifTrue[VDomModifier](
-            div(Topbar.authentication(state))(alignSelf.center, marginTop := "30px")
-          )},
+          Rx {
+            (state.screenSize() == ScreenSize.Small).ifTrue[VDomModifier](
+              div(Topbar.authentication(state))(alignSelf.center, marginTop := "30px")
+            )
+          },
           state.screenSize.map {
             case ScreenSize.Small => VDomModifier(
               width := "100%",
@@ -41,7 +43,7 @@ object Sidebar {
               zIndex := ZIndex.overlay,
               onClick(false) --> state.sidebarOpen
             )
-            case _ => VDomModifier(
+            case _                => VDomModifier(
               minWidth := "40px",
               maxWidth := "250px"
             )
@@ -88,19 +90,19 @@ object Sidebar {
                 paddingRight := "5px",
                 //TODO: inner state.page obs again
                 channelIcon(state, p, page.parentIds.contains(p.id), 30)(ctx)(
-//                  marginRight := "5px" //TODO: outwatch bug? first channelicon does not have a margin
+                  //                  marginRight := "5px" //TODO: outwatch bug? first channelicon does not have a margin
                 ),
                 renderNodeData(p.data)(marginLeft := "5px"),
                 onChannelClick(ChannelAction.Post(p.id))(state),
                 title := p.id.toCuidString
               )
           },
-//          channelDiv(page.mode == PageMode.Orphans, state.pageStyle())(
-//            //TODO: inner state.page obs again
-//            noChannelIcon(page.mode == PageMode.Orphans)(ctx)(marginRight := "5px"),
-//            PageMode.Orphans.toString,
-//            onChannelClick(ChannelAction.Page(PageMode.Orphans))(state)
-//          )
+          //          channelDiv(page.mode == PageMode.Orphans, state.pageStyle())(
+          //            //TODO: inner state.page obs again
+          //            noChannelIcon(page.mode == PageMode.Orphans)(ctx)(marginRight := "5px"),
+          //            PageMode.Orphans.toString,
+          //            onChannelClick(ChannelAction.Page(PageMode.Orphans))(state)
+          //          )
         )
       }
     )
@@ -129,30 +131,30 @@ object Sidebar {
               cls := "node drag-feedback"
             )
           },
-          noChannelIcon(page.mode == PageMode.Orphans)(ctx)(
-            onChannelClick(ChannelAction.Page(PageMode.Orphans))(state)
-          )
+          //          noChannelIcon(page.mode == PageMode.Orphans)(ctx)(
+          //            onChannelClick(ChannelAction.Page(PageMode.Orphans))(state)
+          //          )
         )
       }
     )
   }
 
   def channelIcon(state: GlobalState, post: Node, selected: Boolean, size: Int)(
-      implicit ctx: Ctx.Owner
+    implicit ctx: Ctx.Owner
   ): VNode = {
     div(
       cls := "channelicon",
       Styles.flexStatic,
       margin := "0",
-      width := s"${size}px",
-      height := s"${size}px",
+      width := s"${ size }px",
+      height := s"${ size }px",
       title := post.data.str,
       cursor.pointer,
       backgroundColor := BaseColors.pageBg.copy(h = NodeColor.hue(post.id)).toHex,
-      opacity := (if (selected) 1.0 else 0.75),
-      padding := (if (selected) "2px" else "4px"),
+      opacity := (if(selected) 1.0 else 0.75),
+      padding := (if(selected) "2px" else "4px"),
       border := (
-        if (selected) s"2px solid ${BaseColors.sidebarBg.copy(h = NodeColor.hue(post.id)).toHex}"
+        if(selected) s"2px solid ${ BaseColors.sidebarBg.copy(h = NodeColor.hue(post.id)).toHex }"
         else "none"),
       Avatar.node(post.id)
     )
@@ -168,21 +170,21 @@ object Sidebar {
       val page = state.page.now
       //TODO if (e.shiftKey) {
       action match {
-        case ChannelAction.Post(id) =>
-          if (e.ctrlKey) {
+        case ChannelAction.Post(id)   =>
+          if(e.ctrlKey) {
             val filtered = page.parentIds.filterNot(_ == id)
             val parentIds =
-              if (filtered.size == page.parentIds.size) page.parentIds :+ id
-              else if (filtered.nonEmpty) filtered
+              if(filtered.size == page.parentIds.size) page.parentIds :+ id
+              else if(filtered.nonEmpty) filtered
               else Seq(id)
             page.copy(parentIds = parentIds)
           } else Page(Seq(id))
         case ChannelAction.Page(mode) =>
-          val newMode = if (page.mode != mode) mode else PageMode.Default
-          if (e.ctrlKey) page.copy(mode = newMode) else Page(Seq.empty, mode = newMode)
+          val newMode = if(page.mode != mode) mode else PageMode.Default
+          if(e.ctrlKey) page.copy(mode = newMode) else Page(Seq.empty, mode = newMode)
       }
     } --> sideEffect { page =>
-      val contentView = if (state.view.now.isContent) state.view.now else ViewList.default
+      val contentView = if(state.view.now.isContent) state.view.now else ViewList.default
       state.viewConfig() = state.viewConfig.now.copy(page = page, view = contentView)
     }
 }
