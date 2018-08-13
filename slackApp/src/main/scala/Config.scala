8c7f25c8fb41a305542c6b9ad0b3f572c45ad91b
GRBurst <GRBurst@protonmail.com>
2018-08-13 10:48:26 +0200

    Add Slack configurator

---
+++ slackApp/src/main/scala/Config.scala
@@ -0,0 +1,16 @@
+package wust.slack
+
+import wust.sdk.{OAuthConfig, ServerConfig, WustConfig}
+import com.typesafe.config.{Config => TConfig}
+
+case class SlackClientConfig(token: String, botId: String, signingSecret: String)
+
+case class Config(wust: WustConfig, server: ServerConfig, oauth: OAuthConfig, postgres: TConfig, slack: SlackClientConfig)
+
+object Config {
+  import pureconfig._
+  import wust.util.Config._
+  import pureconfig.error.ConfigReaderFailures
+
+  def load(path: String = "wust.slack"): Either[ConfigReaderFailures, Config] = loadConfig[Config](path)
+}
