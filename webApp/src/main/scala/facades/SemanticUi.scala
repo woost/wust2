cd74c5821d684454736dee6e3f718507e2d95e0d
GRBurst <GRBurst@protonmail.com>
2018-08-24 20:24:44 +0200

    Add semantic ui modal to facade

--- webApp/src/main/scala/facades/SemanticUi.scala
+++ webApp/src/main/scala/facades/SemanticUi.scala
@@ -8,9 +8,18 @@ import scala.scalajs.js.annotation._
 @js.native
 @JSGlobalScope
 object JQuery extends js.Object {
-  def `$`(elem:HTMLElement):JQuerySelectionWithDropdown = js.native
+  def `$`(elem:HTMLElement):JQuerySelection = js.native
 
   def alert(message: String): Unit = js.native
+}
+
+@js.native
+trait JQuerySelection extends js.Object with JQuerySelectionWithDropdown with JQuerySelectionWithModal {
+}
+@js.native
+trait JQuerySelectionWithModal extends js.Object {
+  def modal(behavior: String*): Unit = js.native
+  def modal(options: ModalOptions): JQuerySelectionWithModal = js.native
 }
 
 @js.native
