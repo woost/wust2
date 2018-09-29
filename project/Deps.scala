cbc176c2e06b454710137c10735bb31e3cb5fbbf
GRBurst <GRBurst@protonmail.com>
2018-09-29 12:18:11 +0200

    Update kittens 1.0.0 -> 1.2.0

--- project/Deps.scala
+++ project/Deps.scala
@@ -16,7 +16,7 @@ object Deps {
   // core libraries
   val cats = new {
     val core = dep("org.typelevel" %%% "cats-core" % "1.4.0")
-    val kittens = dep("org.typelevel" %%% "kittens" % "1.0.0")
+    val kittens = dep("org.typelevel" %%% "kittens" % "1.2.0")
   }
   val akka = new {
     private val version = "2.5.17"
