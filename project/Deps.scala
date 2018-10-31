93f85271a9ef146f6451284210db06fd14071eb7
Felix Dietze <felix.dietze@rwth-aachen.de>
2018-10-31 13:24:04 +0100

    Update webpack to 4.23.1, devserver to 3.1.10

--- project/Deps.scala
+++ project/Deps.scala
@@ -109,8 +109,8 @@ object Deps {
   val stringmetric = dep("io.github.GRBurst.stringmetric" %% "stringmetric-core" % "91e2a03")
   //  val stringmetric = dep("com.rockymadden.stringmetric" %% "stringmetric-core" % "0.28.0-SNAPSHOT")
 
-  val webpackVersion = "4.20.2"
-  val webpackDevServerVersion = "3.1.9"
+  val webpackVersion = "4.23.1"
+  val webpackDevServerVersion = "3.1.10"
 
   object npm {
     val defaultPassiveEvents = "default-passive-events" -> "1.0.10"
