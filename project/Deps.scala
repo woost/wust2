a2d3c3bf95f07f580b8cc3495a181dc68240fd79
johannes karoff <johannes@karoff.net>
2018-11-14 17:32:45 +0100

    update covenant and thereby reconnecting-websocket

--- project/Deps.scala
+++ project/Deps.scala
@@ -77,7 +77,7 @@ object Deps {
 
   // rpc
   val covenant = new {
-    private val version = "1edde27"
+    private val version = "bcee98e"
     val core = dep("com.github.cornerman.covenant" %%% "covenant-core" % version)
     val ws = dep("com.github.cornerman.covenant" %%% "covenant-ws" % version)
     val http = dep("com.github.cornerman.covenant" %%% "covenant-http" % version)
