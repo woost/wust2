d3d53fdd68e38401426a0ca45c9c6c21a77ecd0a
Felix Dietze <felix.dietze@rwth-aachen.de>
2018-11-15 15:52:21 +0100

    Update Quill to 2.6.0

--- project/Deps.scala
+++ project/Deps.scala
@@ -92,7 +92,7 @@ object Deps {
   val oAuthClient = dep("com.github.GRBurst" % "akka-http-oauth2-client" % "260bf29")
 
   // database
-  val quill = dep("io.getquill" %% "quill-async-postgres" % "2.5.4")
+  val quill = dep("io.getquill" %% "quill-async-postgres" % "2.6.0")
 
   // interfaces
   //val github4s = dep("com.47deg" %% "github4s" % "0.17.0") // only temporarly here
