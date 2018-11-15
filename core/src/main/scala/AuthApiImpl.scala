cd2947d4422d83d61c794ceadb608b31ce2eda26
johannes karoff <johannes@karoff.net>
2018-11-15 14:11:45 +0100

    do not swallow exceptions on register

--- core/src/main/scala/AuthApiImpl.scala
+++ core/src/main/scala/AuthApiImpl.scala
@@ -32,8 +32,8 @@ class AuthApiImpl(dsl: GuardDsl, db: Db, jwt: JWT)(implicit ec: ExecutionContext
           db.user.activateImplicitUser(prevUserId, name, digest)
         }
       case Some(AuthUser.Assumed(userId)) =>
-        db.user.create(userId, name, digest).map(Some(_)).recover{case NonFatal(_) => None }
-      case _ => db.user.create(UserId.fresh, name, digest).map(Some(_)).recover{case NonFatal(_) => None }
+        db.user.create(userId, name, digest).map(Some(_))
+      case _ => db.user.create(UserId.fresh, name, digest).map(Some(_))
     }
 
     val newAuth = newUser.map(_.map(u => jwt.generateAuthentication(u)).toRight(AuthResult.BadUser))
