92b8bf9d5707743a8fe902db5a3be8673527963e
johannes karoff <johannes@karoff.net>
2018-11-15 17:04:11 +0100

    fix serviceworker showing initial update app

--- webApp/src/main/scala/jsdom/ServiceWorker.scala
+++ webApp/src/main/scala/jsdom/ServiceWorker.scala
@@ -20,16 +20,15 @@ object ServiceWorker {
         sw.register("sw.js").toFuture.onComplete {
           case Success(registration) =>
             console.log("SW registered: ", registration)
-            registration.onupdatefound = {
-              event =>
-                val installingWorker = registration.installing
-                installingWorker.onstatechange = { event =>
-                  console.log("Update of SW found", installingWorker, Navigator.serviceWorker.get.controller)
-                  if (installingWorker.state == "installed" && Navigator.serviceWorker.get.controller.asInstanceOf[js.UndefOr[OriginalServiceWorker]].isDefined) {
-                    console.log("New SW installed, can update.")
-                    subject.onNext(())
-                  }
+            registration.onupdatefound = { event =>
+              val installingWorker = registration.installing
+              installingWorker.onstatechange = { event =>
+                val activeServiceworker = Navigator.serviceWorker.get.controller
+                if (installingWorker.state == "installed" && activeServiceworker != null) {
+                  console.log("New SW installed, can update.")
+                  subject.onNext(())
                 }
+              }
             }
           case Failure(registrationError) =>
             scribe.warn("SW registration failed: ", registrationError)
