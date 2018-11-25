edde2c4947290eddd75404a698cc5adbdbc3e784
johannes karoff <johannes@karoff.net>
2018-11-25 20:05:49 +0100

    fix flatland dependency with sbt assembly

--- project/Deps.scala
+++ project/Deps.scala
@@ -74,7 +74,7 @@ object Deps {
     val core = dep("com.nrinaudo" %%% "kantan.regex" % version)
     val generic = dep("com.nrinaudo" %%% "kantan.regex-generic" % version)
   }
-  val flatland = dep("com.github.fdietze" % "flatland" % "10955d1d")
+  val flatland = dep("com.github.fdietze.flatland" %%% "flatland" % "10955d1d")
 
 
   // rpc
