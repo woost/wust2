b123cecab4cf7df6873adf129f0b775b08b1a859
Felix Dietze <felix.dietze@rwth-aachen.de>
2018-11-26 16:05:11 +0100

    Welcome text in newchannel view

--- webApp/src/main/scala/views/NewChannelView.scala
+++ webApp/src/main/scala/views/NewChannelView.scala
@@ -10,6 +10,7 @@ import wust.webApp.outwatchHelpers._
 import wust.webApp.state.{GlobalState, ScreenSize, View}
 import wust.webApp.views.SharedViewElements._
 import wust.util._
+import wust.webApp.views.Components._
 
 object NewChannelView {
   def apply(state: GlobalState)(implicit ctx: Ctx.Owner) = {
@@ -17,11 +18,31 @@ object NewChannelView {
       Styles.flex,
       flexDirection.column,
       div(
+        padding := "10px",
         height := "100%",
         Styles.flex,
         justifyContent.spaceAround,
         alignItems.center,
         div(
+          Rx{
+            val user = state.user().toNode
+            VDomModifier(
+              h1(
+                "Hello ",
+                Avatar(user)(width := "1em", height := "1em", cls := "avatar", marginLeft := "0.2em", marginRight := "0.1em", marginBottom := "-3px"),
+                displayUserName(user.data),
+                "!"
+              ),
+              div(
+                maxWidth := "80ex",
+                marginBottom := "50px",
+                user.data.isImplicit.ifTrue[VDomModifier](p("You can use Woost without registration. Everything you create is private. If you want to access your data from another device, ", a(href := "#", "create an account",
+                  onClick.preventDefault(state.viewConfig.now.showViewWithRedirect(View.Signup)) --> state.viewConfig,
+                  onClick.preventDefault foreach { Analytics.sendEvent("topbar", "signup") },
+                ),"."))
+              )
+            )
+          },
           marginBottom := "10%",
           textAlign.center,
           newChannelButton(state, label = "+ New Chat", view = View.Chat).apply(padding := "20px", margin := "20px 40px")(
