9ad71ce42aa10f993ccbfbefaf0406dacc38dacf
johannes karoff <johannes@karoff.net>
2018-12-02 00:14:34 +0100

    do not autoclose email notify

--- webApp/src/main/scala/state/GlobalStateFactory.scala
+++ webApp/src/main/scala/state/GlobalStateFactory.scala
@@ -51,7 +51,7 @@ object GlobalStateFactory {
         Client.auth.getUserDetail(user.id).onComplete {
           case Success(Some(detail)) =>
             if(detail.email.isEmpty)
-              UI.toast("You do not have an Email setup, please update your Profile.", click = () => state.view() = View.UserSettings, level = UI.ToastLevel.Warning)
+              UI.toast("You do not have an Email setup, please update your Profile.", click = () => state.view() = View.UserSettings, autoclose = false, level = UI.ToastLevel.Warning)
           case err                   =>
             scribe.info(s"Cannot get User Details: ${ err }")
         }
