7755ebea368b4c7e4876bc9784542b7fba42ca28
GRBurst <GRBurst@protonmail.com>
2018-12-11 20:02:35 +0100

    Fix BrowserSpec title

--- systemTest/src/it/scala/BrowserSpec.scala
+++ systemTest/src/it/scala/BrowserSpec.scala
@@ -2,7 +2,7 @@ import org.specs2.mutable
 
 class BrowserSpec extends mutable.Specification with WustReady {
   "run without errors" in new Browser {
-    browser.getTitle mustEqual "Welcome - Woost"
+    browser.getTitle mustEqual "Woost"
     // wait for potential errors
     Thread.sleep(1000)
     browser.errors mustEqual Nil
