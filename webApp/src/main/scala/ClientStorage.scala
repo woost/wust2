ef3de3024d5040e941a488bd1f388ea8f9ec9edf
GRBurst <GRBurst@protonmail.com>
2018-12-19 18:15:11 +0100

    do not crash on localstorage access

--- webApp/src/main/scala/ClientStorage.scala
+++ webApp/src/main/scala/ClientStorage.scala
@@ -1,8 +1,10 @@
 package wust.webApp
 
+import googleAnalytics.Analytics
 import io.circe._
 import io.circe.parser._
 import io.circe.syntax._
+import monix.execution.Ack
 import outwatch.dom._
 import outwatch.util.LocalStorage
 import rx._
@@ -11,8 +13,10 @@ import wust.api.serialize.Circe._
 import wust.graph.GraphChanges
 import wust.webApp.outwatchHelpers._ //TODO use outwatch.util.Storage(dom.Storage)
 
+import scala.util.{Failure, Success, Try}
+
 class ClientStorage(implicit owner: Ctx.Owner) {
-  private val internal = org.scalajs.dom.ext.LocalStorage
+  import org.scalajs.dom.ext.{LocalStorage => internal}
 
   object keys {
     val auth = "wust.auth"
@@ -24,35 +28,58 @@ class ClientStorage(implicit owner: Ctx.Owner) {
   private def toJson[T: Encoder](value: T): String = value.asJson.noSpaces
   private def fromJson[T: Decoder](value: String): Option[T] = decode[T](value).right.toOption
 
+  val canAccessLs: Boolean = {
+    Try {
+      val woostStr = "woost"
+      internal.update(woostStr, woostStr)
+      internal.remove(woostStr)
+    } match {
+      case Success(_) =>
+        true
+      case Failure(_) =>
+        Analytics.sendEvent("localstorage", "access", "error")
+        scribe.warn("Beware: Localstorage is not accessible!")
+        false
+    }
+  }
+
   val auth: Var[Option[Authentication]] = {
-    LocalStorage
-      .handlerWithoutEvents(keys.auth)
-      .unsafeRunSync()
-      .mapHandler[Option[Authentication]](auth => Option(toJson(auth)))(_.flatMap(fromJson[Authentication]))
-      .unsafeToVar(internal(keys.auth).flatMap(fromJson[Authentication]))
+    if(canAccessLs) {
+      LocalStorage
+        .handlerWithoutEvents(keys.auth)
+        .unsafeRunSync()
+        .mapHandler[Option[Authentication]](auth => Option(toJson(auth)))(_.flatMap(fromJson[Authentication]))
+        .unsafeToVar(internal(keys.auth).flatMap(fromJson[Authentication]))
+    } else Var(None)
   }
 
   //TODO: howto handle with events from other tabs?
   val graphChanges: Handler[List[GraphChanges]] = {
-    LocalStorage
-      .handlerWithoutEvents(keys.graphChanges)
-      .unsafeRunSync()
-      .mapHandler[List[GraphChanges]](changes => Option(toJson(changes)))(_.flatMap(fromJson[List[GraphChanges]]).getOrElse(Nil))
+    if(canAccessLs) {
+      LocalStorage
+        .handlerWithoutEvents(keys.graphChanges)
+        .unsafeRunSync()
+        .mapHandler[List[GraphChanges]](changes => Option(toJson(changes)))(_.flatMap(fromJson[List[GraphChanges]]).getOrElse(Nil))
+    } else Handler.unsafe[List[GraphChanges]]
   }
 
   val sidebarOpen: Var[Option[Boolean]] = {
-    LocalStorage
-      .handlerWithoutEvents(keys.sidebarOpen)
-      .unsafeRunSync()
-      .mapHandler[Option[Boolean]](open => Option(toJson(open)))(_.flatMap(fromJson[Boolean]))
-      .unsafeToVar(internal(keys.sidebarOpen).flatMap(fromJson[Boolean]))
+    if(canAccessLs) {
+      LocalStorage
+        .handlerWithoutEvents(keys.sidebarOpen)
+        .unsafeRunSync()
+        .mapHandler[Option[Boolean]](open => Option(toJson(open)))(_.flatMap(fromJson[Boolean]))
+        .unsafeToVar(internal(keys.sidebarOpen).flatMap(fromJson[Boolean]))
+    } else Var(None)
   }
 
   val backendTimeDelta: Var[Long] = {
-    LocalStorage
-      .handlerWithoutEvents(keys.backendTimeDelta)
-      .unsafeRunSync()
-      .mapHandler[Long](delta => Option(toJson(delta)))(_.flatMap(fromJson[Long]).getOrElse(0L))
-      .unsafeToVar(internal(keys.backendTimeDelta).flatMap(fromJson[Long]).getOrElse(0L))
+    if(canAccessLs) {
+      LocalStorage
+        .handlerWithoutEvents(keys.backendTimeDelta)
+        .unsafeRunSync()
+        .mapHandler[Long](delta => Option(toJson(delta)))(_.flatMap(fromJson[Long]).getOrElse(0L))
+        .unsafeToVar(internal(keys.backendTimeDelta).flatMap(fromJson[Long]).getOrElse(0L))
+    } else Var(0L)
   }
 }
