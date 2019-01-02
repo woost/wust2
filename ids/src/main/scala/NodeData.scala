b9e6793bbec3a60189e89730e3d7849f3ad41d71
GRBurst <GRBurst@protonmail.com>
2019-01-02 15:07:45 +0100

    Property nodes

--- ids/src/main/scala/NodeData.scala
+++ ids/src/main/scala/NodeData.scala
@@ -2,6 +2,8 @@ package wust.ids
 
 import supertagged._
 
+import scala.util.Try
+
 sealed trait NodeData {
   def str: String //TODO: define this properly
   val tpe: NodeData.Type
@@ -14,6 +16,9 @@ object NodeData {
     val tpe = Type(name.value)
   }
 
+  sealed trait PropertyValue {
+//    def constraint: Boolean
+  }
   sealed trait Content extends NodeData {
     def updateStr(str: String): Option[Content]
   }
@@ -30,14 +35,42 @@ object NodeData {
   }
   object Markdown extends Named
 
-  case class PlainText(content: String) extends Named with Content {
+  case class PlainText(content: String) extends Named with Content with PropertyValue {
     def str = content
-    override def updateStr(str: String) = if (content != str) Some(copy(content = str)) else None
+    @inline private def update(str: String) = if (content != str) Some(copy(content = str)) else None
+    override def updateStr(str: String): Option[Content] = update(str)
   }
   object PlainText extends Named
+
+  case class Integer(content: Int) extends Named with Content with PropertyValue {
+    require(content <= Int.MaxValue && content >= Int.MinValue)
+    def str = content.toString
+    override def updateStr(str: String): Option[Content] =
+      Try(str.trim.toInt).toOption.flatMap(value => if(content != value) Some(copy(content = value)) else None) // FIXME: do not use Try
+  }
+  object Integer extends Named
+
+  case class Float(content: Double) extends Named with Content with PropertyValue {
+    require(content <= Double.MaxValue && content >= Double.MinValue)
+    def str = content.toString
+    override def updateStr(str: String): Option[Content] =
+      Try(str.trim.toDouble).toOption.flatMap(value => if(content != value) Some(copy(content = value)) else None) // FIXME: do not use Try
+  }
+  object Float extends Named
+
+  case class Date(content: EpochMilli) extends Named with Content with PropertyValue {
+    def plainStr = content.toString
+    def str = content.humanReadable
+    override def updateStr(str: String): Option[Content] =
+      Try(EpochMilli(str.trim.toLong)).toOption.flatMap(value => if(content != value) Some(copy(content = value)) else None)
+  }
+  object Date extends Named
+
 
   object User extends Named
   case class User(name: String, isImplicit: Boolean, revision: Int) extends Named with NodeData {
     def str = name
   }
+
 }
+
