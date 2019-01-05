a59f9a29d0e5e712647042a0a25d44484d010165
GRBurst <GRBurst@protonmail.com>
2019-01-05 11:55:36 +0100

    Detail view for nodes

---
+++ webApp/src/main/scala/views/DetailView.scala
@@ -0,0 +1,88 @@
+package wust.webApp.views
+
+import outwatch.dom._
+import outwatch.dom.dsl._
+import rx._
+import wust.graph._
+import wust.webApp.outwatchHelpers._
+import wust.webApp.state.GlobalState
+import wust.webApp.views.Components._
+
+object DetailView {
+
+  def apply(state: GlobalState)(implicit ctx: Ctx.Owner): VNode = {
+    @inline def renderAsTag(tags: Seq[Node])(implicit ctx: Ctx.Owner) = {
+      div(
+        if(tags.nonEmpty) tags.map(t => nodeTag(state, t))
+        else "-"
+      )
+    }
+    //    @inline def renderAsRemovableTag(tags: Seq[Node]) = {
+    //      if(tags.nonEmpty) tags.map(tag => removableNodeTag(state, tag, taggedNodeId = parentId))
+    //      else "-"
+    //    }
+
+    // View elements
+    val list = div(cls := "ui list")
+    val item = div(cls := "item")
+    val content = div(cls := "content")
+    val header = div(cls := "header")
+    val description = div(cls := "description")
+
+    div(
+      overflow.auto,
+      padding := "10px",
+
+      Rx {
+        // Data
+        state.page().parentId map { subjectId =>
+          val graph = state.graph()
+          val subjectIdx = graph.idToIdx(subjectId)
+          val subject = graph.nodes(subjectIdx)
+          val tags = graph.tagParentsIdx(subjectIdx).map(graph.nodes)
+          val properties = graph.propertiesEdgeIdx(subjectIdx).map(eidx => graph.edges(eidx).asInstanceOf[Edge.LabeledProperty]).map(e => (e.data.key, graph.nodesById(e.targetId).str))
+
+          val parents = graph.parentsIdx(subjectIdx).map(graph.nodes)
+          val children = graph.childrenIdx(subjectIdx).map(graph.nodes)
+
+          list(
+            item(
+              content(
+                header("Data"),
+                description(subject.data.str)
+              ),
+              content(
+                header("Role"),
+                description(subject.role.toString)
+              ),
+              content(
+                header("Access"),
+                description(subject.meta.accessLevel.str)
+              ),
+              content(
+                header("Porperties"),
+                description(
+                  if(properties.nonEmpty) properties.map{ case (propertyKey: String, propertyValue: String) => s"$propertyKey: $propertyValue"}.mkString(", ")
+                  else "-"
+                )
+              ),
+              content(
+                header("Tags"),
+                description(renderAsTag(tags))
+              ),
+              content(
+                header("Parents"),
+                description(description(renderAsTag(parents)))
+              ),
+              content(
+                header("Children"),
+                description(description(renderAsTag(children)))
+              ),
+            )
+          )
+        },
+      }
+    )
+  }
+}
+
