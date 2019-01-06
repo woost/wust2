0752b99023c4f41a44db17f90cf12003fda6d25d
GRBurst <GRBurst@protonmail.com>
2019-01-06 13:01:06 +0100

    Fix multiple "/ /" in breadcrumb on empty VNodeModifiers

--- webApp/src/main/scala/views/BreadCrumbs.scala
+++ webApp/src/main/scala/views/BreadCrumbs.scala
@@ -24,6 +24,13 @@ object BreadCrumbs {
 //    case Nil                => Nil
     case short => short
   }
+  private def intersperseWhile[T](list: List[T], co: T, cond: T => Boolean): List[T] = list match {
+    case one :: two :: rest if cond(one) => one :: co :: intersperseWhile(two :: rest, co, cond)
+    case _ :: two :: rest => intersperseWhile(two :: rest, co, cond)
+    //    case one :: Nil         => one :: co :: Nil
+    //    case Nil                => Nil
+    case short => short
+  }
 
   private def cycleIndicator(rotate : Boolean) = {
     //"\u21ba"
@@ -45,39 +52,37 @@ object BreadCrumbs {
           page.parentId.map { (parentId: NodeId) =>
             val parentDepths = graph.parentDepths(parentId)
             val distanceToNodes = parentDepths.toList.sortBy { case (depth, _) => -depth }
-            val elements = distanceToNodes.flatMap { case (distance, gIdToNodeIds) =>
+            def elements = distanceToNodes.flatMap { case (distance, gIdToNodeIds) =>
               // when distance is 0, we are either showing ourselves (i.e. id) or
               // a cycle that contains ourselves. The latter case we want to draw, the prior not.
               if(!showOwn && distance == 0 && gIdToNodeIds.size == 1 && gIdToNodeIds.head._2.size == 1)
                 None
               else {
                 val sortedByGroupId = gIdToNodeIds.toList.sortBy(_._1)
-                Some(span(
+                Some(
                   // "D:" + distance + " ",
-                  sortedByGroupId.map { case (gId, nodes) =>
+                  sortedByGroupId.flatMap { case (gId, nodes) =>
                     // sort nodes within a group by their length towards the root node
                     // this ensures that e.g. „Channels“ comes first
                     val sortedNodes = nodes.sortBy(graph.parentDepth(_))
-                    span(
-                      cls := "breadcrumb",
-                      if(gId != -1) cycleIndicator(false) else "",
-                      sortedNodes.map { (nid: NodeId) =>
-                        graph.nodesByIdGet(nid) match {
-                          // hiding the stage prevents accidental zooming into stages, which in turn prevents to create inconsistent state.
-                          // example of unwanted inconsistent state: task is only child of stage, but child of nothing else.
-                          case Some(node) if (showOwn || nid != parentId) && node.role != NodeRole.Stage =>
-                            nodeTag(state, node, dragOptions = nodeId => drag(DragItem.BreadCrumb(nodeId)))(cursor.pointer)
-                          case _                                                  =>
-                            VDomModifier.empty
-                        }
-                      },
-                      if(gId != -1) cycleIndicator(true) else "",
-                    )
+                    sortedNodes.map { nid: NodeId =>
+                      graph.nodesByIdGet(nid) match {
+                        // hiding the stage prevents accidental zooming into stages, which in turn prevents to create inconsistent state.
+                        // example of unwanted inconsistent state: task is only child of stage, but child of nothing else.
+                        case Some(node) if (showOwn || nid != parentId) && node.role != NodeRole.Stage =>
+                          span(
+                            cls := "breadcrumb",
+                            nodeTag(state, node, dragOptions = nodeId => drag(DragItem.BreadCrumb(nodeId)))(cursor.pointer),
+                          )
+                        case _                                                  =>
+                          VDomModifier.empty
+                      }
+                    }
                   }.toSeq
-                ))
+                )
               }
-            }
-            div(intersperse(elements, span("/", cls := "divider")))
+            }.flatten
+            div(intersperseWhile(elements, span("/", cls := "divider"), (mod: VDomModifier) => !mod.isInstanceOf[outwatch.dom.EmptyModifier.type]))
           }
         },
         registerDragContainer(state),
