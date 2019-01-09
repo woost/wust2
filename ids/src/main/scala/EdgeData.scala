8ab640a748257048eb4f8e83e3cceea27ec783b4
GRBurst <GRBurst@protonmail.com>
2019-01-09 09:46:56 +0100

    remove accept type from property edge

--- ids/src/main/scala/EdgeData.scala
+++ ids/src/main/scala/EdgeData.scala
@@ -51,7 +51,7 @@ object EdgeData {
   trait PropertyKey
   case object Assigned extends Named with EdgeData with PropertyKey
 
-  case class LabeledProperty(key: String, acceptType: String) extends Named with EdgeData with PropertyKey
+  case class LabeledProperty(key: String) extends Named with EdgeData with PropertyKey
   object LabeledProperty extends Named
 
 }
