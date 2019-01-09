66a031c33cd847c2b3b648ccc708d0616e9f9336
GRBurst <GRBurst@protonmail.com>
2019-01-09 11:35:59 +0100

    Only catch nonfatal in save string ops

--- webUtil/src/main/scala/StringOps.scala
+++ webUtil/src/main/scala/StringOps.scala
@@ -3,11 +3,13 @@ package wust.webUtil
 import scala.scalajs.js
 import wust.ids.EpochMilli
 
+import scala.util.control.NonFatal
+
 object StringOps {
   @inline def toEpoch(epochString: String): EpochMilli = {
     EpochMilli(new js.Date(epochString).getTime.toLong)
   }
   @inline def safeToEpoch(epochString: String): Option[EpochMilli] = {
-    try { Some(EpochMilli(new js.Date(epochString).getTime.toLong)) } catch { case _: Throwable => None }
+    try { Some(EpochMilli(new js.Date(epochString).getTime.toLong)) } catch { case NonFatal(_) => None }
   }
 }
