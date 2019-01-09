589f4bc7c275d61a4cbb3c645cf8f933f5695100
johannes karoff <johannes@karoff.net>
2019-01-09 19:57:53 +0100

    poor man's "go-back to previous page"

--- webApp/src/main/scala/state/ViewConfig.scala
+++ webApp/src/main/scala/state/ViewConfig.scala
@@ -2,7 +2,6 @@ package wust.webApp.state
 
 import wust.api.Authentication
 import wust.graph._
-import wust.ids.NodeId
 import wust.webApp.parsers.{ViewConfigParser, ViewConfigWriter}
 
 case class ShareOptions(title: String, text: String, url: String)
@@ -10,6 +9,8 @@ case class ShareOptions(title: String, text: String, url: String)
 //TODO: get rid of pagechange, currently needed to know whether we should get a new graph on page change or not.
 // we only know whether we need this when changing the page. But it feels like mixing data and commands.
 case class PageChange(page: Page, needsGet: Boolean = true)
+
+case class ViewConfig(view: Option[View], pageChange: PageChange, redirectTo: Option[View], shareOptions: Option[ShareOptions], invitation: Option[Authentication.Token], prevPage: Option[Page]) {
   private val canRedirectTo: View => Boolean = {
     case View.Login | View.Signup => false
     case _ => true
@@ -21,15 +22,20 @@ case class PageChange(page: Page, needsGet: Boolean = true)
 
   def redirect: ViewConfig = redirectTo.fold(this)(view => copy(view = Some(view), redirectTo = None))
 
-  def focusView(page: Page, view:View, needsGet: Boolean = true): ViewConfig = {
-    focus(page, Some(view), needsGet)
+  @inline def goBackOrEmptyPage: ViewConfig = {
+    prevPage match {
+      case Some(prevPage) => copy(pageChange = PageChange(prevPage), prevPage = None)
+      case None => copy(pageChange = PageChange(Page.empty))
     }
+  }
+
+  @inline def focusView(page: Page, view:View, needsGet: Boolean = true): ViewConfig = focus(page, Some(view), needsGet)
   def focus(page: Page, view:Option[View] = None, needsGet: Boolean = true): ViewConfig = {
-    copy(pageChange = PageChange(page, needsGet = needsGet), view = view, redirectTo = None)
+    copy(pageChange = PageChange(page, needsGet = needsGet), view = view, redirectTo = None, prevPage = Some(page))
   }
 }
 object ViewConfig {
-  val default = ViewConfig(view = None, PageChange(Page.empty), None, None, None)
+  val default = ViewConfig(view = None, PageChange(Page.empty), None, None, None, None)
 
   def fromUrlHash(hash: String): ViewConfig = ViewConfigParser.parse(hash)
 
