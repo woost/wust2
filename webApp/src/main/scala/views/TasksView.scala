f795b6443411092f7ca49ef16423ddfa91a8a025
Felix Dietze <felix.dietze@rwth-aachen.de>
2019-01-09 23:13:44 +0100

    Fix TasksView heuristic

--- webApp/src/main/scala/views/TasksView.scala
+++ webApp/src/main/scala/views/TasksView.scala
@@ -15,28 +15,24 @@ import wust.util._
 // Combines task list and kanban-view into one view with a kanban-switch
 object TasksView {
   def apply(state: GlobalState)(implicit ctx: Ctx.Owner): VNode = {
-    val topLevelStageExists = Rx {
-      // TODO: we should not react on graph, but on the result of a getGraph Request.
-      val page = state.page()
-      val graph = state.graph()
+    def topLevelStageExists(page:Page, graph:Graph) = {
       page.parentId.fold(false) { parentId =>
         val parentIdx = graph.idToIdx(parentId)
         val workspacesIdx = graph.workspacesForParent(parentIdx)
         val doneNodes:Array[Int] = workspacesIdx.flatMap(workspaceIdx => graph.doneNodeForWorkspace(workspaceIdx))
         graph.notDeletedChildrenIdx.exists(parentIdx) { childIdx =>
           val node = graph.nodes(childIdx)
-          val res = node.role == NodeRole.Stage && !doneNodes.contains(childIdx)
-          res
+          node.role == NodeRole.Stage && !doneNodes.contains(childIdx)
         }
       }
     }
-    
+
     val kanbanSwitch = Var(false)
 
     var lastPage = Page.empty
-    state.graph.foreach{ _ =>
+    state.rawGraph.foreach{ graph =>
       if(lastPage != state.page.now) {
-        kanbanSwitch() = topLevelStageExists.now
+        kanbanSwitch() = topLevelStageExists(state.page.now, graph)
         lastPage = state.page.now
       }
     }
