ee1fbab9a167b97f3247194856e97a4c4b5b087c
GRBurst <GRBurst@protonmail.com>
2019-01-11 19:23:00 +0100

    ContentRole trait for tasks + messages

--- ids/src/main/scala/NodeRole.scala
+++ ids/src/main/scala/NodeRole.scala
@@ -4,13 +4,15 @@ import wust.util.macros.SubObjects
 
 sealed trait NodeRole extends Product with Serializable
 object NodeRole {
-  case object Message extends NodeRole
+  sealed trait ContentRole extends NodeRole
+  case object Message extends ContentRole
+  case object Task extends ContentRole
+
   case object Property extends NodeRole
   case object Stage extends NodeRole { override def toString: String = "Stage (Column)" }
-  case object Task extends NodeRole
   case object Tag extends NodeRole
 
-  @inline def default:NodeRole = Message
+  @inline def default: NodeRole = Message
 
   def all: List[NodeRole] = macro SubObjects.list[NodeRole]
 }
