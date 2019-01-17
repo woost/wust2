4866fb474339e491d2575d5350e1d7a11056e28b
GRBurst <GRBurst@protonmail.com>
2019-01-17 18:55:06 +0100

    fix saveToDouble

--- util/shared/src/main/scala/StringOps.scala
+++ util/shared/src/main/scala/StringOps.scala
@@ -16,7 +16,7 @@ object StringOps {
   @inline def safeToInt(intStr: String): Option[Int] = {
     try { Some(intStr.toInt) } catch { case NonFatal(_) => None }
   }
-  @inline def safeToDouble(doubleStr: String): Option[Int] = {
-    try { Some(doubleStr.toInt) } catch { case NonFatal(_) => None }
+  @inline def safeToDouble(doubleStr: String): Option[Double] = {
+    try { Some(doubleStr.toDouble) } catch { case NonFatal(_) => None }
   }
 }
