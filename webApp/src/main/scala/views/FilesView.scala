53ac3060d263c42acf704f156fcc45597349f00d
johannes karoff <johannes@karoff.net>
2019-01-24 16:32:53 +0100

    file upload button in files view

--- webApp/src/main/scala/views/FilesView.scala
+++ webApp/src/main/scala/views/FilesView.scala
@@ -1,5 +1,6 @@
 package wust.webApp.views
 
+import monix.execution.Ack
 import outwatch.dom._
 import outwatch.dom.dsl._
 import outwatch.dom.helpers.EmitterBuilder
@@ -12,6 +13,8 @@ import wust.webApp.state.GlobalState
 import wust.webApp.views.Components._
 import wust.util._
 
+import scala.concurrent.Future
+
 object FilesView {
   def apply(state: GlobalState)(implicit ctx: Ctx.Owner): VNode = {
     val files: Rx.Dynamic[Seq[(NodeId, NodeData.File)]] = Rx {
@@ -22,20 +25,35 @@ object FilesView {
 
     }
 
+    val fileUploadHandler = Var[Option[AWS.UploadableFile]](None)
+
+    fileUploadHandler.foreach(_.foreach { uploadFile =>
+      SharedViewElements.uploadFileAndCreateNode(state, "", state.page.now.parentId, uploadFile)
+      fileUploadHandler() = None
+    })
+
     div(
       padding := "20px",
       overflow.auto,
-      files.map { files =>
-        if (files.isEmpty) p("There are no files in this workspace, yet.", color := "grey")
-        else div(
-          Styles.flex,
-          flexDirection.row,
-          flexWrap.wrap,
-          files.map { case (id, file) =>
-            Components.renderUploadedFile(state, id, file).apply(cls := "ui bordered medium", padding := "10px", margin := "10px", border := "2px solid grey", borderRadius := "10px")
-          }
-        )
-      }
+      p(
+        marginBottom := "20px",
+        Components.uploadField(state, fileUploadHandler),
+      ),
+      UI.horizonalDivider("Files")(marginTop := "20px", marginBottom := "20px"),
+      div(
+        marginTop := "20px",
+        files.map { files =>
+          if (files.isEmpty) p("There are no files in this workspace, yet.", color := "grey")
+          else div(
+            Styles.flex,
+            flexDirection.row,
+            flexWrap.wrap,
+            files.map { case (id, file) =>
+              Components.renderUploadedFile(state, id, file).apply(cls := "ui bordered medium", padding := "10px", margin := "10px", border := "2px solid grey", borderRadius := "10px")
+            }
+          )
+        }
+      )
     )
   }
 }
