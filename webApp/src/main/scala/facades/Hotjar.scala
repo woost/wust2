228ca61cd83210d31b1efb1dca23484e74dfeff1
Felix Dietze <felix.dietze@rwth-aachen.de>
2019-02-01 16:58:13 +0100

    Fix warning

--- webApp/src/main/scala/facades/Hotjar.scala
+++ webApp/src/main/scala/facades/Hotjar.scala
@@ -1,6 +1,6 @@
 
 import org.scalajs.dom
-import org.scalajs.dom.{MouseEvent, html}
+import org.scalajs.dom.{ MouseEvent, html }
 import org.scalajs.dom.raw.NodeList
 
 import scala.scalajs.js
@@ -11,17 +11,16 @@ package hotjar {
   @js.native
   @JSGlobalScope
   object GlobalScope extends js.Object {
-    def hj(what:String, page:String) = js.native
+    def hj(what: String, page: String): Unit = js.native
   }
 }
 
 package object hotjar {
   // https://help.hotjar.com/hc/en-us/articles/115011805448-Tracking-Virtual-Page-Views
-  def pageView(page:String) = {
+  def pageView(page: String) = {
     // if hj is not defined, do nothing
-    if(GlobalScope.asInstanceOf[js.Dynamic].hj.asInstanceOf[js.UndefOr[js.Dynamic]] != js.undefined) {
+    if (GlobalScope.asInstanceOf[js.Dynamic].hj.asInstanceOf[js.UndefOr[js.Dynamic]] != js.undefined) {
       GlobalScope.hj("vpv", page)
     }
   }
 }
-
