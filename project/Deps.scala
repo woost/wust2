2ae1486956846bedd5a495044fda329c659787ee
Felix Dietze <felix.dietze@rwth-aachen.de>
2019-02-04 14:57:42 +0100

    fix reordering in repairdom

--- project/Deps.scala
+++ project/Deps.scala
@@ -52,7 +52,7 @@ object Deps {
   // val scalarx = dep("com.lihaoyi" %%% "scalarx" % "0.4.0")
   val scalarx = dep("com.github.fdietze.duality" %%% "scalarx" % "a15d3ae")
   // val scalarx = dep("com.github.fdietze.duality" %%% "scalarx" % "94c6d80") // jitpack cannot handle the . in repo name scala.rx
-  val outwatch = dep("com.github.outwatch" % "outwatch" % "bd8ca5a")
+  val outwatch = dep("com.github.outwatch" % "outwatch" % "a36c938")
   // val outwatch = dep("io.github.outwatch" %%% "outwatch" % "0.11.1-SNAPSHOT")
 
   // utility
