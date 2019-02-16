50cd14042e0ccdad9e0f2f0ba8e5dbb94e889bd8
johannes karoff <johannes@karoff.net>
2019-02-16 19:10:15 +0100

    fix viewrender without parentid

--- webApp/src/main/scala/views/ViewRender.scala
+++ webApp/src/main/scala/views/ViewRender.scala
@@ -8,19 +8,30 @@ import wust.webApp.state.GlobalState
 import wust.webApp.views.graphview.GraphView
 
 object ViewRender {
-  def apply(state: GlobalState, focusState: FocusState, view: View.Visible)(implicit ctx: Ctx.Owner): VNode = view match {
-    case View.Chat             => ChatView(state, focusState)
-    case View.Thread           => ThreadView(state, focusState)
-    case View.List             => ListView(state, focusState)
-    case View.Kanban           => KanbanView(state, focusState)
-    case View.Graph            => GraphView(state, focusState)
-    case View.Dashboard        => DashboardView(state, focusState)
-    case View.Welcome          => WelcomeView(state)
-    case View.Files            => FilesView(state, focusState)
-    case View.Login            => AuthView.login(state)
-    case View.Signup           => AuthView.signup(state)
-    case View.UserSettings     => UserSettingsView(state)
-    case View.Empty            => dsl.div
-    case View.Tiled(op, views) => TiledView(op, views.map(ViewRender(state, focusState, _)), state)
+
+  def apply(state: GlobalState, focusState: FocusState, view: View.Visible)(implicit ctx: Ctx.Owner): VNode = apply(state, Some(focusState), view)
+  def apply(state: GlobalState, focusState: Option[FocusState], view: View.Visible)(implicit ctx: Ctx.Owner): VNode = {
+    def emptyView = dsl.div
+    def withoutFocusState: PartialFunction[View.Visible, VNode] = {
+      case View.Login            => AuthView.login(state)
+      case View.Signup           => AuthView.signup(state)
+      case View.UserSettings     => UserSettingsView(state)
+      case View.Welcome          => WelcomeView(state)
+      case View.Tiled(op, views) => TiledView(op, views.map(ViewRender(state, focusState, _)), state)
+      case View.Empty            => emptyView
+    }
+    def withFocusState(focusState: FocusState): PartialFunction[View.Visible, VNode] = withoutFocusState orElse {
+      case View.Chat             => ChatView(state, focusState)
+      case View.Thread           => ThreadView(state, focusState)
+      case View.List             => ListView(state, focusState)
+      case View.Kanban           => KanbanView(state, focusState)
+      case View.Graph            => GraphView(state, focusState)
+      case View.Dashboard        => DashboardView(state, focusState)
+      case View.Files            => FilesView(state, focusState)
+    }
+
+    val renderView: PartialFunction[View.Visible, VNode] = focusState.fold(withoutFocusState)(withFocusState)
+
+    renderView.applyOrElse[View.Visible, VNode](view, _ => emptyView)
   }
 }
