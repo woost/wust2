3abc9c167a10befa23736a0258b56293faa2142d
Felix Dietze <felix.dietze@rwth-aachen.de>
2019-02-20 19:59:43 +0100

    Name prompt with dark field

--- webApp/src/main/scala/views/SharedViewElements.scala
+++ webApp/src/main/scala/views/SharedViewElements.scala
@@ -811,7 +811,7 @@ object SharedViewElements {
           showSubmitIcon = true,
           textAreaModifiers = VDomModifier(
             color.white,
-            backgroundColor := "rgba(255, 255, 255, 0.1)"
+            backgroundColor := "rgba(0, 0, 0, 0.6)"
 
           )
         ),
