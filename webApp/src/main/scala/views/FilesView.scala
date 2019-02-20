41d182fa37614b2cdfa6d994aada1eb50d5d4067
johannes karoff <johannes@karoff.net>
2019-02-20 13:18:03 +0100

    show date of file in filesview

--- webApp/src/main/scala/views/FilesView.scala
+++ webApp/src/main/scala/views/FilesView.scala
@@ -7,13 +7,17 @@ import wust.css.{Styles, ZIndex}
 import wust.ids._
 import wust.webApp.outwatchHelpers._
 import wust.webApp.state.{FocusState, GlobalState}
+import wust.webUtil.StringOps
 
 object FilesView {
   def apply(state: GlobalState, focusState: FocusState)(implicit ctx: Ctx.Owner): VNode = {
-    val files: Rx.Dynamic[Seq[(NodeId, NodeData.File)]] = Rx {
+    val files: Rx.Dynamic[Seq[(NodeId, NodeData.File, EpochMilli)]] = Rx {
       val graph = state.graph()
 
-      graph.pageFiles(focusState.focusedId).sortBy { case (id, _) => id }.reverse
+      graph.pageFiles(focusState.focusedId).map { case (id, data) =>
+        val creationDate = graph.nodeCreated(graph.idToIdx(id))
+        (id, data, creationDate)
+      }.sortBy { case (_, _, date) => date }.reverse
     }
 
     div(
@@ -27,8 +31,21 @@ object FilesView {
           Styles.flex,
           flexDirection.row,
           flexWrap.wrap,
-          files.map { case (id, file) =>
-            Components.renderUploadedFile(state, id, file).apply(cls := "ui bordered medium", padding := "10px", margin := "10px", border := "2px solid grey", borderRadius := "10px")
+          files.map { case (id, file, date) =>
+            Components.renderUploadedFile(state, id, file).apply(
+              cls := "ui bordered medium",
+              padding := "10px",
+              margin := "10px",
+              border := "2px solid grey",
+              borderRadius := "10px",
+              div(
+                color.gray,
+                width := "100%",
+                Styles.flex,
+                justifyContent.flexEnd,
+                date.isoDate
+              )
+            )
           }
         )
       }
