09f91ead409b85d03d1d0cc54a75245524f1a8cd
johannes karoff <johannes@karoff.net>
2019-03-04 12:10:12 +0100

    toString for NonEmptyString

--- ids/src/main/scala/NonEmptyString.scala
+++ ids/src/main/scala/NonEmptyString.scala
@@ -1,6 +1,8 @@
 package wust.ids
 
-class NonEmptyString private(val string: String) extends AnyVal
+class NonEmptyString private(val string: String) extends AnyVal {
+  override def toString = string
+}
 object NonEmptyString {
   def apply(string: String): Option[NonEmptyString] = if (string.isEmpty) None else Some(new NonEmptyString(string))
 }
