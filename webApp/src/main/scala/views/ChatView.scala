9ffaed98110ebedab4e92391b710f67f8f88fe65
johannes karoff <johannes@karoff.net>
2019-03-12 23:31:04 +0100

    stoppropagation for chatview reply parent buttons

--- webApp/src/main/scala/views/ChatView.scala
+++ webApp/src/main/scala/views/ChatView.scala
@@ -382,12 +382,12 @@ object ChatView {
                     )
                 }
               ),
-              div(cls := "fa-fw", freeSolid.faReply, padding := "3px 20px 3px 5px", onClick foreach { currentReply.update(_ ++ Set(parentId)) }, cursor.pointer),
-              div(cls := "fa-fw", Icons.zoom, padding := "3px 20px 3px 5px", onClick foreach {
+              div(cls := "fa-fw", freeSolid.faReply, padding := "3px 20px 3px 5px", onClick.stopPropagation foreach { currentReply.update(_ ++ Set(parentId)) }, cursor.pointer),
+              div(cls := "fa-fw", Icons.zoom, padding := "3px 20px 3px 5px", onClick.stopPropagation foreach {
                   state.urlConfig.update(_.focus(Page(node.id)))
                   selectedNodes() = Set.empty[SelectedNode]
               }, cursor.pointer),
-              pinReply.map{pinReply => div(cls := "fa-fw", freeSolid.faThumbtack, Rx { pinReply().ifFalse[VDomModifier](opacity := 0.4)}, padding := "3px 20px 3px 5px", onClick foreach { pinReply() = !pinReply.now; inputFieldFocusTrigger.onNext(()); () }, cursor.pointer)},
+              pinReply.map{pinReply => div(cls := "fa-fw", freeSolid.faThumbtack, Rx { pinReply().ifFalse[VDomModifier](opacity := 0.4)}, padding := "3px 20px 3px 5px", onClick.stopPropagation foreach { pinReply() = !pinReply.now; inputFieldFocusTrigger.onNext(()); () }, cursor.pointer)},
             )
           )
         )
