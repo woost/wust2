6588d06b796419c2831dacf535722c28d1ecddb5
Felix Dietze <felix.dietze@rwth-aachen.de>
2019-04-26 14:46:06 +0200

    Submit feedback only via button

--- webApp/src/main/scala/views/FeedbackForm.scala
+++ webApp/src/main/scala/views/FeedbackForm.scala
@@ -1,39 +1,40 @@
 package wust.webApp.views
 
+import wust.webApp.DevOnly
 import fontAwesome._
 import googleAnalytics.Analytics
-import org.scalajs.dom.window.{clearTimeout, setTimeout, navigator}
+import org.scalajs.dom.window.{ clearTimeout, setTimeout, navigator }
 import outwatch.dom._
 import outwatch.dom.dsl._
 import rx._
 import wust.api.ClientInfo
-import wust.css.{CommonStyles, Styles, ZIndex}
+import wust.css.{ CommonStyles, Styles, ZIndex }
 import wust.graph._
 import wust.ids
 import wust.ids._
-import wust.webApp.{BrowserDetect, Client, Icons}
+import wust.webApp.{ BrowserDetect, Client, Icons }
 import wust.webApp.outwatchHelpers._
-import wust.webApp.state.{GlobalState, PageChange, ScreenSize, NodePermission}
+import wust.webApp.state.{ GlobalState, PageChange, ScreenSize, NodePermission }
 import wust.webApp.views.Elements._
 import wust.util._
 
-import scala.util.{Success, Failure}
+import scala.util.{ Success, Failure }
 
 object FeedbackForm {
 
   def apply(state: GlobalState)(implicit ctx: Ctx.Owner) = {
     val show = Var(false)
-    val activeDisplay = Rx { display := (if(show()) "block" else "none") }
+    val activeDisplay = Rx { display := (if (show()) "block" else "none") }
 
     val feedbackText = Var("")
     val clear = Handler.unsafe[Unit].mapObservable(_ => "")
 
-    val initialStatus = if(BrowserDetect.isMobile) "" else "(Press Enter to submit)"
+    val initialStatus = ""
     val statusText = Var(initialStatus)
 
-    var timeout:Option[Int] = None
-    def submit():Unit = {
-      if(feedbackText.now.trim.nonEmpty) {
+    var timeout: Option[Int] = None
+    def submit(): Unit = {
+      if (feedbackText.now.trim.nonEmpty) {
         Client.api.feedback(ClientInfo(navigator.userAgent), feedbackText.now).onComplete {
           case Success(()) =>
             statusText() = "Thank you!"
@@ -42,7 +43,7 @@ object FeedbackForm {
             clear.onNext(())
           case Failure(t) =>
             scribe.warn("Error sending feedback to backend", t)
-            statusText() = "Failed to send feedback, please try again!"
+            statusText() = "Failed to send feedback, please try again or via email..."
             timeout.foreach(clearTimeout)
             timeout = Some(setTimeout(() => statusText() = initialStatus, 3000))
         }
@@ -60,7 +61,6 @@ object FeedbackForm {
         cls := "ui form",
         textArea(
           cls := "field",
-          valueWithEnter(clearValue = false) foreach { submit() },
           onInput.value --> feedbackText,
           value <-- clear,
           rows := 5, //TODO: auto expand textarea: https://codepen.io/vsync/pen/frudD
@@ -77,8 +77,8 @@ object FeedbackForm {
         "Feedback ",
         freeSolid.faCaretDown,
         cls := "ui positive tiny compact button",
-        onClick.stopPropagation foreach{
-          Analytics.sendEvent("feedback", if(show.now) "close" else "open")
+        onClick.stopPropagation foreach {
+          Analytics.sendEvent("feedback", if (show.now) "close" else "open")
           show.update(!_)
         },
         onGlobalEscape(false) --> show,
@@ -86,7 +86,7 @@ object FeedbackForm {
       ),
       div(
         activeDisplay,
-        position.fixed, top := s"${CommonStyles.topBarHeight}px", right <-- Rx{ if(state.screenSize() == ScreenSize.Small) "0px" else "100px" },
+        position.fixed, top := s"${CommonStyles.topBarHeight}px", right <-- Rx{ if (state.screenSize() == ScreenSize.Small) "0px" else "100px" },
         zIndex := ZIndex.formOverlay,
         padding := "10px", background := "#F8F8F8", border := "1px solid #888",
         feedbackForm,
@@ -100,12 +100,12 @@ object FeedbackForm {
             cls := "ui tiny compact primary button",
             "Submit",
             onClick foreach { submit() },
-            onClick(false) --> show,
+            // onClick(false) --> show,
           ),
         ),
-        onClick.stopPropagation foreach{}, // prevents closing feedback form by global click
+        onClick.stopPropagation foreach {}, // prevents closing feedback form by global click
       )
     )
   }
 
-}
+      }
