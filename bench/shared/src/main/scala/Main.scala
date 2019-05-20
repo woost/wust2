fd661a59ed7e375ce24225e93dd57179b5634481
johannes karoff <johannes@karoff.net>
2019-05-20 13:10:28 +0200

    disable logger in bench

--- bench/shared/src/main/scala/Main.scala
+++ bench/shared/src/main/scala/Main.scala
@@ -3,9 +3,13 @@ package wust.bench
 import scala.concurrent.duration._
 import bench._
 import bench.util._
+import scribe.Logger
 
 object Main {
   def main(args: Array[String]): Unit = {
+    Logger.root.clearHandlers().replace()
+
+
     runComparison(DepthFirstSearch.comparison, expRange(4000, 8), 1 minutes)
     // runComparison(GraphBenchmarks.graphAlgorithms, expRange(1000, 4), 2 minutes)
     // runComparison(GraphBenchmarks.topologicalSort, expRange(1000, 4), 2 minutes)
