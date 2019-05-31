1372d482e649a594d0873f313e96a95e73dc9da0
johannes karoff <johannes@karoff.net>
2019-05-31 03:59:25 +0200

    selectable user and add task in assignment view

--- webApp/src/main/scala/views/AssignedTasksView.scala
+++ webApp/src/main/scala/views/AssignedTasksView.scala
@@ -1,12 +1,14 @@
 package wust.webApp.views
 
+import monix.reactive.subjects.PublishSubject
 import outwatch.dom.VNode
 import outwatch.dom._
 import outwatch.dom.dsl._
 import rx._
 import wust.css.Styles
-import wust.ids.{EpochMilli, NodeId}
-import wust.webApp.state.{FocusState, GlobalState, TraverseState}
+import wust.graph.{Edge, GraphChanges, Node}
+import wust.ids.{ChildId, EpochMilli, NodeId, ParentId, UserId}
+import wust.webApp.state.{FocusState, GlobalState, Placeholder, TraverseState}
 import wust.webApp.outwatchHelpers._
 import wust.util.collection._
 import wust.webApp
@@ -34,27 +36,62 @@ object AssignedTasksView  {
       "Today",
       "Tomorrow",
       "Next Week",
+      "Next Month",
     )
     val buckets = Array[EpochMilli](
       renderTime,
       datePlusDays(renderTime, 1),
       datePlusDays(renderTime, 2),
-      datePlusDays(renderTime, 7)
+      datePlusDays(renderTime, 7),
+      datePlusDays(renderTime, 30)
     )
 
+    val selectedUserId = Var(state.userId.now)
+
+    val selectableUsers = Rx {
+      val graph = state.graph()
+      graph.members(focusState.focusedId)
+    }
+
     val assignedTasks = Rx {
-      AssignedTasksData.assignedTasks(state.graph(), state.userId(), buckets)
+      AssignedTasksData.assignedTasks(state.graph(), selectedUserId(), buckets)
     }
     val assignedTasksDue = Rx { assignedTasks().dueTasks }
     val assignedTasksOther = Rx { assignedTasks().tasks }
 
+    def addNewTask(str: String): Unit = {
+      val newTask = Node.MarkdownTask(str)
+      val changes = GraphChanges(
+        addNodes = Array(newTask),
+        addEdges = Array(
+          Edge.Child(ParentId(focusState.focusedId), ChildId(newTask.id)),
+          Edge.Assigned(newTask.id, selectedUserId.now),
+        )
+      )
+
+      state.eventProcessor.changes.onNext(changes)
+    }
+
     div(
-      keyed,
+      uniqueKeyed(focusState.focusedId.toStringFast), // needed for thunks below to be unique in nodeid
       width := "100%",
       Styles.flex,
       flexDirection.column,
       padding := "20px",
       overflow.auto,
+
+      div(
+        Styles.flex,
+        alignItems.center,
+
+        chooseUser(selectableUsers, selectedUserId).apply(Styles.flexStatic),
+
+        InputRow(
+          state,
+          addNewTask,
+          placeholder = Placeholder("Add Task"),
+        ).apply(flexGrow := 1),
+      ),
 
       Rx {
         var foundSomething = false
@@ -77,7 +114,7 @@ object AssignedTasksView  {
       Rx {
         val tasks = assignedTasksOther()
         VDomModifier.ifTrue(tasks.nonEmpty)(UI.segment(
-          "Todo",
+          VDomModifier("Todo", cls := "grey"),
           assignedTasksOther().map(renderTask(state, focusState, _)),
           segmentClass = "basic", segmentsClass = "basic"
         ))
@@ -95,4 +132,23 @@ object AssignedTasksView  {
     showCheckbox = true,
     inOneLine = true
   ).apply(margin := "8px")
+
+  private def chooseUser(users: Rx[Seq[Node.User]], selectedUserId: Var[UserId])(implicit ctx: Ctx.Owner): VNode = {
+    div(
+      Rx {
+        Avatar.user(selectedUserId()).apply(height := "20px")
+      },
+      i(cls := "dropdown icon"),
+      UI.dropdownMenu(
+        VDomModifier(
+          padding := "10px",
+          div(cls := "item", display.none), //TODO ui dropdown needs at least one element
+          users.map(_.map { user =>
+            Components.renderUser(user).apply(cursor.pointer, onClick.stopPropagation(user.id) --> selectedUserId)
+          }),
+        ),
+        close = selectedUserId.toTailObservable.map(_ => ()), dropdownModifier = cls := "top left"
+      )
+    )
+  }
 }
