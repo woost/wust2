9325407d03d3f1e34ae1264886f80cd5764989fa
Felix Dietze <felix.dietze@rwth-aachen.de>
2019-06-29 19:36:51 -0400

    Send Feedback via crisp.chat

--- webApp/src/main/scala/wust/webApp/views/FeedbackForm.scala
+++ webApp/src/main/scala/wust/webApp/views/FeedbackForm.scala
@@ -1,29 +1,33 @@
 package wust.webApp.views
 
+import wust.facades.crisp._
+import scala.util.Try
+import scala.scalajs.js
 import wust.facades.googleanalytics.Analytics
-import wust.facades.nolt.{NoltData, nolt}
+import wust.facades.nolt.{ NoltData, nolt }
 import fontAwesome._
-import org.scalajs.dom.window.{clearTimeout, navigator, setTimeout}
+import org.scalajs.dom.window.{ clearTimeout, navigator, setTimeout }
 import outwatch.dom._
 import outwatch.dom.dsl._
 import rx._
 import wust.webUtil.Elements._
 import wust.webUtil.outwatchHelpers._
 import wust.api.ClientInfo
-import wust.css.{Styles, ZIndex}
+import wust.css.{ Styles, ZIndex }
 import wust.sdk.Colors
 import wust.webApp.Client
-import wust.webApp.state.{GlobalState, ScreenSize}
+import wust.webApp.state.{ GlobalState, ScreenSize }
 import wust.webApp.views.Components._
 
 import scala.scalajs.LinkingInfo
-import scala.util.{Failure, Success}
+import scala.util.{ Failure, Success }
+import wust.webApp.ProductionOnly
 
 object FeedbackForm {
 
   def apply(state: GlobalState)(implicit ctx: Ctx.Owner) = {
-    val show = Var(false)
-    val activeDisplay = Rx { display := (if (show()) "block" else "none") }
+    val showPopup = Var(false)
+    val activeDisplay = Rx { display := (if (showPopup()) "block" else "none") }
 
     val feedbackText = Var("")
     val clear = Handler.unsafe[Unit].mapObservable(_ => "")
@@ -34,7 +38,8 @@ object FeedbackForm {
     var timeout: Option[Int] = None
     def submit(): Unit = {
       if (feedbackText.now.trim.nonEmpty) {
-        Client.api.feedback(ClientInfo(navigator.userAgent), feedbackText.now).onComplete {
+        //         Client.api.feedback(ClientInfo(navigator.userAgent), feedbackText.now).onComplete { }
+        Try(crisp.push(js.Array("do", "message:send", js.Array("text", feedbackText.now)))) match {
           case Success(()) =>
             statusText() = "Thank you!"
             timeout.foreach(clearTimeout)
@@ -42,7 +47,7 @@ object FeedbackForm {
             clear.onNext(())
           case Failure(t) =>
             scribe.warn("Error sending feedback to backend", t)
-            statusText() = "Failed to send feedback, please try again or via email..."
+            statusText() = "Failed to send. Please try again or via email... "
             timeout.foreach(clearTimeout)
             timeout = Some(setTimeout(() => statusText() = initialStatus, 3000))
         }
@@ -54,6 +59,7 @@ object FeedbackForm {
     val feedbackForm = div(
       fontSize.smaller,
       cls := "enable-text-selection",
+      h3("How can we help you?"),
       div(
         cls := "ui form",
         textArea(
@@ -69,19 +75,19 @@ object FeedbackForm {
 
     div(
       div(
-        "Feedback ", freeSolid.faCaretDown,
+        "Feedback/Support ", freeSolid.faCaretDown,
         // like semantic-ui tiny button
-        fontSize := "0.85714286rem", 
+        fontSize := "0.85714286rem",
         fontWeight := 700,
         padding := ".58928571em 1.125em .58928571em",
         cursor.pointer,
 
         onClick.stopPropagation foreach {
-          Analytics.sendEvent("feedback", if (show.now) "close" else "open")
-          show.update(!_)
+          Analytics.sendEvent("feedback", if (showPopup.now) "close" else "open")
+          showPopup.update(!_)
         },
-        onGlobalEscape(false) --> show,
-        onGlobalClick(false) --> show,
+        onGlobalEscape(false) --> showPopup,
+        onGlobalClick(false) --> showPopup,
       ),
       div(
         activeDisplay,
@@ -104,41 +110,59 @@ object FeedbackForm {
           button(
             Styles.flexStatic,
             tpe := "button",
-            cls := "ui tiny violet button",
+            cls := "ui tiny blue button",
             marginRight := "0",
-            "Submit",
+            "Send",
             onClick foreach { submit() },
-            // onClick(false) --> show,
+          // onClick(false) --> showPopup,
           ),
         ),
-        div("Or send us an email: ", Components.woostTeamEmailLink, "."),
+        div("Or write us an email: ", Components.woostTeamEmailLink, "."),
 
         div(cls := "ui divider", marginTop := "30px"),
-        div(width := "200px", "You can also vote on features and suggest new ones:"),
-        div(voteOnFeatures),
+        supportChatButton(showPopup),
+        voteOnFeaturesButton,
         onClick.stopPropagation foreach {}, // prevents closing feedback form by global click
       )
     )
   }
 
-  def voteOnFeatures = button(
-    "Vote on features",
-    cls := "ui violet tiny button",
+  def supportChatButton(showPopup: Var[Boolean]) = {
+    button(
+      freeSolid.faComments, " Open Support Chat",
+      cls := "ui blue tiny fluid button",
+      marginTop := "5px",
+      ProductionOnly{
+        onClick.stopPropagation.foreach { _ =>
+          Try{
+            crisp.push(js.Array("do", "chat:show"))
+            crisp.push(js.Array("do", "chat:open"))
+          }
+          showPopup() = false
+        }
+      }
+    )
+  }
+
+  def voteOnFeaturesButton = button(
+    freeSolid.faSort, " Vote on features",
+    cls := "ui violet tiny fluid button",
     marginTop := "5px",
     cls := "vote-button",
     snabbdom.VNodeProxy.repairDomBeforePatch, // nolt button modifies the dom
-    VDomModifier.ifNot(LinkingInfo.developmentMode)(
+    ProductionOnly {
       onDomMount.foreach { _ =>
         try {
           nolt("init", new NoltData {
             var url = "https://woost.nolt.io"
             var selector = ".vote-button"
           });
-        } catch { case e: Throwable =>
-          scribe.error("Failed to init nolt", e)
+        } catch {
+          case e: Throwable =>
+            scribe.error("Failed to init nolt", e)
         }
       }
-    )
+    }
   )
 
 }
---
+++ webUtil/src/main/scala/wust/facades/nolt/Crisp.scala
@@ -0,0 +1,11 @@
+package wust.facades.crisp
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation._
+
+@js.native
+@JSGlobal("$crisp")
+object crisp extends js.Object {
+  def push(options: js.Array[js.Any]): Unit = js.native
+}
+
