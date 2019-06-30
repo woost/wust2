f6b5d7f166c34c10090cf28f3a5ea0169a0a1cb9
Felix Dietze <felix.dietze@rwth-aachen.de>
2019-06-30 13:00:20 -0400

    Marked 0.6.3

--- project/Deps.scala
+++ project/Deps.scala
@@ -133,7 +133,7 @@ object Deps {
   object npm {
     val defaultPassiveEvents = "default-passive-events" -> "1.0.10"
     val intersectionObserver = "intersection-observer" -> "0.7.0"
-    val marked = "marked" -> "0.6.1" // https://github.com/markedjs/marked/issues/1505
+    val marked = "marked" -> "0.6.3"
     val markedSanitizer = "marked-sanitizer-github" -> "1.0.0"
     val highlight = "highlight.js" -> "9.15.8"
     val dateFns = "date-fns" -> "v2.0.0-alpha.27"
