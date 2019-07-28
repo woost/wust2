384014f96d43a076dc3fe65049d575e3b7f54c9e
Felix Dietze <felix.dietze@rwth-aachen.de>
2019-07-27 20:31:29 -0400

    ADT for ViewGraphTransformation

--- webApp/src/main/scala/wust/webApp/views/ViewGraphTransformation.scala
+++ webApp/src/main/scala/wust/webApp/views/ViewGraphTransformation.scala
@@ -3,69 +3,56 @@ package wust.webApp.views
 import outwatch.dom.VDomModifier
 import wust.webApp.Icons
 import wust.webUtil.outwatchHelpers._
+import wust.util.macros.SubObjects
 
 
-final case class ViewGraphTransformation(
-  transform: UserViewGraphTransformation,
-  icon: VDomModifier,
-  description: String,
-  enablesTransform: Seq[UserViewGraphTransformation] = Seq.empty[UserViewGraphTransformation],
-  disablesTransform: Seq[UserViewGraphTransformation] = Seq.empty[UserViewGraphTransformation],
-  invertedSwitch: Boolean = false, //Filter is active, so enabling it will turn it off
-)
+sealed trait ViewGraphTransformation{
+  def transform: UserViewGraphTransformation
+  def icon: VDomModifier
+  def description: String
+  def enablesTransform: Seq[UserViewGraphTransformation] = Seq.empty[UserViewGraphTransformation]
+  def disablesTransform: Seq[UserViewGraphTransformation] = Seq.empty[UserViewGraphTransformation]
+  def invertedSwitch: Boolean = false //Filter is active, so enabling it will turn it off
+}
 
 object ViewGraphTransformation {
-  val allTransformations: List[ViewGraphTransformation] = List(
-    //    ViewGraphTransformation.Deleted.inGracePeriod,
-    ViewGraphTransformation.Deleted.onlyDeleted,
-    ViewGraphTransformation.Deleted.excludeDeleted,
-    //    ViewGraphTransformation.Deleted.noDeletedButGraced,
-    ViewGraphTransformation.Assignments.onlyAssignedTo,
-    ViewGraphTransformation.Assignments.onlyNotAssigned,
-    ViewGraphTransformation.Automated.hideTemplates,
-    //    Identity,
-  )
-
-  val identity = ViewGraphTransformation(
-    icon = Icons.noFilter,
-    description = "Reset ALL filters",
-    transform = GraphOperation.Identity,
-  )
+  val allTransformations: Array[ViewGraphTransformation] = SubObjects.all[ViewGraphTransformation]
 
   object Deleted {
-    val onlyDeleted = ViewGraphTransformation (
-      icon = Icons.delete,
-      description = "Show only deleted items",
-      transform = GraphOperation.OnlyDeletedChildren,
-      disablesTransform = List(GraphOperation.ExcludeDeletedChildren)
-    )
-    val excludeDeleted = ViewGraphTransformation (
-      icon = Icons.undelete,
-      description = "Show deleted items", // Turns this filter off
-      transform = GraphOperation.ExcludeDeletedChildren,
-      invertedSwitch = true
-    )
+    case object onlyDeleted extends ViewGraphTransformation {
+      def icon = Icons.delete
+      def description = "Show only deleted items"
+      def transform = GraphOperation.OnlyDeletedChildren
+      override def disablesTransform = List(GraphOperation.ExcludeDeletedChildren)
+    }
+
+    case object excludeDeleted extends ViewGraphTransformation {
+      def icon = Icons.undelete
+      def description = "Show deleted items" // Turns this filter off
+      def transform = GraphOperation.ExcludeDeletedChildren
+      override def invertedSwitch = true
+    }
   }
 
   object Automated {
-    val hideTemplates = ViewGraphTransformation(
-      icon = Icons.automate,
-      description = "Show automation templates", // Turns this filter off
-      transform = GraphOperation.AutomatedHideTemplates,
-      invertedSwitch = true
-    )
+    case object hideTemplates extends ViewGraphTransformation{
+      def icon = Icons.automate
+      def description = "Show automation templates" // Turns this filter off
+      def transform = GraphOperation.AutomatedHideTemplates
+      override def invertedSwitch = true
+    }
   }
 
   object Assignments {
-    val onlyAssignedTo = ViewGraphTransformation (
-      icon = Icons.task,
-      description = s"Show items assigned to: Me",
-      transform = GraphOperation.OnlyAssignedTo,
-    )
-    val onlyNotAssigned = ViewGraphTransformation (
-      icon = Icons.task,
-      description = "Show unassigned items",
-      transform = GraphOperation.OnlyNotAssigned,
-    )
+    case object onlyAssignedTo extends ViewGraphTransformation {
+      def icon = Icons.task
+      def description = s"Show items assigned to: Me"
+      def transform = GraphOperation.OnlyAssignedTo
+    }
+    case object onlyNotAssigned extends ViewGraphTransformation {
+      def icon = Icons.task
+      def description = "Show unassigned items"
+      def transform = GraphOperation.OnlyNotAssigned
+    }
   }
 }
