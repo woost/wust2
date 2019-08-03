9c75c9aa277d2b0c571dc4b97f2fcdbb7dd9bb78
Felix Dietze <felix.dietze@rwth-aachen.de>
2019-08-03 15:37:46 -0400

    suppress warning in crisp facade

--- webUtil/src/main/scala/wust/facades/crisp/Crisp.scala
+++ webUtil/src/main/scala/wust/facades/crisp/Crisp.scala
@@ -2,9 +2,14 @@ package wust.facades.crisp
 
 import scala.scalajs.js
 import scala.scalajs.js.annotation._
+import com.github.ghik.silencer.silent
 
+//TODO: this is a workaround for scalajs: https://github.com/scala-js/scala-js/issues/3737
+object JSNames {
+  @silent("possible missing interpolator") final val Crisp = "$crisp"
+}
 @js.native
-@JSGlobal("$crisp")
+@JSGlobal(JSNames.Crisp)
 object crisp extends js.Object {
   def push(options: js.Array[js.Any]): Unit = js.native
   def is: js.UndefOr[js.Function1[String,Boolean]] = js.native
