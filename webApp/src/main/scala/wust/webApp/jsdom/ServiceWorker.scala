406bcbea911f8dea3bdcf841715ec811928b21d6
johannes karoff <johannes@karoff.net>
2019-08-03 16:28:33 +0200

    workaround missing sw.controller on initial page load

--- webApp/src/main/scala/wust/webApp/jsdom/ServiceWorker.scala
+++ webApp/src/main/scala/wust/webApp/jsdom/ServiceWorker.scala
@@ -5,12 +5,16 @@ import io.circe.{Decoder, Encoder, Json}
 import monix.reactive.Observable
 import monix.reactive.subjects.PublishSubject
 import org.scalajs.dom.window
+import org.scalajs.dom.experimental.serviceworkers
 import wust.webUtil.outwatchHelpers._
 import wust.api.Authentication
 
 import scala.util.{Failure, Success, Try}
 
 object ServiceWorker {
+  // TODO: need var to have active serviceworker. why is navigator.serviceWorker.controller null on initial page load where sw installed?! should be same as registration.active:
+  // https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerContainer/controller
+  private var activeServiceworker: Option[serviceworkers.ServiceWorker] = None
 
   // returns an observable that notifies whenever a new serviceworker is registered and activated
   def register(location: String): Observable[Unit] = {
@@ -25,8 +29,9 @@ object ServiceWorker {
               scribe.info("SW successfully registered")
               registration.onupdatefound = { event =>
                 registration.installing.onstatechange = { event =>
-                  if (registration.active != null && sw.controller != null) {
+                  if (registration.active != null && activeServiceworker.forall(_ != registration.active)) {
                     scribe.info("SW successfully activated")
+                    activeServiceworker = Option(registration.active)
                     subject.onNext(())
                   }
                 }
@@ -41,6 +46,7 @@ object ServiceWorker {
         }
       })
 
+      // not needed, because our updates should trigger automatically when core-version is offline...
       // // when a new serviceworker took over, reload the page
       // // Approach #2 from
       // // https://redfin.engineering/how-to-fix-the-refresh-button-when-using-service-workers-a8e27af6df68
@@ -69,6 +75,7 @@ object ServiceWorker {
   object WorkerMessage {
     import io.circe.generic.extras.semiauto._
     import wust.api.serialize.Circe._ // das gibt die die config mit `{"type": "SubClassName", .... }`
+
     implicit val serviceWorkerMessageEncoder: Encoder[WorkerMessage] = deriveEncoder[WorkerMessage]
     implicit val serviceWorkerMessageDecoder: Decoder[WorkerMessage] = deriveDecoder[WorkerMessage]
   }
@@ -81,19 +88,18 @@ object ServiceWorker {
   def sendAuth(auth: Authentication): Unit = {
     import io.circe.syntax._
 
-    scribe.info("Querying serviceworker for auth-sync")
-    Navigator.serviceWorker.foreach { sw =>
-      val activeServiceworker = sw.controller
-      if (activeServiceworker != null) auth match {
+    activeServiceworker match {
+
+      case Some(activeServiceworker) => auth match {
         case Authentication.Verified(_, _, token) =>
           scribe.info("Sending auth to serviceworker")
           activeServiceworker.postMessage((AuthMessage(token): WorkerMessage).asJson.noSpaces);
         case _ =>
           scribe.info("Sending de-auth to serviceworker")
           activeServiceworker.postMessage((DeAuthMessage: WorkerMessage).asJson.noSpaces);
-      } else {
-        scribe.info("No serviceworker found")
       }
+
+      case None => scribe.info("No serviceworker found for auth sync.")
     }
   }
 
