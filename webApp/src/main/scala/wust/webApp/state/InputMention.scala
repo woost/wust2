b0af9ec62c1e565381c5270bb27127fbfae6333f
johannes karoff <johannes@karoff.net>
2019-08-06 13:23:28 +0200

    use user id of username is empty in mention

--- webApp/src/main/scala/wust/webApp/state/InputMention.scala
+++ webApp/src/main/scala/wust/webApp/state/InputMention.scala
@@ -6,8 +6,13 @@ import wust.util.StringOps
 object InputMention {
   val mentionsRegex = raw"@(?:[^ \\]|\\\\)*(?:\\ [^ ]*)*".r
   def nodeToMentionsString(node: Node): String = {
-    val lines = node.str.linesIterator
-    if (lines.hasNext) stringToMentionsString(lines.next) else ""
+    val firstLine = {
+      val lines = node.str.linesIterator
+      if (lines.hasNext) lines.next else ""
+    }
+
+    val str = if (firstLine.isEmpty) node.id.toBase58 else firstLine
+    stringToMentionsString(str)
   }
   def stringToMentionsString(str: String): String = {
     StringOps.trimToMaxLength(str.replace("\\", "\\\\").replace(" ", "\\ "), 100)
