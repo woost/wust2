8ffeeb3aebfc8b10ae672c5c0ecb0de9775021ef
johannes karoff <johannes@karoff.net>
2019-08-08 17:10:26 +0200

    wip

--- dbMigration/core/V113__node_access_materialized.sql
+++ dbMigration/core/V113__node_access_materialized.sql
@@ -9,25 +9,25 @@
 -- 4) insert member. need to recalute that specific user in deep children/targets.
 -- 5) delete member. need to invalidate and recalute that specific user in deep children/targets.
 
+-- table to store invalidated nodes via changes
 create table node_can_access_invalid(
-  node_id uuid references node not null on delete cascade,
+  node_id uuid not null references node on delete cascade
 );
-create unique index on node_can_access_mat (node_id);
+create unique index on node_can_access_invalid (node_id);
 
 -- materialized table to cache granted access for user on node (valid = false means it needs to be recalculated)
 create table node_can_access_mat(
-  node_id uuid references node not null on delete cascade,
-  user_id uuid references node not null on delete cascade,
+  node_id uuid not null references node on delete cascade,
+  user_id uuid not null references node on delete cascade
 );
 create unique index on node_can_access_mat (node_id, user_id);
 create index on node_can_access_mat (user_id);
 
 -- materialized table to cache public nodes including inheritance (valid = false means it needs to be recalculated)
 create table node_can_access_public_mat(
-  node_id uuid references node not null on delete cascade,
+  node_id uuid not null references node on delete cascade
 );
 create index on node_can_access_public_mat (node_id);
-create index on node_can_access_public_mat (valid);
 
 -- trigger for node update
 create function node_update() returns trigger
@@ -36,8 +36,7 @@ create function node_update() returns trigger
 as $$
   begin
     IF (new.accesslevel <> old.accesslevel) THEN
-        --TODO: invalidate children
-        insert into node_can_access_invalid values(new.id);
+        insert into node_can_access_invalid select unnest(node_can_access_deep_children(new.id)) on conflict do nothing;
     end if;
     return new;
   end;
@@ -51,10 +50,10 @@ create function edge_insert() returns trigger
 as $$
   begin
     IF (new.data->>'type' = 'Child' or new.data->>'type' = 'LabeledProperty') THEN
-        insert into node_can_access_invalid values(new.targetid);
+        insert into node_can_access_invalid select unnest(node_can_access_deep_children(new.targetid)) on conflict do nothing;
     ELSIF(new.data->>'type' = 'Member') THEN
         --TODO: strictly speaking we just need to recalculate the user of this membership for this node.
-        insert into node_can_access_invalid values(new.sourceid);
+        insert into node_can_access_invalid select unnest(node_can_access_deep_children(new.sourceid)) on conflict do nothing;
     end IF;
     return new;
   end;
@@ -69,10 +68,10 @@ as $$
   begin
     IF (new.sourceid <> old.sourceid or new.targetid <> old.targetid or new.data->>'type' <> old.data->>'type') THEN
         IF (new.data->>'type' = 'Child' or new.data->>'type' = 'LabeledProperty') THEN
-            insert into node_can_access_invalid values(new.targetid);
+            insert into node_can_access_invalid select unnest(node_can_access_deep_children(new.targetid)) on conflict do nothing;
         ELSIF(new.data->>'type' = 'Member') THEN
             --TODO: strictly speaking we just need to recalculate the user of this membership for this node.
-            insert into node_can_access_invalid values(new.sourceid);
+            insert into node_can_access_invalid select unnest(node_can_access_deep_children(new.sourceid)) on conflict do nothing;
         end IF;
     end IF;
     return new;
@@ -87,154 +86,165 @@ create function edge_delete() returns trigger
 as $$
   begin
     IF (new.data->>'type' = 'Child' or new.data->>'type' = 'LabeledProperty') THEN
-        --TODO: invalidate children...
-        insert into node_can_access_invalid values(new.targetid);
+        insert into node_can_access_invalid select unnest(node_can_access_deep_children(new.targetid)) on conflict do nothing;
     ELSIF(new.data->>'type' = 'Member') THEN
         --TODO: strictly speaking we just need to recalculate the user of this membership for this node.
-        insert into node_can_access_invalid values(new.sourceid);
+        insert into node_can_access_invalid select unnest(node_can_access_deep_children(new.sourceid)) on conflict do nothing;
     end IF;
     return new;
   end;
 $$;
-
 create trigger edge_delete_trigger before delete on edge for each row execute procedure edge_delete();
 
--- recalculate invalid nodes can_access_node_mat
-create function refresh_node_can_access(id uuid)
-  returns lazy.account_balances_mat
+-- deep access children of node
+create function node_can_access_deep_children(node_id uuid)
+  returns table(id uuid)
   security definer
   language sql
 as $$
-  with t as (
-    select
-      coalesce(
-        sum(amount) filter (where post_time <= current_timestamp),
-        0
-      ) as balance,
-      coalesce(
-        min(post_time) filter (where current_timestamp < post_time),
-        'Infinity'
-      ) as expiration_time
-    from transactions
-    where name=_name
-  )
-  update lazy.account_balances_mat
-  set balance = t.balance, expiration_time = t.expiration_time
-  from t
-  where name = id
-  returning node_can_access_mat.*;
+    with recursive
+        content(id) AS (
+            select id from node where id = node_id
+            union -- discards duplicates, therefore handles cycles and diamond cases
+            select accessedge.target_nodeid FROM content INNER JOIN accessedge ON accessedge.source_nodeid = content.id
+        )
+        -- all transitive children
+        select id from content;
 $$;
 
--- recursively check whether a node is accessible. with side-effect of filling the cache.
--- non-existing user: we assume that a user exist in any cases and therefore we do not handle this explicitly
-create function node_can_access_reursive(userid uuid, nodeid uuid, visited uuid[], get_via_url_mode boolean) returns can_access_result as $$
+CREATE TYPE node_can_access_public_result AS ENUM ('unknown', 'private', 'public');
+
+create function node_can_access_public_agg_fun(accum node_can_access_public_result, curr node_can_access_public_result) returns node_can_access_public_result AS $$
+    select (
+        CASE
+        WHEN accum = 'public' THEN 'public'
+        WHEN curr = 'public' THEN 'public'
+        WHEN accum = 'unknown' THEN 'unknown'
+        ELSE curr
+        END
+    );
+$$ LANGUAGE sql strict;
+create aggregate node_can_access_public_agg(node_can_access_public_result)
+(
+    INITCOND = 'private',
+    STYPE = node_can_access_public_result,
+    SFUNC = node_can_access_public_agg_fun
+);
+
+-- recursively check whether a node is public. will use cache if valid and otherwise with side-effect of filling the cache.
+-- returns true if public and false if not
+create function node_can_access_public_recursive(nodeid uuid, visited uuid[]) returns node_can_access_public_result as $$
 declare
-    member_access_level accesslevel;
     node_access_level accesslevel;
-    result boolean;
-    access_result can_access_result;
+    public_result node_can_access_public_result;
+    is_public boolean;
+    is_invalid boolean;
 begin
-    select exists(select can_access from node_can_access_mat where node_id = nodeid and user_id = userid and valid limit 1) into result;
-    IF ( result = true ) THEN return 'allowed'; end if;
-    IF ( result = false ) THEN return 'forbidden'; end if;
     IF ( nodeid = any(visited) ) THEN return 'unknown'; end if; -- prevent inheritance cycles
 
-    -- is there a membership?
-    select data->>'level' into member_access_level from member where member.target_userid = userid and member.source_nodeid = nodeid limit 1;
-    IF (member_access_level IS NOT NULL) THEN -- if member edge exists
-        -- either the use is granted readwrite, then he can access. otherwise not.
-        result := member_access_level = 'readwrite';
-        IF ( result = true ) THEN
-            insert into node_can_access_mat VALUES (nodeid, result) on conflict (node_id, user_id) do update set valid = true;
-            return 'allowed';
-        ELSE
-            delete from node_can_access_mat where node_id = nodeid and user_id = userid;
-            return 'forbidden';
-        end if;
-    END IF;
+    is_public := false;
 
-    -- if no member edge exists, read access level directly from node
+    select exists(select * from node_can_access_invalid where node_id = nodeid) into is_invalid;
+    IF ( is_invalid = false ) THEN
+        is_public := (select exists(select * from node_can_access_public_mat where node_id = nodeid));
+        if (is_public = true) then
+            return 'public';
+        else
+            return 'private';
+        end if;
+    end if;
+
+    -- read node access level to decide for recurison
     select accessLevel into node_access_level from node where id = nodeid limit 1;
 
-    -- get_via_url_mode allows access to public nodes, you can become a member of these nodes.
-    -- IF (get_via_url_mode and node_access_level = 'readwrite') THEN
-        -- insert into node_can_access_mat VALUES (nodeid, result) on conflict (node_id, user_id) do update set valid = true;
-        -- return 'allowed';
-    -- END IF;
-
-    -- if node access level is inherited or public, check above, else not grant access.
-    IF (node_access_level IS NULL or node_access_level = 'readwrite') THEN -- null means inherit for the node
+    -- if node access level is public, return true
+    IF (node_access_level = 'readwrite') THEN
+        is_public := true;
+    ELSIF (node_access_level IS NULL) THEN-- null means inherit for the node
         -- recursively inherit permissions from parents. minimum one parent needs to allowed access.
-        select can_access_agg(node_can_access_reursive(userid, accessedge.source_nodeid, visited || nodeid, get_via_url_mode)) into access_result from accessedge where accessedge.target_nodeid = nodeid;
-        -- return unknown if we cannot get a recursive answer, because of a cycle
-        -- if this is because of visited, then we cannot know whether this node is accessible
-        -- but if visited is empty, we know for sure that this node is inaccessible.
-        if access_result = 'unknown' and cardinality(visited) > 0 then return 'unknown'; end if;
-        result := access_result = 'allowed';
-        insert into node_can_access_mat VALUES (nodeid, result) on conflict (node_id, user_id) do update set valid = true;
-        IF ( result = true ) THEN
-            insert into node_can_access_mat VALUES (nodeid, result) on conflict (node_id, user_id) do update set valid = true;
-            return 'allowed';
-        ELSE
-            delete from node_can_access_mat where node_id = nodeid and user_id = userid;
-            return 'forbidden';
-        end if;
+        public_result := (select node_can_access_public_agg(node_can_access_reursive(accessedge.source_nodeid, visited || nodeid)) from accessedge where accessedge.target_nodeid = nodeid);
+        if (access_result = 'unknown' and cardinality(visited) > 0) then return 'unknown'; end if;
+        if (public_result = 'public') then is_public := true; end if;
     END IF;
 
-    delete from node_can_access_mat where node_id = nodeid and user_id = userid;
-    return 'forbidden';
+    delete from node_can_access_public_mat where node_id = nodeid;
+    delete from node_can_access_invalid_node where node_id = nodeid;
+    if (is_public = true) then
+        insert into node_can_access_public_mat VALUES(node_id);
+        return 'public';
+    else
+        return 'private';
+    end if;
 end;
 $$ language plpgsql strict;
 
--- recursively check whether a node is accessible. with side-effect of filling the cache.
--- non-existing user: we assume that a user exist in any cases and therefore we do not handle this explicitly
-create function node_can_access_reursive(userid uuid, nodeid uuid, visited uuid[], get_via_url_mode boolean) returns can_access_result as $$
+CREATE TYPE node_can_access_result AS (
+    known boolean,
+    user_ids uuid[]
+);
+
+create function node_can_access_agg_fun(accum node_can_access_result, curr node_can_access_result) returns node_can_access_result AS $$
+    select false, accum.user_ids || curr.user_ids where not accum.known or not curr.known
+    UNION ALL
+    select true, accum.user_ids || curr.user_ids where accum.known and curr.known
+$$ LANGUAGE sql strict;
+create aggregate node_can_access_agg(node_can_access_result)
+(
+    INITCOND = '(true,{})',
+    STYPE = node_can_access_result,
+    SFUNC = node_can_access_agg_fun
+);
+
+-- recursively check whether a node is accessible. will use cache if valid and otherwise with side-effect of filling the cache.
+-- returns the user-ids that are allowed to access this node
+create function node_can_access_recursive(nodeid uuid, visited uuid[]) returns node_can_access_result as $$
 declare
-    member_access_level accesslevel;
     node_access_level accesslevel;
-    result boolean;
-    access_result can_access_result;
+    user_ids uuid[];
+    is_known boolean;
+    result node_can_access_result;
+    is_invalid boolean;
 begin
-    select exists(select can_access from node_can_access_mat where node_id = nodeid and user_id = userid and valid limit 1) into result;
-    IF ( result = true ) THEN return 'allowed'; end if;
-    IF ( result = false ) THEN return 'forbidden'; end if;
-    IF ( nodeid = any(visited) ) THEN return 'unknown'; end if; -- prevent inheritance cycles
+    IF ( nodeid = any(visited) ) THEN return row(false, array[]::uuid[]); end if; -- prevent inheritance cycles
 
-    -- is there a membership?
-    select data->>'level' into member_access_level from member where member.target_userid = userid and member.source_nodeid = nodeid limit 1;
-    IF (member_access_level IS NOT NULL) THEN -- if member edge exists
-        -- either the use is granted readwrite, then he can access. otherwise not.
-        result := member_access_level = 'readwrite';
-        IF ( result = true ) THEN
-            insert into node_can_access_mat VALUES (nodeid, result);
-            return 'allowed';
-        ELSE
-            delete from node_can_access_mat where node_id = nodeid and user_id = userid;
-            return 'forbidden';
-        end if;
-    END IF;
+    is_known := true;
 
-    -- if no member edge exists, read access level directly from node
+    select exists(select * from node_can_access_invalid where node_id = nodeid) into is_invalid;
+    IF ( is_invalid = false ) THEN
+        user_ids := (select array_agg(user_id) from node_can_access_mat where node_id = nodeid);
+        return row(true, user_ids);
+    end if;
+
+    -- collect all user_ids that have granted access for this node
+    select array_agg(member.target_userid) into user_ids from member where data->>'level' = 'readwrite' and member.source_nodeid = nodeid;
+
+    -- read node access level to decide for recurison
     select accessLevel into node_access_level from node where id = nodeid limit 1;
 
-    -- get_via_url_mode allows access to public nodes, you can become a member of these nodes.
-    IF (get_via_url_mode and node_access_level = 'readwrite') THEN return 'allowed'; END IF;
-
     -- if node access level is inherited or public, check above, else not grant access.
-    IF (node_access_level IS NULL or node_access_level = 'readwrite') THEN -- null means inherit for the node
+    IF (node_access_level IS NULL or node_access_level = 'readwrite') THEN -- null means inherit for the node, readwrite/public inherits as well
         -- recursively inherit permissions from parents. minimum one parent needs to allowed access.
-        select can_access_agg(node_can_access_reursive(userid, accessedge.source_nodeid, visited || nodeid, get_via_url_mode)) into access_result from accessedge where accessedge.target_nodeid = nodeid;
-        -- return unknown if we cannot get a recursive answer, because of a cycle
-        -- if this is because of visited, then we cannot know whether this node is accessible
-        -- but if visited is empty, we know for sure that this node is inaccessible.
-        if access_result = 'unknown' and cardinality(visited) > 0 then return 'unknown'; end if;
-        result := access_result = 'allowed';
-        insert into node_can_access_mat VALUES (nodeid, userid, result);
-        IF ( result = true ) THEN return 'allowed'; end if;
-        return 'forbidden';
+        result := (select node_can_access_agg(node_can_access_reursive(accessedge.source_nodeid, visited || nodeid)) from accessedge where accessedge.target_nodeid = nodeid);
+        if (not result.known and cardinality(visited) > 0) then is_known := false end if;
+        user_ids := user_ids || node_can_access_result.user_ids;
     END IF;
 
-    delete from node_can_access_mat where node_id = nodeid and user_id = userid;
-    return 'forbidden';
+    delete from node_can_access_mat where node_id = nodeid;
+    delete from node_can_access_invalid_node where node_id = nodeid;
+    insert into node_can_access_mat select node_id, user_id from unnest(user_ids) as user_id;
+    return row(is_known, user_ids);
+end;
+$$ language plpgsql strict;
+
+create function node_can_access(nodeid uuid, userid uuid) returns boolean as $$
+begin
+    IF NOT EXISTS (select 1 from node where id = nodeid) then return true; end if; -- everybody has full access to non-existant nodes
+    node_can_access_recursive(nodeid, array[]::uuid[]) = 'allowed';
+end;
+$$ language plpgsql strict;
+
+create function node_can_access_public(nodeid uuid) returns boolean as $$
+begin
+    return node_can_access_public_recursive(nodeid, array[]::uuid[]) = 'public';
 end;
 $$ language plpgsql strict;
