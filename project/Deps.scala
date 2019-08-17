7280258623ed3cb23485ce8b0ad1fb10a7d3931d
johannes karoff <johannes@karoff.net>
2019-08-17 17:26:57 +0200

    use fork of autoresize-textarea because of missing padding in height

--- project/Deps.scala
+++ project/Deps.scala
@@ -153,7 +153,7 @@ object Deps {
     val juration = "juration" -> "0.1.0"
     val wdtEmojiBundle = "wdt-emoji-bundle" -> "git://github.com/fdietze/wdt-emoji-bundle.git#fcf05d9"
     val tribute = "tributejs" -> "3.7.1"
-    val autoresizeTextarea = "autoresize-textarea" -> "1.1.3"
+    val autoresizeTextarea = "autoresize-textarea" -> "git://github.com/cornerman/autoresize-textarea.git#d197ddc"
 
     val webpackDependencies =
       "webpack-closure-compiler" -> "git://github.com/roman01la/webpack-closure-compiler.git#3677e5e" :: //TODO: "closure-webpack-plugin" -> "1.0.1" :: https://github.com/webpack-contrib/closure-webpack-plugin/issues/47
