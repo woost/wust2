d26b705bd63ac919913f016ffdf3291f0c76fd23
johannes karoff <johannes@karoff.net>
2019-08-18 18:36:54 +0200

    log with scribe again in sw related stuff

--- webApp/src/main/scala/wust/webApp/jsdom/ServiceWorker.scala
+++ webApp/src/main/scala/wust/webApp/jsdom/ServiceWorker.scala
@@ -26,16 +26,16 @@ object ServiceWorker {
         Try(sw.register(location)).toEither match {
           case Right(registered) => registered.toFuture.onComplete {
             case Success(registration) =>
-              println("SW successfully registered!")
+              scribe.info("SW successfully registered!")
               if (registration.active != null) {
-                println("SW is already active")
+                scribe.info("SW is already active")
                 activeServiceworker = Option(registration.active)
                 subject.onNext(())
               }
               registration.onupdatefound = { event =>
                 registration.installing.onstatechange = { event =>
                   if (registration.active != null && activeServiceworker.forall(_ != registration.active)) {
-                    println("SW newly activated")
+                    scribe.info("SW newly activated")
                     activeServiceworker = Option(registration.active)
                     subject.onNext(())
                   }
@@ -97,14 +97,14 @@ object ServiceWorker {
 
       case Some(activeServiceworker) => auth match {
         case Authentication.Verified(_, _, token) =>
-          println("Sending auth to serviceworker")
+          scribe.info("Sending auth to serviceworker")
           activeServiceworker.postMessage((AuthMessage(token): WorkerMessage).asJson.noSpaces);
         case _ =>
-          println("Sending de-auth to serviceworker")
+          scribe.info("Sending de-auth to serviceworker")
           activeServiceworker.postMessage((DeAuthMessage: WorkerMessage).asJson.noSpaces);
       }
 
-      case None => println("No serviceworker found for auth sync.")
+      case None => scribe.info("No serviceworker found for auth sync.")
     }
   }
 
