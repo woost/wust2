30da8313bd576502a43431fe0f914ba49e0682dd
Felix Dietze <felix.dietze@rwth-aachen.de>
2019-08-23 11:53:00 +0200

    Kanban/List: Dragging into background drops into inbox

--- webApp/src/main/scala/wust/webApp/views/ListView.scala
+++ webApp/src/main/scala/wust/webApp/views/ListView.scala
@@ -12,7 +12,8 @@ import wust.ids.{Feature, _}
 import wust.webApp.dragdrop.DragContainer
 import wust.webApp.state.{FocusState, GlobalState, Placeholder, TraverseState}
 import wust.webApp.views.Components._
-import wust.webApp.views.DragComponents.registerDragContainer
+import wust.webApp.views.DragComponents.{registerDragContainer, drag}
+import wust.webApp.dragdrop.{DragItem}
 
 object ListView {
   import SharedViewElements._
@@ -33,6 +34,8 @@ object ListView {
       overflow.auto,
       padding := "5px",
       flexGrow := 2,
+      drag(target = DragItem.Workspace(focusState.focusedId)),
+      registerDragContainer(DragContainer.Checklist),
     )
   }
 
