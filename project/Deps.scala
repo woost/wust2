5358c2ce1634eaa7e02db545323c62b2d046d6cc
Felix Dietze <felix.dietze@rwth-aachen.de>
2019-09-12 00:39:11 +0200

    Update exif to change minimization-critical code

--- project/Deps.scala
+++ project/Deps.scala
@@ -158,7 +158,7 @@ object Deps {
     val chartJs = "chart.js" -> "2.8.0"
     val hopscotch = "hopscotch" -> "0.3.1"
     val canvasImageUploader = "canvas-image-uploader" -> "git+https://git@github.com/selbekk/CanvasImageUploader.git#6e5a71b5c2c01b00e76c86d65f2959d3fa9f3125" // fork without jquery and black canvas fix
-    val exifJS = "exif-js" -> "git+https://git@github.com/fdietze/exif-js.git#7d4f2119a91480b51a23eb2681c6562572e7c731" // fork merges PR which avoids runtime error
+    val exifJS = "exif-js" -> "git+https://git@github.com/fdietze/exif-js.git#da3116c" // fork merges PR which avoids runtime error
 
     val webpackDependencies =
       "webpack-closure-compiler" -> "git://github.com/roman01la/webpack-closure-compiler.git#3677e5e" :: //TODO: "closure-webpack-plugin" -> "1.0.1" :: https://github.com/webpack-contrib/closure-webpack-plugin/issues/47
