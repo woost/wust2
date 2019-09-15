f6311e27df7b1fb3e2360792ce58984b09e359d6
johannes karoff <johannes@karoff.net>
2019-09-15 17:02:30 +0200

    rem debug print

--- webApp/src/main/scala/wust/webApp/parsers/UrlConfigParsing.scala
+++ webApp/src/main/scala/wust/webApp/parsers/UrlConfigParsing.scala
@@ -37,8 +37,6 @@ private object UrlOption {
     val key = "system"
 
     private def decodeSystemView(s: String): DecodeResult[View.System] = {
-      println(s)
-      println(View.map)
       View.map.get(s) match {
         case Some(view: View.System) => Right(view)
         case _               => Left(DecodeError.TypeError(s"Expected View.System, but got: '$view'"))
@@ -47,12 +45,10 @@ private object UrlOption {
 
     val regex = Regex[String](rx"^\w+$$")
       .map(_.flatMap { viewStr =>
-        println("decoding"+viewStr)
         decodeSystemView(viewStr)
       })
 
     def update(config: UrlConfig, text: String): DecodeResult[UrlConfig] = {
-      println("TEST " + text)
       parseSingle(regex, text).map { view =>
         config.copy(systemView = Some(view))
       }
@@ -139,11 +135,9 @@ object UrlConfigParser {
       }
     }
 
-    println("parsing " + route)
     val result = route.hash.fold[DecodeResult[UrlConfig]](Right(UrlConfig.default)) { hash =>
       val matched = decodeSeq(allOptionsRegex.eval(hash).toList)
       matched.map(_.foldLeft[UrlConfig](UrlConfig.default) { case (cfg, (key, value)) =>
-        println("GOT AN OPTION " + key + value)
         allOptionsMap.get(key) match {
           case Some(option) => option.update(cfg, value) match {
             case Right(cfg) => cfg
