cbb22fc1dbfbe3491e7c5ba73d2453ccf4f7f641
johannes karoff <johannes@karoff.net>
2019-09-27 15:58:01 +0200

    update outwatch

--- project/Deps.scala
+++ project/Deps.scala
@@ -56,7 +56,7 @@ object Deps {
   val scalarx = dep("com.github.fdietze.duality" %%% "scalarx" % "fc761c9")
   // val scalarx = dep("com.github.fdietze.duality" %%% "scalarx" % "94c6d80") // jitpack cannot handle the . in repo name scala.rx
   val outwatch = new {
-    private val version = "a5e35b8e"
+    private val version = "a141410"
     // val core = dep("io.github.outwatch" %%% "outwatch" % "0.11.1-SNAPSHOT")
     val core = dep("com.github.cornerman.outwatch" %%% "outwatch" % version)
     val monix = dep("com.github.cornerman.outwatch" %%% "outwatch-monix" % version)
