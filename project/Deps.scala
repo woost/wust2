be2a08b12d0a35424d149ecfaa19f7be4c72dd1b
johannes karoff <johannes@karoff.net>
2019-10-13 23:11:00 +0200

    update outwatch

--- project/Deps.scala
+++ project/Deps.scala
@@ -56,7 +56,7 @@ object Deps {
   val scalarx = dep("com.github.cornerman.scalarx" %%% "scalarx" % "7daef55")
   // val scalarx = dep("com.github.fdietze.duality" %%% "scalarx" % "fc761c9")
   val outwatch = new {
-    private val version = "51a53bc1"
+    private val version = "c586321e"
     // val core = dep("io.github.outwatch" %%% "outwatch" % "0.11.1-SNAPSHOT")
     val core = dep("com.github.cornerman.outwatch" %%% "outwatch" % version)
     // val monix = dep("io.github.outwatch" %%% "outwatch-monix" % "0.11.1-SNAPSHOT")
